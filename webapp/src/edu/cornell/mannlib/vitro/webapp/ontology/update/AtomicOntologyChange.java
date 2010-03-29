556836a905cdb56f4af86d7fa014f0ec07fbe2b6
bjl23 <bjl23@440791e1-c2bf-4c1e-ad7c-caf3f8b0ebe8>
2010-03-29 16:27:38 +0000

    resolves NIHVIVO-253 re ontology change objects

---
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/ontology/update/AtomicOntologyChange.java
@@ -0,0 +1,37 @@
+package edu.cornell.mannlib.vitro.webapp.ontology.update;
+
+public class AtomicOntologyChange {
+
+	private String sourceURI;	
+	private String destinationURI;
+	private AtomicChangeType atomicChangeType;
+
+	public String getSourceURI() {
+		return this.sourceURI;
+	}
+	
+	public void setSourceURI(String sourceURI) {
+		this.sourceURI = sourceURI;
+	}
+	
+	public String getDestinationURI() {
+		return this.destinationURI;
+	}
+	
+	public void setDestinationURI(String destinationURI) {
+		this.destinationURI = destinationURI;
+	}
+	
+	public AtomicChangeType getAtomicChangeType() {
+		return atomicChangeType;
+	}
+
+	public void setAtomicChangeType(AtomicChangeType atomicChangeType) {
+		this.atomicChangeType = atomicChangeType;
+	}
+	
+	public enum AtomicChangeType {
+		ADD, DELETE, RENAME
+	}
+	
+}
