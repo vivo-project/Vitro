787e378a3c594e52725e8cf67a65d423ff550d59
jeb228 <jeb228@440791e1-c2bf-4c1e-ad7c-caf3f8b0ebe8>
2010-05-21 17:42:03 +0000

    NIHVIVO-160 Start writing unit tests as the implementation progresses.

---
+++ webapp/test/edu/cornell/mannlib/vitro/webapp/utils/filestorage/FileStorageImplTest.java
@@ -0,0 +1,51 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
+
+package edu.cornell.mannlib.vitro.webapp.utils.filestorage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Test the FileStorage methods. The zero-argument constructor was tested in
+ * {@link FileStorageFactoryTest}.
+ */
+public class FileStorageImplTest {
+	@Ignore
+	@Test
+	public void baseDirDoesntExist() {
+		fail("baseDirDoesntExist not implemented");
+	}
+
+	@Ignore
+	@Test
+	public void partialInitializationRoot() {
+		fail("partialInitializationRoot not implemented");
+	}
+
+	@Ignore
+	@Test
+	public void partialInitializationNamespaces() {
+		fail("partialInitializationNamespaces not implemented");
+	}
+
+	@Ignore
+	@Test
+	public void notInitialized() {
+		fail("notInitialized not implemented");
+	}
+
+	@Ignore
+	@Test
+	public void initializedOK() {
+		fail("initializedOK not implemented");
+	}
+
+	@Ignore
+	@Test
+	public void initializedNamespacesDontMatch() {
+		fail("initializedNamespacesDontMatch not implemented");
+	}
+
+}
