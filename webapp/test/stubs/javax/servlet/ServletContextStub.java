bddc92fb72bfc3d919d6f5cb1d25bb45a4b2d8cc
jeb228 <jeb228@440791e1-c2bf-4c1e-ad7c-caf3f8b0ebe8>
2010-06-04 16:13:11 +0000

    NIHVIVO-510 Remove warnings for deprecated methods in ServletContext

--- webapp/test/stubs/javax/servlet/ServletContextStub.java
+++ webapp/test/stubs/javax/servlet/ServletContextStub.java
@@ -19,6 +19,7 @@ import javax.servlet.ServletException;
 /**
  * A simple stand-in for the {@link ServletContext}, for use in unit tests.
  */
+@SuppressWarnings("deprecation")
 public class ServletContextStub implements ServletContext {
 
 	// ----------------------------------------------------------------------
