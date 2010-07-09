273a49512e796b29efbd42b874a9b549429b879c
rjy7 <rjy7@440791e1-c2bf-4c1e-ad7c-caf3f8b0ebe8>
2010-07-09 16:28:59 +0000

    NIHVIVO-647 Continued work on publication form

---
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/edit/n3editing/PersonHasPublicationValidator.java
@@ -0,0 +1,16 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
+
+package edu.cornell.mannlib.vitro.webapp.edit.n3editing;
+
+import java.util.Map;
+
+public class PersonHasPublicationValidator implements N3Validator {
+
+    @Override
+    public Map<String, String> validate(EditConfiguration editConfig,
+            EditSubmission editSub) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
