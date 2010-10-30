e6d1d66a5a9821621d5962c2069df9961144decb
sjm222 <sjm222@440791e1-c2bf-4c1e-ad7c-caf3f8b0ebe8>
2010-10-30 00:34:53 +0000

    NIHVIVO-1220 initial development completed.

--- webapp/src/edu/cornell/mannlib/vitro/webapp/reasoner/SimpleReasoner.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/reasoner/SimpleReasoner.java
@@ -8,6 +8,7 @@ import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntProperty;
 import com.hp.hpl.jena.rdf.listeners.StatementListener;
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.ResourceFactory;
 import com.hp.hpl.jena.rdf.model.Statement;
@@ -51,9 +52,7 @@ public class SimpleReasoner extends StatementListener {
 			if (stmt.getPredicate().equals(RDF.type)) {
 			   materializeTypes(stmt);
 			}
-			
-			//TODO: make a configuration option controlling whether properties are materialized
-			//materializeProperties(stmt);
+
 		} catch (Exception e) {
 			// don't stop the edit if there's an exception
 			log.error("Exception while adding incremental inferences: ", e);
@@ -69,9 +68,7 @@ public class SimpleReasoner extends StatementListener {
 			if (stmt.getPredicate().equals(RDF.type)) {
 			   retractTypes(stmt);
 			}
-			
-			//TODO: make a configuration option controlling whether properties are materialized
-			//retractProperties(stmt);
+
 		} catch (Exception e) {
 			// don't stop the edit if there's an exception
 			log.error("Exception while retracting inferences: ", e);
@@ -104,7 +101,6 @@ public class SimpleReasoner extends StatementListener {
 					}	
 				}
 			} else {
-				//TODO: I could even take out this check and let it go to the exception, if you think it's better
 				log.debug("Didn't find target class (the object of the added rdf:type statement) in the TBox: " + ((Resource)stmt.getObject()).getURI());
 			}
 		} finally {
@@ -112,6 +108,46 @@ public class SimpleReasoner extends StatementListener {
 		}
 	}
 
+	
+	public void retractTypes(Statement stmt) {
+		
+		tboxModel.enterCriticalSection(Lock.READ);
+		
+		try {
+			OntClass cls = tboxModel.getOntClass(((Resource)stmt.getObject()).getURI()); 
+			
+			if (cls != null) {
+				ExtendedIterator<OntClass> superIt = cls.listSuperClasses(false);
+				while (superIt.hasNext()) {
+					OntClass parentClass = superIt.next();
+					
+					if (parentClass.isAnon()) continue;  
+					
+					if (entailedType(stmt.getSubject(),parentClass)) continue;    // if a type is still entailed without the
+					                                                              // removed statement, then don't remove it
+					                                                              // from the inferences
+					
+					Statement infStmt = ResourceFactory.createStatement(stmt.getSubject(), RDF.type, parentClass);
+						
+					inferenceModel.enterCriticalSection(Lock.WRITE);
+					try {
+						if (inferenceModel.contains(infStmt)) {
+							log.debug("Removing this inferred statement:  " + infStmt.toString() + " - " + infStmt.getSubject().toString() + " - " + infStmt.getPredicate().toString() + " - " + infStmt.getObject().toString());
+							inferenceModel.remove(infStmt);
+						}
+					} finally {
+						inferenceModel.leaveCriticalSection();
+					}	
+				}
+			} else {
+				log.debug("Didn't find target class (the object of the removed rdf:type statement) in the TBox: " + ((Resource)stmt.getObject()).getURI());
+			}
+		} finally {
+			tboxModel.leaveCriticalSection();
+		}
+	}
+
+	// The following two methods aren't called currently; current default behavior of VIVO is to not materialize such inferences.
 	public void materializeProperties(Statement stmt) {
 
 		tboxModel.enterCriticalSection(Lock.READ);
@@ -143,57 +179,46 @@ public class SimpleReasoner extends StatementListener {
 			tboxModel.leaveCriticalSection();
 		}
 	}
-	
-	public void retractTypes(Statement stmt) {
+	public void retractProperties(Statement stmt) {
 		
 		tboxModel.enterCriticalSection(Lock.READ);
 		
 		try {
-			OntClass cls = tboxModel.getOntClass(((Resource)stmt.getObject()).getURI()); 
+			OntProperty prop = tboxModel.getOntProperty(stmt.getPredicate().getURI());
 			
-			if (cls != null) {
-				ExtendedIterator<OntClass> superIt = cls.listSuperClasses(false);
+			if (prop != null) {	
+				ExtendedIterator<? extends OntProperty> superIt = prop.listSuperProperties(false);
+			
 				while (superIt.hasNext()) {
-					OntClass parentClass = superIt.next();
+					OntProperty parentProp = superIt.next();
 					
-					if (parentClass.isAnon()) continue;  
+					if (entailed(stmt.getSubject(),parentProp,stmt.getObject() )) continue;    // if the statement is still entailed 
+					                                                                           // don't remove it from the inference graph.
+                                                                                
+					Statement infStmt = ResourceFactory.createStatement(stmt.getSubject(), parentProp, stmt.getObject());
 					
-					if (entailedType(stmt.getSubject(),parentClass)) continue;          // if a type is still entailed without the
-					                                                                    // removed statement, then don't remove it
-					                                                                    // from the inferences
-					
-					Statement infStmt = ResourceFactory.createStatement(stmt.getSubject(), RDF.type, parentClass);
-						
 					inferenceModel.enterCriticalSection(Lock.WRITE);
 					try {
 						if (inferenceModel.contains(infStmt)) {
-							log.debug("Removing this inferred statement:  " + infStmt.toString() + " - " + infStmt.getSubject().toString() + " - " + infStmt.getPredicate().toString() + " - " + infStmt.getObject().toString());
+							log.debug("Removing inferred statement: " + infStmt.toString() + " - " + infStmt.getSubject().toString() + " - " + infStmt.getPredicate().toString() + " - " + infStmt.getObject().toString());
 							inferenceModel.remove(infStmt);
 						}
 					} finally {
 						inferenceModel.leaveCriticalSection();
-					}	
+					}
 				}
+				
 			} else {
-				log.debug("Didn't find target class (the object of the removed rdf:type statement) in the TBox: " + ((Resource)stmt.getObject()).getURI());
+				log.debug("Didn't find predicate of the removed statement in the TBox: " + stmt.getPredicate().getURI());
 			}
 		} finally {
 			tboxModel.leaveCriticalSection();
-		}
+		}	
 	}
 
-	public void retractProperties(Statement stmt) {
-		
-	}
-
-	//TODO:  should I move this out of this method to be just inline code, for performance reasons?
-	//TODO:  by the time removedStatement is called, can I assume that that statement will not be
-	//       found in the ABox when I query it? (the "contains" below).
-	//TODO:  will cls be in the list of child classes returned? If so, I have to check for it.
+	// Returns true if it is entailed by class subsumption that subject is
+	// of type cls; otherwise returns false.
 	public boolean entailedType(Resource subject, OntClass cls) {
-		
-		// Returns true if it is entailed by class subsumption that
-		// subject is of type cls.
 		
 		aboxModel.enterCriticalSection(Lock.READ);
 		
@@ -202,7 +227,37 @@ public class SimpleReasoner extends StatementListener {
 			while (iter.hasNext()) {
 				
 				OntClass childClass = iter.next();
+				
+				//TODO: do I need this?
+				if (childClass.equals(cls)) continue;
+				
 				Statement stmt = ResourceFactory.createStatement(subject, RDF.type, childClass);
+				if (aboxModel.contains(stmt)) return true;
+			}
+			
+			return false;
+		} finally {
+			aboxModel.leaveCriticalSection();
+		}	
+	}
+	
+	// Returns true if the statement is entailed by property subsumption 
+	public boolean entailed(Resource subject, OntProperty prop, RDFNode object) {
+		
+		aboxModel.enterCriticalSection(Lock.READ);
+		
+		try {
+			
+			ExtendedIterator<? extends OntProperty> iter = prop.listSubProperties(false);
+			
+			while (iter.hasNext()) {
+				
+				OntProperty childProp = iter.next();
+				
+				//TODO: do I need this?
+				if (childProp.equals(prop)) continue;
+				
+				Statement stmt = ResourceFactory.createStatement(subject, childProp, object);
 				if (aboxModel.contains(stmt)) return true;
 			}
 			
