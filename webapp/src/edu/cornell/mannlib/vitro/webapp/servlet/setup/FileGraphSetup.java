e48c9c228a716f626d91954a34a4997e9d87da4d
sjm222 <sjm222@440791e1-c2bf-4c1e-ad7c-caf3f8b0ebe8>
2010-11-11 16:40:17 +0000

    additional initial development for NIHVIVO-1311

--- webapp/src/edu/cornell/mannlib/vitro/webapp/servlet/setup/FileGraphSetup.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/servlet/setup/FileGraphSetup.java
@@ -6,7 +6,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -14,10 +17,13 @@ import javax.servlet.ServletContextListener;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.sdb.SDBFactory;
 import com.hp.hpl.jena.sdb.Store;
+import com.hp.hpl.jena.sdb.util.StoreUtils;
 
 import edu.cornell.mannlib.vitro.webapp.dao.jena.OntModelSelectorImpl;
 
@@ -38,16 +44,20 @@ public class FileGraphSetup implements ServletContextListener {
 			OntModelSelectorImpl baseOms = (OntModelSelectorImpl) sce.getServletContext().getAttribute("baseOms");
 			Store kbStore = (Store) sce.getServletContext().getAttribute("kbStore");
 			
+			// ABox files
 			Set<String> pathSet = sce.getServletContext().getResourcePaths(PATH_ROOT + ABOX);
-			cleanupDB(kbStore, pathSet, ABOX);
+			
+			cleanupDB(kbStore, pathToURI(pathSet, ABOX), ABOX);
 			
 			if (pathSet != null) {
  			   OntModel aboxBaseModel = baseOms.getABoxModel();
 			   readGraphs(sce, pathSet, kbStore, ABOX, aboxBaseModel);		
 			}
 			
+			// TBox files
 			pathSet = sce.getServletContext().getResourcePaths(PATH_ROOT + TBOX);
-			cleanupDB(kbStore, pathSet,TBOX);
+			
+			cleanupDB(kbStore, pathToURI(pathSet, TBOX),TBOX);
 			
 			if (pathSet != null) {
 			   OntModel tboxBaseModel = baseOms.getTBoxModel();
@@ -75,55 +85,55 @@ public class FileGraphSetup implements ServletContextListener {
 	 */
 	public void readGraphs(ServletContextEvent sce, Set<String> pathSet, Store kbStore, String type, OntModel baseModel) {
 			
-			int count = 0;
-			
-			// For each file graph in the target directory update or add that graph to
-			// the Jena SDB, and attach the graph as a submodel of the base model
-			for ( String p : pathSet ) {
+		int count = 0;
+		
+		// For each file graph in the target directory update or add that graph to
+		// the Jena SDB, and attach the graph as a submodel of the base model
+		for ( String p : pathSet ) {
 
-				count++; // note this will count the empty files too
-				File file = new File(sce.getServletContext().getRealPath(p));
-				
-				try {
-					FileInputStream fis = new FileInputStream( file );
-					try {
-						Model model = ModelFactory.createDefaultModel(); 
-						if ( p.endsWith(".n3") || p.endsWith(".N3") || p.endsWith(".ttl") || p.endsWith(".TTL") ) {
-							model.read( fis, null, "N3" );
-						} else if ( p.endsWith(".owl") || p.endsWith(".OWL") || p.endsWith(".rdf") || p.endsWith(".RDF") || p.endsWith(".xml") || p.endsWith(".XML") ) {
-							model.read( fis, null, "RDF/XML" );
-						} else {
-							log.warn("Ignoring " + type + " file graph " + p + " because the file extension is unrecognized.");
-						}
-						
-						if ( !model.isEmpty() ) {							
-							 baseModel.addSubModel(model);
-							 log.info("Attached file graph as " + type + " submodel " + p);
-						} 
-						
-						updateGraphInDB(kbStore, model, p);
-						
-					} catch (Exception ioe) {
-						//TODO: code had fis.close() - is that important?
-						log.error("Unable to process file graph " + p, ioe);
-						System.out.println("Unable to process file graph " + p);
-						ioe.printStackTrace();
-					}
-				} catch (FileNotFoundException fnfe) {
-					log.warn(p + " not found. Unable to process file graph" + 
-							((fnfe.getLocalizedMessage() != null) ? 
-							fnfe.getLocalizedMessage() : "") );
-				}
-			} // end - for
+			count++; // note this will count the empty files too
+			File file = new File(sce.getServletContext().getRealPath(p));
 			
-			System.out.println("Read " + count + " "  + type + " file graph" + ((count == 1) ? "" : "s") + " from " + PATH_ROOT + type);
+			try {
+				FileInputStream fis = new FileInputStream( file );
+				try {
+					Model model = ModelFactory.createDefaultModel(); 
+					if ( p.endsWith(".n3") || p.endsWith(".N3") || p.endsWith(".ttl") || p.endsWith(".TTL") ) {
+						model.read( fis, null, "N3" );
+					} else if ( p.endsWith(".owl") || p.endsWith(".OWL") || p.endsWith(".rdf") || p.endsWith(".RDF") || p.endsWith(".xml") || p.endsWith(".XML") ) {
+						model.read( fis, null, "RDF/XML" );
+					} else {
+						log.warn("Ignoring " + type + " file graph " + p + " because the file extension is unrecognized.");
+					}
+					
+					if ( !model.isEmpty() ) {							
+						 baseModel.addSubModel(model);
+						 log.info("Attached file graph as " + type + " submodel " + p);
+					} 
+					
+					updateGraphInDB(kbStore, model, type, p);
+					
+				} catch (Exception ioe) {
+					//TODO: code had fis.close() - is that important?
+					log.error("Unable to process file graph " + p, ioe);
+					System.out.println("Unable to process file graph " + p);
+					ioe.printStackTrace();
+				}
+			} catch (FileNotFoundException fnfe) {
+				log.warn(p + " not found. Unable to process file graph" + 
+						((fnfe.getLocalizedMessage() != null) ? 
+						fnfe.getLocalizedMessage() : "") );
+			}
+		} // end - for
+		
+		System.out.println("Read " + count + " "  + type + " file graph" + ((count == 1) ? "" : "s") + " from " + PATH_ROOT + type);
 		
 		return;
 	}
 	
 	
 	/*
-	 * If a graph with the given doesn't exist in the DB then add it.
+	 * If a graph with the given name doesn't exist in the DB then add it.
 	 * 
 	 * Otherwise, if a graph with the given name is in the DB and is isomorphic with
 	 * the graph that was read from the files system, then do nothing.
@@ -132,32 +142,83 @@ public class FileGraphSetup implements ServletContextListener {
 	 * the graph that was read from the file system then replace the graph
 	 * in the DB with the one read from the file system.
 	 */
-	public void updateGraphInDB(Store kbStore, Model graph, String filegraph) {
+	public void updateGraphInDB(Store kbStore, Model fileModel, String type, String path) {
 			
+		String graphURI = pathToURI(path,type);
+		Model dbModel = SDBFactory.connectNamedModel(kbStore, graphURI);
+		
+		if (dbModel.isEmpty() ) {
+			dbModel.add(fileModel);
+		} else if (!dbModel.isIsomorphicWith(fileModel)) {
+		    dbModel.removeAll();
+		    dbModel.add(fileModel);
+		}
 		
 		return;
 	}
 	
 	/*
-	 * Deletes any file graphs of a given type (ABox or TBox) from the DB that
-	 * are no longer present in the file system.
+	 * Deletes any file graphs that are  no longer present in the file system
+	 * from the DB.
 	 */
-	public void cleanupDB(Store kbStore, Set<String> pathSet, String type) {
+	public void cleanupDB(Store kbStore, Set<String> nameSet, String type) {
 		
-		int count = 0; 
-		
-		if (pathSet == null) {
-			//create an empty set so the same logic below can be used in all cases
-			pathSet = new HashSet<String>();
-		}
-
-		
-		
-
-		
+		Pattern graphURIPat = Pattern.compile(URI_ROOT + type + "/(.*?)");   
+		 
+	    Iterator<Node> iter = StoreUtils.storeGraphNames(kbStore);	
+	    
+	    while (iter.hasNext()) {
+	    	Node node = iter.next();
+            Matcher matcher = graphURIPat.matcher(node.getURI());
+		    
+            if (matcher.find()) {
+		    	String graphName = matcher.group(1);
+		    	
+		    	if (!nameSet.contains(graphName)) {
+		    		 Model model = SDBFactory.connectNamedModel(kbStore, node.getURI());
+		    		 model.removeAll(); // delete the graph from the DB
+					 log.info("Removed " + type + " file graph " + node.getURI() + " from the DB store because the file no longer exists in the file system");
+		    	}
+            }            
+	    }
+	
 		return;
 	}
 		
+	/*
+	 * Takes a set of path names for file graphs and returns a set containing
+	 * a graph uri for each path name in the input set. If pathSet is null
+	 * returns an empty set.
+	 */
+	public Set<String> pathToURI (Set<String> pathSet, String type) {
+		
+		HashSet<String> uriSet = new HashSet<String>();
+		
+	    if (pathSet != null) {
+			for ( String path : pathSet ) {
+				uriSet.add(pathToURI(path,type));
+			}
+	    }
+		
+		return uriSet;
+	}
+
+	/*
+	 * Takes a path name for a file graph and returns a uri for the graph
+	 */
+	public String pathToURI (String path, String type) {
+		
+		String uri = null;
+		
+	    if (path != null) {
+			String[] pathParts = path.split("/");
+			String fileName = pathParts[pathParts.length-1].substring(0, pathParts[pathParts.length-1].lastIndexOf('.'));
+			uri = URI_ROOT + type + "/" + fileName; 
+	    }
+		
+		return uri;
+	}
+
 	public void contextDestroyed( ServletContextEvent sce ) {
 		// nothing to do
 	}
