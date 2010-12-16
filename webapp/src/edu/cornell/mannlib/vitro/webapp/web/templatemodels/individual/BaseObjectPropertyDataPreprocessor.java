f56243ac7a3409a4afdce331b508ddb9f401a7f9
jeb228 <jeb228@440791e1-c2bf-4c1e-ad7c-caf3f8b0ebe8>
2010-12-16 20:59:18 +0000

    Add license tags where required.

--- webapp/src/edu/cornell/mannlib/vitro/webapp/web/templatemodels/individual/BaseObjectPropertyDataPreprocessor.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/web/templatemodels/individual/BaseObjectPropertyDataPreprocessor.java
@@ -1,3 +1,4 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
 package edu.cornell.mannlib.vitro.webapp.web.templatemodels.individual;
 
 import java.util.List;
