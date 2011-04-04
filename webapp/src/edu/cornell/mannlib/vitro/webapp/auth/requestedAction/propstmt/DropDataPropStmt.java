c0beb997201a6c9975d2ea73c61cca1130b359c6
j2blake <j2blake@440791e1-c2bf-4c1e-ad7c-caf3f8b0ebe8>
2011-04-04 20:11:36 +0000

    remove obsolete commented code.

--- webapp/src/edu/cornell/mannlib/vitro/webapp/auth/requestedAction/propstmt/DropDataPropStmt.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/auth/requestedAction/propstmt/DropDataPropStmt.java
@@ -34,52 +34,5 @@ public class DropDataPropStmt extends AbstractDataPropertyAction {
     public String lang(){ return dataPropStmt.getLanguage(); }
     public String datatype(){return dataPropStmt.getDatatypeURI(); }
     
-    /*
-     * TODO: needs to be fixed to work with lang/datatype literals
-     */
-    
-    /*
-    
-    protected String resourceUri;
-    protected String dataPropUri;
-    protected String value;
-
-    //TODO: needs to be fixed to work with lang/datatype literals
-    public DropDataPropStmt(String resourceUri, String dataPropUri, String value) {
-        super();
-        this.resourceUri = resourceUri;
-        this.dataPropUri = dataPropUri;
-        this.value = value;
-    }
-
-    public String getDataPropUri() {
-        return dataPropUri;
-    }
-
-    public void setDataPropUri(String dataPropUri) {
-        this.dataPropUri = dataPropUri;
-    }
-
-    public String getResourceUri() {
-        return resourceUri;
-    }
-
-    public void setResourceUri(String resourceUri) {
-        this.resourceUri = resourceUri;
-    }
-//TODO: needs to be fixed to work with lang/datatype literals
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    public String getURI() {
-        return RequestActionConstants.actionNamespace + this.getClass().getName();
-    }
-    public PolicyDecision accept(PolicyIface policy, IdentifierBundle ids){
-        return policy.visit(ids,this);
-    } */
+    // TODO: needs to be fixed to work with lang/datatype literals
 }
