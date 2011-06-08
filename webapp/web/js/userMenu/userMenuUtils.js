09c2f33633fd5f4d992858d06addee8c1cbfdda4
manolobevia <manolobevia@440791e1-c2bf-4c1e-ad7c-caf3f8b0ebe8>
2011-06-08 19:57:35 +0000

    NIHVIVO-2588. Forgot to commit js file to handle pull down menu for user menu

---
+++ webapp/web/js/userMenu/userMenuUtils.js
@@ -0,0 +1,19 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
+
+$(function(){
+
+    $("ul.dropdown li").hover(function(){
+    
+        $(this).addClass("hover");
+        $('ul:first',this).css('visibility', 'visible');
+    
+    }, function(){
+    
+        $(this).removeClass("hover");
+        $('ul:first',this).css('visibility', 'hidden');
+    
+    });
+    
+    $("ul.dropdown li ul li:has(ul)").find("a:first").append(" &raquo; ");
+
+});
