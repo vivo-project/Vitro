2bbaf97408f9f8469b835769f7bc1e6ded7014ba
runeliza <runeliza@440791e1-c2bf-4c1e-ad7c-caf3f8b0ebe8>
2011-06-15 18:33:23 +0000

    Some cleanup of JSONReconcileServlet. Added new servlet GrefinePropertyListServlet for servicing Google Refine's "Add columns from VIVO" requests.

--- webapp/src/edu/cornell/mannlib/vitro/webapp/controller/GrefinePropertyListServlet.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/GrefinePropertyListServlet.java
@@ -4,8 +4,12 @@ package edu.cornell.mannlib.vitro.webapp.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -30,14 +34,27 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.WildcardQuery;
-import org.apache.lucene.util.Version;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.hp.hpl.jena.vocabulary.OWL;
+
+import edu.cornell.mannlib.vitro.webapp.beans.DataProperty;
+import edu.cornell.mannlib.vitro.webapp.beans.Datatype;
 import edu.cornell.mannlib.vitro.webapp.beans.Individual;
+import edu.cornell.mannlib.vitro.webapp.beans.Ontology;
+import edu.cornell.mannlib.vitro.webapp.beans.PropertyGroup;
+import edu.cornell.mannlib.vitro.webapp.beans.VClass;
+import edu.cornell.mannlib.vitro.webapp.beans.VClassGroup;
 import edu.cornell.mannlib.vitro.webapp.config.ConfigurationProperties;
+import edu.cornell.mannlib.vitro.webapp.dao.DataPropertyDao;
+import edu.cornell.mannlib.vitro.webapp.dao.DatatypeDao;
 import edu.cornell.mannlib.vitro.webapp.dao.IndividualDao;
+import edu.cornell.mannlib.vitro.webapp.dao.OntologyDao;
+import edu.cornell.mannlib.vitro.webapp.dao.VClassDao;
+import edu.cornell.mannlib.vitro.webapp.dao.VClassGroupDao;
+import edu.cornell.mannlib.vitro.webapp.dao.WebappDaoFactory;
 import edu.cornell.mannlib.vitro.webapp.search.SearchException;
 import edu.cornell.mannlib.vitro.webapp.search.lucene.Entity2LuceneDoc;
 import edu.cornell.mannlib.vitro.webapp.search.lucene.Entity2LuceneDoc.VitroLuceneTermNames;
@@ -45,17 +62,20 @@ import edu.cornell.mannlib.vitro.webapp.search.lucene.LuceneIndexFactory;
 import edu.cornell.mannlib.vitro.webapp.search.lucene.LuceneSetup;
 
 /**
- * This servlet is for servicing JSON requests from Google Refine's
- * Reconciliation Service.
+ * This servlet is for servicing Google Refine's
+ * "Add columns from VIVO" requests.
  * 
  * @author Eliza Chan (elc2013@med.cornell.edu)
  * 
  */
-public class JSONReconcileServlet extends VitroHttpServlet {
+public class GrefinePropertyListServlet extends VitroHttpServlet {
 
+	private int MAXDEPTH = 7;
+	private int NUM_COLS = 9;
 	private static String QUERY_PARAMETER_NAME = "term";
 	public static final int MAX_QUERY_LENGTH = 500;
-	private static final Log log = LogFactory.getLog(JSONReconcileServlet.class.getName());
+	private static final Log log = LogFactory.getLog(GrefinePropertyListServlet.class.getName());
+	
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
@@ -70,49 +90,283 @@ public class JSONReconcileServlet extends VitroHttpServlet {
 		super.doGet(req, resp);
 		resp.setContentType("application/json");
 		VitroRequest vreq = new VitroRequest(req);
-//		System.out.println("vreq");
-//		System.out.println(vreq.getWebappDaoFactory());
   
 		try {
-			if (vreq.getParameter("query") != null
-					|| vreq.getParameter("queries") != null) {
-				JSONObject qJson = getResult(vreq, req, resp);
-//				System.out.println("result: " + qJson.toString());
-				String responseStr = (vreq.getParameter("callback") == null) ? qJson
-						.toString() : vreq.getParameter("callback") + "("
-						+ qJson.toString() + ")";
-				ServletOutputStream out = resp.getOutputStream();
-				out.print(responseStr);
-			} else { // metadata
-				String defaultNamespace = null;
-				//String defaultTypeList = null;
-				String serverName = null;
-				int serverPort = req.getServerPort();
 
-				if (vreq.getWebappDaoFactory() != null) {
-					defaultNamespace = vreq.getWebappDaoFactory().getDefaultNamespace();	
-				}
-				String defaultTypeList = 
-					ConfigurationProperties.getBean(getServletContext()).getProperty("Vitro.reconcile.defaultTypeList");
-
-				//defaultTypeList = 
-				//	"http://vivoweb.org/ontology/core#Course, Course; " + 
-				//	"http://vivoweb.org/ontology/core#Grant, Grant; " + 
-				//	"http://aims.fao.org/aos/geopolitical.owl, Location; " + 
-				//	"http://xmlns.com/foaf/0.1/Organization, Organization; " + 
-				//	"http://xmlns.com/foaf/0.1/Person, Person; " + 
-				//	"http://purl.org/ontology/bibo/Article, Publication";
-				serverName = req.getServerName();
-				JSONObject metaJson = getMetadata(req, resp, defaultNamespace, defaultTypeList, serverName, serverPort);
 				String callbackStr = (vreq.getParameter("callback") == null) ? ""
 						: vreq.getParameter("callback");
 				ServletOutputStream out = resp.getOutputStream();
-				out.print(callbackStr + "(" + metaJson.toString() + ")");
-			}
+
+		
+				// *******
+				// methodology adopted from DatatypePropertiesListingController and ClassHierarchyListingController
+				// *******
+				VClassDao vcDao = vreq.getFullWebappDaoFactory().getVClassDao();
+				DataPropertyDao dao = vreq.getFullWebappDaoFactory().getDataPropertyDao();
+				String topUri = vreq.getParameter("type");
+				VClass topClass = vcDao.getVClassByURI(topUri);
+				HashSet<String> propURIs = new HashSet<String>();
+				HashMap<VClass, List<DataProperty>> classPropertiesMap = 
+					populateClassPropertiesMap(vcDao, dao, topUri, propURIs);
+				
+
+				// Construct json String
+				JSONObject completeJson = new JSONObject();
+				JSONArray propertiesJsonArr = new JSONArray();
+				if (classPropertiesMap.size() > 0) {
+					for (Iterator<VClass> iter = classPropertiesMap.keySet().iterator(); iter.hasNext();) { // add results to schema
+						VClass vc = (VClass) iter.next();
+						System.out.println("vc uri: " + vc.getURI());
+						System.out.println("vc name: " + vc.getName());	
+
+						ArrayList<DataProperty> vcProps = (ArrayList<DataProperty>)classPropertiesMap.get(vc);
+						for (DataProperty prop: vcProps) {
+							String nameStr = prop.getPublicName()==null ? prop.getName()==null ? null : prop.getName() : prop.getPublicName();
+								System.out.println("--- uri: " + prop.getURI());
+								System.out.println("--- name: " + nameStr);
+					        	// top level
+								JSONObject propertiesItemJson = new JSONObject();
+								JSONObject rootSchemaJson = new JSONObject();
+								rootSchemaJson.put("id", vc.getURI());
+								rootSchemaJson.put("name", vc.getName());
+								rootSchemaJson.put("alias", new JSONArray());
+								propertiesItemJson.put("schema", rootSchemaJson);
+								// second level
+								propertiesItemJson.put("id", prop.getURI());
+								propertiesItemJson.put("name", nameStr);
+								propertiesItemJson.put("alias", new JSONArray());
+
+								JSONObject expectsJson = new JSONObject();
+								expectsJson.put("id", prop.getURI());
+								expectsJson.put("name", nameStr);
+								expectsJson.put("alias", new JSONArray());
+								propertiesItemJson.put("expects", expectsJson);
+								
+								propertiesJsonArr.put(propertiesItemJson);
+						}
+					}
+				}
+	            
+
+				// get data properties from subclasses
+				List<VClass> lvl2Classes = new ArrayList<VClass>();
+				List roots = null;
+				String requestType = vreq.getParameter("type");
+				if (requestType != null) {
+			       	roots = new LinkedList<VClass>();
+		        	roots.add(vcDao.getVClassByURI(requestType));
+				}
+
+				if (roots != null) {
+					String ontologyUri = null;
+					Collections.sort(roots);
+			        Iterator rootIt = roots.iterator();
+			        if (rootIt.hasNext()) {
+			            while (rootIt.hasNext()) {
+			                VClass root = (VClass) rootIt.next();
+				            if (root != null) {
+				            	List<VClass> lvl2ChildClasses = new ArrayList<VClass>();
+				                addChildren(vcDao, vreq.getFullWebappDaoFactory(), root, lvl2ChildClasses, 0, ontologyUri);
+				                lvl2Classes.addAll(lvl2ChildClasses);
+			                }
+			            }
+			        }
+				}
+				
+
+				for (VClass lvl2Class: lvl2Classes) {
+					HashMap<VClass, List<DataProperty>> lvl2ClassPropertiesMap = 
+						populateClassPropertiesMap(vcDao, dao, lvl2Class.getURI(), propURIs);	
+					if (lvl2ClassPropertiesMap.size() > 0) {
+						for (Iterator<VClass> iter = lvl2ClassPropertiesMap.keySet().iterator(); iter.hasNext();) { // add results to schema
+							VClass vc = (VClass) iter.next();
+							ArrayList<DataProperty> vcProps = (ArrayList<DataProperty>)lvl2ClassPropertiesMap.get(vc);
+							for (DataProperty prop: vcProps) {
+								String nameStr = prop.getPublicName()==null ? prop.getName()==null ? null : prop.getName() : prop.getPublicName();
+						        	// top level
+									JSONObject propertiesItemJson = new JSONObject();
+									
+									JSONObject rootSchemaJson = new JSONObject();
+									rootSchemaJson.put("id", topClass.getURI());
+									rootSchemaJson.put("name", topClass.getName());
+									rootSchemaJson.put("alias", new JSONArray());
+									propertiesItemJson.put("schema", rootSchemaJson);
+
+									// second level
+									propertiesItemJson.put("id", vc.getURI());
+									propertiesItemJson.put("name", vc.getName());
+									propertiesItemJson.put("alias", new JSONArray());
+																		
+									propertiesItemJson.put("id2", prop.getURI());
+									propertiesItemJson.put("name2", nameStr);
+									propertiesItemJson.put("alias2", new JSONArray());
+									
+									JSONObject expectsJson = new JSONObject();
+									expectsJson.put("id", prop.getURI());
+									expectsJson.put("name", nameStr);
+									expectsJson.put("alias", new JSONArray());
+									propertiesItemJson.put("expects", expectsJson);
+									
+									propertiesJsonArr.put(propertiesItemJson);
+							}
+						}
+						
+					}
+				}
+
+				completeJson.put("properties", propertiesJsonArr);
+				out.print(callbackStr + "(" + completeJson.toString() + ")");
+				
+
+
 		} catch (Exception ex) {
 			log.warn(ex, ex);
 		}
 	}
+	
+
+	private HashMap<VClass, List<DataProperty>> populateClassPropertiesMap (
+			VClassDao vcDao, 
+			DataPropertyDao dao, 
+			String uri, 
+			HashSet<String> propURIs) {
+
+		HashMap<VClass, List<DataProperty>> classPropertiesMap = new HashMap<VClass, List<DataProperty>>();
+		List<DataProperty> props = new ArrayList<DataProperty>();
+		VClass topVc = vcDao.getVClassByURI(uri);
+	        Collection <DataProperty> dataProps = dao.getDataPropertiesForVClass(uri);
+	        Iterator<DataProperty> dataPropIt = dataProps.iterator();
+	        while (dataPropIt.hasNext()) {
+	            DataProperty dp = dataPropIt.next();
+	            if (!(propURIs.contains(dp.getURI()))) {
+	                propURIs.add(dp.getURI());
+	                DataProperty prop = dao.getDataPropertyByURI(dp.getURI());
+	                if (prop != null) {
+	                    props.add(prop);
+	                }
+	            }
+	        }
+        
+    	
+        if (props.size() > 0) {
+
+        	Collections.sort(props);
+        	for (DataProperty prop: props) {
+        		String nameStr = prop.getPublicName()==null ? prop.getName()==null ? null : prop.getName() : prop.getPublicName();
+				if (nameStr != null) {
+            		if (prop.getDomainClassURI() != null) {
+            			VClass vc = vcDao.getVClassByURI(prop.getDomainClassURI());
+            			if (classPropertiesMap.get(vc) != null) {
+            				ArrayList<DataProperty> existingList = (ArrayList<DataProperty>)classPropertiesMap.get(vc);
+            				existingList.add(prop);
+            			} else {
+            				ArrayList<DataProperty> newList = new ArrayList<DataProperty>();
+            				newList.add(prop);
+            				classPropertiesMap.put(vc, newList);
+            			}
+
+            		} else { // some properties have no domain, belong to top vc by default
+            			if (classPropertiesMap.get(topVc) != null) {
+            				ArrayList<DataProperty> existingList = (ArrayList<DataProperty>)classPropertiesMap.get(topVc);
+            				existingList.add(prop);
+            			} else {
+            				ArrayList<DataProperty> newList = new ArrayList<DataProperty>();
+            				newList.add(prop);
+            				classPropertiesMap.put(topVc, newList);
+            			}
+            		}							
+				}
+        	}
+        }
+        return classPropertiesMap;
+	}
+
+	    private void addChildren(VClassDao vcDao, WebappDaoFactory wadf, VClass parent, List<VClass> list, int position, String ontologyUri) {
+	    	List<VClass> rowElts = addVClassDataToResultsList(wadf, parent, position, ontologyUri);
+	    	int childShift = (rowElts.size() > 0) ? 1 : 0;  // if addVClassDataToResultsList filtered out the result, don't shift the children over 
+	    	list.addAll(rowElts);
+	        List childURIstrs = vcDao.getSubClassURIs(parent.getURI());
+	        if ((childURIstrs.size()>0) && position<MAXDEPTH) {
+	            List childClasses = new ArrayList();
+	            Iterator childURIstrIt = childURIstrs.iterator();
+	            while (childURIstrIt.hasNext()) {
+	                String URIstr = (String) childURIstrIt.next();
+	                try {
+		                VClass child = (VClass) vcDao.getVClassByURI(URIstr);
+		                if (!child.getURI().equals(OWL.Nothing.getURI())) {
+		                	childClasses.add(child);
+		                }
+	                } catch (Exception e) {}
+	            }
+	            Collections.sort(childClasses);
+	            Iterator childClassIt = childClasses.iterator();
+	            while (childClassIt.hasNext()) {
+	                VClass child = (VClass) childClassIt.next();
+	                addChildren(vcDao, wadf, child, list, position + childShift, ontologyUri);
+	            }
+
+	        }
+
+	    }
+
+	    private List<VClass> addVClassDataToResultsList(WebappDaoFactory wadf, VClass vcw, int position, String ontologyUri) {
+	    	List<VClass> results = new ArrayList<VClass>();
+	        if (ontologyUri == null || ( (vcw.getNamespace()!=null) && (vcw.getNamespace().equals(ontologyUri)) ) ) {
+	        	results.add(vcw);
+
+/*
+	        	for (int i=0; i<position; i++) {
+	                results.add("@@entities");
+	            }
+	            if (position==0)
+	                results.add("XX"); // column 1
+	            Integer numCols = (NUM_COLS-1)-position;
+
+	            try {
+	                numCols = addColToResults(((vcw.getLocalNameWithPrefix() == null) ? "" : "<a href=\"vclassEdit?uri="+URLEncoder.encode(vcw.getURI(),"UTF-8")+"\">"+vcw.getLocalNameWithPrefix()+"</a>"), results, numCols);
+	            } catch (Exception e) {
+	                numCols = addColToResults(((vcw.getLocalNameWithPrefix() == null) ? "" : vcw.getLocalNameWithPrefix()), results, numCols); // column 2
+	            }
+	            numCols = addColToResults(((vcw.getShortDef() == null) ? "" : vcw.getShortDef()), results, numCols); // column 3
+	            numCols = addColToResults(((vcw.getExample() == null) ? "" : vcw.getExample()), results, numCols); // column 4
+	            
+	            // Get group name if it exists
+	            VClassGroupDao groupDao= wadf.getVClassGroupDao();
+	            String groupURI = vcw.getGroupURI();
+	            String groupName = null;
+	            VClassGroup classGroup = null;
+	            if(groupURI != null) { 
+	            	classGroup = groupDao.getGroupByURI(groupURI);
+	            	if (classGroup != null) {
+	            		groupName = classGroup.getPublicName();
+	            	}
+	            }
+	            numCols = addColToResults(((groupName == null) ? "" : groupName), results, numCols); // column 5
+	      
+	            // Get ontology name
+				String ontName = null;
+				try {
+	            	OntologyDao ontDao = wadf.getOntologyDao();
+	            	Ontology ont = ontDao.getOntologyByURI(vcw.getNamespace());
+	            	ontName = ont.getName();
+				} catch (Exception e) {}
+	            numCols = addColToResults(((ontName == null) ? "" : ontName), results, numCols); // column 6
+	            
+	            numCols = addColToResults(vcw.getHiddenFromDisplayBelowRoleLevel()  == null ? "unspecified" : vcw.getHiddenFromDisplayBelowRoleLevel().getShorthand(), results, numCols); // column 7
+	            numCols = addColToResults(vcw.getProhibitedFromUpdateBelowRoleLevel() == null ? "unspecified" : vcw.getProhibitedFromUpdateBelowRoleLevel().getShorthand(), results, numCols); // column 8
+
+	            results.add("XX"); // column 9
+*/
+	        }
+	        return results;
+	    }
+
+	    private Integer addColToResults (String value, List results, Integer colIndex) {
+	        if (colIndex>0) {
+	            results.add(value);
+	        }
+	        return colIndex-1;
+	    }
 
 	protected JSONObject getResult(VitroRequest vreq, HttpServletRequest req,
 			HttpServletResponse resp) throws ServletException {
@@ -133,8 +387,6 @@ public class JSONReconcileServlet extends VitroHttpServlet {
 			// "q2":{"query":"Dina","type":"http://xmlns.com/foaf/0.1/Person","type_strict":"should"}}
 			String qStr = (String) qObj;
 			queries.add(qStr);
-//			System.out.println();
-//			System.out.println("query: " + qStr + "\n");
 		}
 
 		try {
@@ -302,7 +554,8 @@ public class JSONReconcileServlet extends VitroHttpServlet {
 							Individual ind = iDao.getIndividualByURI(uri);
 							if (ind != null) {
 								String name = ind.getName();
-								String modUri = uri.replace("#", "%23"); // encodes # to %23
+								// encode # to %23
+								String modUri = uri.replace("#", "%23");
 								resultJson.put("id", modUri);
 								resultJson.put("name", name);
 							}
