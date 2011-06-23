1aef7ddcb756d3d539f39afdf235d3213c352866
j2blake <j2blake@440791e1-c2bf-4c1e-ad7c-caf3f8b0ebe8>
2011-06-23 16:17:46 +0000

    NIHVIVO-2720 Add methods to PolicyHelper to check authorization for adding or dropping Jena statements, or entire Jena models.

--- webapp/src/edu/cornell/mannlib/vitro/webapp/auth/requestedAction/propstmt/AddDataPropStmt.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/auth/requestedAction/propstmt/AddDataPropStmt.java
@@ -2,6 +2,8 @@
 
 package edu.cornell.mannlib.vitro.webapp.auth.requestedAction.propstmt;
 
+import com.hp.hpl.jena.rdf.model.Literal;
+
 /** Should we allow the user to add this DataPropertyStatement? */
 public class AddDataPropStmt extends AbstractDataPropertyAction {
     protected String data;
@@ -15,6 +17,13 @@ public class AddDataPropStmt extends AbstractDataPropertyAction {
         this.lang = lang;
     }
 
+    public AddDataPropStmt(String subjectUri, String predicateUri, Literal literal) {
+    	super(subjectUri, predicateUri);
+    	this.data= literal.getValue().toString();
+    	this.dataType = literal.getDatatypeURI();
+    	this.lang = literal.getLanguage();
+    }
+    
     public String getData() {
         return data;
     }
--- webapp/src/edu/cornell/mannlib/vitro/webapp/auth/requestedAction/propstmt/DropDataPropStmt.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/auth/requestedAction/propstmt/DropDataPropStmt.java
@@ -2,6 +2,8 @@
 
 package edu.cornell.mannlib.vitro.webapp.auth.requestedAction.propstmt;
 
+import com.hp.hpl.jena.rdf.model.Literal;
+
 import edu.cornell.mannlib.vitro.webapp.beans.DataPropertyStatement;
 import edu.cornell.mannlib.vitro.webapp.beans.DataPropertyStatementImpl;
 
@@ -23,7 +25,15 @@ public class DropDataPropStmt extends AbstractDataPropertyAction {
         dataPropStmt.setData(data);        
     }
     
-    public String data(){ return dataPropStmt.getData(); }
+	public DropDataPropStmt(String subjectUri, String predicateUri, Literal data) {
+    	super(subjectUri, predicateUri);
+        dataPropStmt = new DataPropertyStatementImpl();
+        dataPropStmt.setIndividualURI(subjectUri);
+        dataPropStmt.setDatapropURI(predicateUri);
+        dataPropStmt.setData(data.getValue().toString());        
+	}
+
+	public String data(){ return dataPropStmt.getData(); }
     public String lang(){ return dataPropStmt.getLanguage(); }
     public String datatype(){return dataPropStmt.getDatatypeURI(); }
     
