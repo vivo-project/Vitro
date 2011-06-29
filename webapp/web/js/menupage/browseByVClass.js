239256187d5c90b4507a1ede660ff5d00243c1bb
ryounes <ryounes@440791e1-c2bf-4c1e-ad7c-caf3f8b0ebe8>
2011-06-29 14:45:58 +0000

    NIHVIVO-2787 Change title selections when selecting a vclass for which no individuals are returned

--- webapp/web/js/menupage/browseByVClass.js
+++ webapp/web/js/menupage/browseByVClass.js
@@ -138,14 +138,15 @@ var browseByVClass = {
                 if ( results.pages.length ) {
                     pages = results.pages;
                     browseByVClass.pagination(pages, page);
-                }
-                
-                $('h3.selected-class').text(results.vclass.name);
-                
-                // Set selected class, alpha and page
-                browseByVClass.selectedVClass(results.vclass.URI);
-                browseByVClass.selectedAlpha(alpha);
+                }                
+
             }
+            
+            // Set selected class, alpha and page
+            // Do this whether or not there are any results
+            $('h3.selected-class').text(results.vclass.name);
+            browseByVClass.selectedVClass(results.vclass.URI);
+            browseByVClass.selectedAlpha(alpha);
         });
     },
     
