c89953d73f6276251f783cf91bd82fbab732f082
j2blake <j2blake@440791e1-c2bf-4c1e-ad7c-caf3f8b0ebe8>
2012-06-29 21:28:06 +0000

    Disable the "Faculty Mentor" gadget. Restrict the "RDR Test Gadget" to edit mode on the profile page.

--- opensocial/shindig_example_gadgets.sql
+++ opensocial/shindig_example_gadgets.sql
@@ -6,10 +6,10 @@ DELETE FROM `orng_apps`;
 INSERT INTO `orng_apps` (`appid`, `name`, `url`, `PersonFilterID`, `enabled`, `channels`) VALUES
 (100, 'Google Search', 'http://dev-profiles.ucsf.edu/orng/GoogleSearch.xml', NULL, 1, NULL),
 (101, 'Featured Presentations', 'http://dev-profiles.ucsf.edu/orng/SlideShare.xml', NULL, 1, NULL),
-(102, 'Faculty Mentor', 'http://dev-profiles.ucsf.edu/orng/Mentor.xml', NULL, 1, NULL),
+(102, 'Faculty Mentor', 'http://dev-profiles.ucsf.edu/orng/Mentor.xml', NULL, 0, NULL),
 (103, 'Websites', 'http://dev-profiles.ucsf.edu/orng/Links.xml', NULL, 1, NULL),
 (104, 'Profile List', 'http://dev-profiles.ucsf.edu/orng/ProfileListTool.xml', NULL, 1, 'JSONPersonIds'),
-(106, 'RDF Test Gadget', 'http://dev-profiles.ucsf.edu/orng/RDFTest.xml', NULL, 0, NULL);
+(106, 'RDF Test Gadget', 'http://dev-profiles.ucsf.edu/orng/RDFTest.xml', NULL, 1, NULL);
 
 DELETE FROM `orng_app_views`;
 
@@ -25,4 +25,5 @@ INSERT INTO `orng_app_views` (`appid`, `viewer_req`, `owner_req`, `page`, `view`
 (104, 'U', NULL, 'gadgetDetails', 'canvas', 700, 700, 0, 'gadgets-detail', NULL),
 (104, 'U', NULL, 'SimilarPeople.aspx', 'small', 160, 160, 0, 'gadgets-tools', NULL),
 (104, 'U', NULL, 'individual', 'small', 290, 290, 0, 'gadgets-view', NULL),
-(104, 'U', NULL, 'CoAuthors.aspx', 'small', 160, 160, 0, 'gadgets-tools', NULL);
+(104, 'U', NULL, 'CoAuthors.aspx', 'small', 160, 160, 0, 'gadgets-tools', NULL),
+(106, NULL, NULL, 'individual-EDIT-MODE', 'home', 700, 700, 1, 'gadgets-edit', NULL);
