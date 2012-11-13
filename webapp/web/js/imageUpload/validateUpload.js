d93068bdfa4ec78d318c0666c876db54740f3fcf
j2blake <jeb228@cornell.edu>
2012-11-13 17:19:45 -0500

    validateUpload.js is no longer used in Vitro, VIVO or vivoCornell

--- webapp/web/js/imageUpload/validateUpload.js
+++
@@ -1,9 +0,0 @@
-/* $This file is distributed under the terms of the license in /doc/license.txt$ */
-
-function validate_upload_file(form_passed){
-
-    if (form_passed.datafile.value == "") {
-        alert ("Please browse and select a photo");
-        return false;
-    }
-}
