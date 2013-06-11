f3acf5afe6a40bef231737dfcd732139cb4288cb
tworrall <tlw72@cornell.edu>
2013-06-11 13:35:19 -0400

    VIVO-65. for property groups, edit form was incorrect

--- webapp/src/edu/cornell/mannlib/vitro/webapp/controller/freemarker/ListPropertyGroupsController.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/freemarker/ListPropertyGroupsController.java
@@ -73,7 +73,7 @@ public class ListPropertyGroupsController extends FreemarkerHttpServlet {
                         publicName = publicName.replace("\"","\\\"");
                         publicName = publicName.replace("\'","\\\'");
                         try {
-                            json += "{ \"name\": \"<a href='./editForm?uri="+URLEncoder.encode(pg.getURI(),"UTF-8")+"&amp;controller=Classgroup'>" + publicName + "</a>\", ";
+                            json += "{ \"name\": \"<a href='./editForm?uri="+URLEncoder.encode(pg.getURI(),"UTF-8")+"&amp;controller=PropertyGroup'>" + publicName + "</a>\", ";
                         } catch (Exception e) {
                             json += "{ \"name\": \"" + publicName + "\", ";
                         }
