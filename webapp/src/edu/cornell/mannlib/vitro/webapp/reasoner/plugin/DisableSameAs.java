535500dd410d2ff20b32362406835b53591fd28d
Brian Caruso <bdc34@cornell.edu>
2013-08-29 15:08:45 -0400

    fixing compile problem. issue-vivo-110

--- webapp/src/edu/cornell/mannlib/vitro/webapp/reasoner/plugin/DisableSameAs.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/reasoner/plugin/DisableSameAs.java
@@ -5,6 +5,10 @@ package edu.cornell.mannlib.vitro.webapp.reasoner.plugin;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Statement;
+
 import edu.cornell.mannlib.vitro.webapp.reasoner.ReasonerPlugin;
 import edu.cornell.mannlib.vitro.webapp.reasoner.SimpleReasoner;
 
