b20445b23300dc3886edb24c5d1b63bc31e501c0
Jim Blake <jeb228@cornell.edu>
2014-04-18 09:52:21 -0400

    VIVO-731 Obj2DocIface is never used.

--- webapp/src/edu/cornell/mannlib/vitro/webapp/search/beans/IndexerIface.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/search/beans/IndexerIface.java
@@ -2,11 +2,8 @@
 
 package edu.cornell.mannlib.vitro.webapp.search.beans;
 
-import java.util.List;
-
 import edu.cornell.mannlib.vitro.webapp.beans.Individual;
 import edu.cornell.mannlib.vitro.webapp.search.IndexingException;
-import edu.cornell.mannlib.vitro.webapp.search.docbuilder.Obj2DocIface;
 
 /**
  * IntexerIface is for objects that will be used by the IndexBuilder.  The
