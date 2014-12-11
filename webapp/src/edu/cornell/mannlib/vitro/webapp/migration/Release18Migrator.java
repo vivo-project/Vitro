73b76a6e904296e43783bcc9d1e0e814bca647d6
Jim Blake <jeb228@cornell.edu>
2014-12-11 15:51:59 -0500

    VIVO-933 Read PropertyConfig.n3 into an existing model, just once.

---
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/migration/Release18Migrator.java
@@ -0,0 +1,26 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
+
+package edu.cornell.mannlib.vitro.webapp.migration;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Handle the tasks that move an installation from 1.7 to 1.8.
+ */
+public class Release18Migrator implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		ServletContext ctx = sce.getServletContext();
+
+		new FauxPropertiesUpdater(ctx, this).migrate();
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// Nothing to tear down.
+	}
+
+}
