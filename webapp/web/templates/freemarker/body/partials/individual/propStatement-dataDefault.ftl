c47964fbde3308d329bc10200d1633fb694a6bcc
Tim Worrall <tlw72@cornell.edu>
2015-04-01 12:48:15 -0400

    VIVO-921: widget-ized the data property form. Dates, ints, integers, floats, uris and booleans no longer use tinyMCE.

--- webapp/web/templates/freemarker/body/partials/individual/propStatement-dataDefault.ftl
+++ webapp/web/templates/freemarker/body/partials/individual/propStatement-dataDefault.ftl
@@ -5,7 +5,7 @@
      This template must be self-contained and not rely on other variables set for the individual page, because it
      is also used to generate the property statement during a deletion.  
  -->
-
+<#import "lib-datetime.ftl" as dt>
 <@showStatement statement />
 
 <#macro showStatement statement>
@@ -19,10 +19,18 @@
     <#if theValue?contains("<p>") >
         <#assign theValue = theValue?replace("<p>","<p style='margin-bottom:.6em'>") />
     </#if>
-    ${theValue}
+	<#if theValue?matches("^(10-20)\\d\\d[- \\/.](0[1-9]|1[012])[- \\/.](0[1-9]|[12][0-9]|3[01])$") >
+		<#assign theValue = theValue + "T00:00:00" />
+		${dt.formatXsdDateTimeLong(theValue, "yearMonthDayPrecision")}
+	<#elseif theValue?matches("^(10-20)\\d{2}(-|\\/)((0[1-9])|(1[0-2]))(-|\\/)((0[1-9])|([1-2][0-9])|(3[0-1]))(T|\\s)(([0-1][0-9])|(2[0-3])):([0-5][0-9]):([0-5][0-9])")>
+		${dt.formatXsdDateTimeLong(theValue, "yearMonthDayTimePrecision")}
+	<#elseif theValue?matches("^((10-20)\\d\\d+)-(0[1-9]|1[012])")>
+		<#assign theValue = theValue + "-01T00:00:00" />
+		${dt.formatXsdDateTimeLong(theValue, "yearMonthPrecision")}
+	<#elseif theValue?matches("^--(0[1-9]|1[012])")>
+		<#assign theValue = "2000" + theValue?substring(1) + "-01T00:00:00" />
+		${dt.formatXsdDateTimeLong(theValue, "monthPrecision")}
+	<#else>
+    	${theValue}
+	</#if>
 </#macro>
-
-
-
-
-
--- webapp/web/templates/freemarker/edit/forms/js/defaultDataPropertyUtils.js
+++ webapp/web/templates/freemarker/edit/forms/js/defaultDataPropertyUtils.js
@@ -6,7 +6,9 @@ var defaultDataPropertyUtils = {
 
         this.initObjectReferences();                 
         this.bindEventListeners();
-        
+		if ( $('#literal').val().length > 0 ) {
+			this.parseLiteralValue();
+		}
     },
 
     initObjectReferences: function() {
@@ -14,27 +16,115 @@ var defaultDataPropertyUtils = {
         this.form = $('form.editForm');
         this.textArea = $('textarea.useTinyMce');
 
+		$.extend(this, datatype);
+
     },
 
     bindEventListeners: function() {
 
         this.form.submit(function() {
-            var theText = tinyMCE.get('literal').getContent();
-
-            if ( theText.indexOf("<!--") > -1 && theText.indexOf("-->") > -1 ) {
-                var start = theText.indexOf("<p><!--");
-                var end = (theText.indexOf("--></p>") + 10);
-                var removeText = theText.slice(start,end);
-                var newText = theText.replace(removeText,"");
-                tinyMCE.get('literal').setContent(newText);                
-            }
-            else if ( theText.indexOf("&lt;!--") > -1 && theText.indexOf("--&gt;") > -1 ) {
-                var start = theText.indexOf("<p>&lt;!--");
-                var end = (theText.indexOf("--&gt;</p>") + 10);
-                var removeText = theText.slice(start,end);
-                var newText = theText.replace(removeText,"");
-                tinyMCE.get('literal').setContent(newText);                
-            }
+			if ( defaultDataPropertyUtils.textArea.length ) {
+	            var theText = tinyMCE.get('literal').getContent();
+	
+	            if ( theText.indexOf("<!--") > -1 && theText.indexOf("-->") > -1 ) {
+	                var start = theText.indexOf("<p><!--");
+	                var end = (theText.indexOf("--></p>") + 10);
+	                var removeText = theText.slice(start,end);
+	                var newText = theText.replace(removeText,"");
+	                tinyMCE.get('literal').setContent(newText);                
+	            }
+	            else if ( theText.indexOf("&lt;!--") > -1 && theText.indexOf("--&gt;") > -1 ) {
+	                var start = theText.indexOf("<p>&lt;!--");
+	                var end = (theText.indexOf("--&gt;</p>") + 10);
+	                var removeText = theText.slice(start,end);
+	                var newText = theText.replace(removeText,"");
+	                tinyMCE.get('literal').setContent(newText);                
+	            }
+			}
+			else { 
+				defaultDataPropertyUtils.createLiteralValue(); 
+			}
         });               
     },
-} 
+
+	createLiteralValue: function() {
+		var theType = datatype.substring(datatype.lastIndexOf("#") + 1);
+		var temp = "";
+		
+		switch (theType) {
+			case 'date':
+				temp = $('#dateTimeField-year').val() + "-" 
+				         + $('#dateTimeField-month').val() + "-"
+				         + $('#dateTimeField-day').val(); 
+				$('#literal').val(temp);
+				break;
+			case 'dateTime':
+				temp = $('#dateTimeField-year').val() + "-" 
+			         	+ $('#dateTimeField-month').val() + "-"
+			         	+ $('#dateTimeField-day').val() + "T" 
+						+ $('#dateTimeField-hour').val() + ":"
+						+ $('#dateTimeField-minute').val() + ":" 
+						+ $('#dateTimeField-second').val();
+				$('#literal').val(temp);
+				break;
+			case 'time':
+				temp = $('#dateTimeField-hour').val() + ":"
+						+ $('#dateTimeField-minute').val() + ":" 
+						+ $('#dateTimeField-second').val();
+				$('#literal').val(temp);
+				break;
+			case 'gYear':
+				$('#literal').val($('#dateTimeField-year').val());
+				break;
+			case 'gYearMonth':
+				temp = $('#dateTimeField-year').val() + "-" + $('#dateTimeField-month').val();
+				$('#literal').val(temp);
+				break;
+			case 'gMonth':
+				temp = "--" + $('#dateTimeField-month').val()
+				$('input#literal').val(temp);
+				break;			
+		}
+	},
+	
+	parseLiteralValue: function() {
+		var theType = datatype.substring(datatype.lastIndexOf("#") + 1);
+		var temp = $('#literal').val();
+		
+		switch (theType) {
+			case 'date':
+				$('#dateTimeField-year').val(temp.substring(0, temp.indexOf("-")));
+				$('#dateTimeField-month').val(temp.substring(temp.indexOf("-")+1,temp.lastIndexOf("-")));
+				$('#dateTimeField-day').val(temp.substring(temp.lastIndexOf("-")+1)); 
+				break;
+			case 'dateTime':
+				$('#dateTimeField-year').val(temp.substring(0, temp.indexOf("-")));
+				$('#dateTimeField-month').val(temp.substring(temp.indexOf("-")+1,temp.lastIndexOf("-")));
+				$('#dateTimeField-day').val(temp.substring(temp.lastIndexOf("-")+1,temp.indexOf("T"))); 
+				$('#dateTimeField-hour').val(temp.substring(temp.indexOf("T")+1,temp.indexOf(":")));
+				$('#dateTimeField-minute').val(temp.substring(temp.indexOf(":")+1,temp.lastIndexOf(":"))); 
+				$('#dateTimeField-second').val(temp.substring(temp.lastIndexOf(":")+1));
+				break;
+			case 'time':
+				$('#dateTimeField-hour').val(temp.substring(0,temp.indexOf(":")));
+				$('#dateTimeField-minute').val(temp.substring(temp.indexOf(":")+1,temp.lastIndexOf(":"))); 
+				$('#dateTimeField-second').val(temp.substring(temp.lastIndexOf(":")+1));
+				break;
+			case 'gYear':
+				$('#dateTimeField-year').val(temp);
+				break;
+			case 'gYearMonth':
+				$('#dateTimeField-year').val(temp.substring(0, temp.indexOf("-")));
+				$('#dateTimeField-month').val(temp.substring(temp.indexOf("-")+1));
+				break;
+			case 'gMonth':
+				$('#dateTimeField-month').val(temp.substring(temp.lastIndexOf("-")+1));
+				break;			
+		}
+	}
+	
+}
+$(document).ready(function(){
+	defaultDataPropertyUtils.onLoad();
+});
+ 
