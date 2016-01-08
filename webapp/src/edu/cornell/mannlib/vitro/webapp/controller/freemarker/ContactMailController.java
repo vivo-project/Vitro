467a127f2a909b717f8e53cc1ffe1776ca364e29
Ariel D. Moya Sequeira <amoya@amoya.me>
2016-01-08 10:05:28 -0600

    Adds UTF-8 charset to emails

--- webapp/src/edu/cornell/mannlib/vitro/webapp/controller/freemarker/ContactMailController.java
+++ webapp/src/edu/cornell/mannlib/vitro/webapp/controller/freemarker/ContactMailController.java
@@ -276,7 +276,7 @@ public class ContactMailController extends FreemarkerHttpServlet {
         msg.setSubject( deliveryfrom );
 
         // add the multipart to the message
-        msg.setContent(msgText,"text/html");
+        msg.setContent(msgText,"text/html; charset=UTF-8");
 
         // set the Date: header
         msg.setSentDate( new Date() );
