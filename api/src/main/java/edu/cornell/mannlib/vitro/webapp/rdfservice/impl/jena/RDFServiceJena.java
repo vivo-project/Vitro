28c8444833e29a8ba2e6e6612220cf490244fe0f
Graham Triggs <gtriggs@duraspace.org>
2017-10-11 19:37:07 +0100

    [VIVO-1399] Improve performance of Graph URI lookups

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/rdfservice/impl/jena/RDFServiceJena.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/rdfservice/impl/jena/RDFServiceJena.java
@@ -61,6 +61,9 @@ public abstract class RDFServiceJena extends RDFServiceImpl implements RDFServic
 
     protected abstract DatasetWrapper getDatasetWrapper();
 
+    protected volatile boolean rebuildGraphURICache = true;
+    private final List<String> graphURIs = new ArrayList<>();
+
     @Override
     public abstract boolean changeSetUpdate(ChangeSet changeSet) throws RDFServiceException;
 
@@ -527,18 +530,29 @@ public abstract class RDFServiceJena extends RDFServiceImpl implements RDFServic
 
     @Override
     public List<String> getGraphURIs() throws RDFServiceException {
-        DatasetWrapper dw = getDatasetWrapper();
-        try {
-            Dataset d = dw.getDataset();
-            List<String> graphURIs = new ArrayList<String>();
-            Iterator<String> nameIt = d.listNames();
-            while (nameIt.hasNext()) {
-                graphURIs.add(nameIt.next());
+        if (rebuildGraphURICache) {
+            synchronized (RDFServiceJena.class) {
+                if (rebuildGraphURICache) {
+                    rebuildGraphURICache = false;
+                    graphURIs.clear();
+
+                    DatasetWrapper dw = getDatasetWrapper();
+                    try {
+                        Dataset d = dw.getDataset();
+                        List<String> graphURIs = new ArrayList<String>();
+                        Iterator<String> nameIt = d.listNames();
+                        while (nameIt.hasNext()) {
+                            graphURIs.add(nameIt.next());
+                        }
+                        return graphURIs;
+                    } finally {
+                        dw.close();
+                    }
+                }
             }
-            return graphURIs;
-        } finally {
-            dw.close();
         }
+
+        return graphURIs;
     }
 
     @Override
--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/rdfservice/impl/jena/sdb/RDFServiceSDB.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/rdfservice/impl/jena/sdb/RDFServiceSDB.java
@@ -113,6 +113,7 @@ public class RDFServiceSDB extends RDFServiceJena implements RDFService {
             abortTransaction(sdbConn);
             throw new RDFServiceException(e);
         } finally {
+            rebuildGraphURICache = true;
             close(sdbConn);
         }
     }
