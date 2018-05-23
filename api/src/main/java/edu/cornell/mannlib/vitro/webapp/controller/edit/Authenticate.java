df58f2d18c99b60ed5a6bece95a2ac9870af8d2d
AsimA <ahmad.asim@tib.eu>
2018-05-23 11:28:35 +0200

    remvoed the unused import

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/controller/edit/Authenticate.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/controller/edit/Authenticate.java
@@ -23,7 +23,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.mysql.cj.api.x.core.Warning;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
