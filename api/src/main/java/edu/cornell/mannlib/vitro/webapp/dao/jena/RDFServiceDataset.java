af2e2160b194bd9b5947990a37e41554332bb9dc
Graham Triggs <graham.triggs@tib.eu>
2019-04-30 19:27:53 +0100

    Upgrade Jena to 3.11.0

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/dao/jena/RDFServiceDataset.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dao/jena/RDFServiceDataset.java
@@ -14,6 +14,7 @@ import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.LabelExistsException;
 import org.apache.jena.query.ReadWrite;
+import org.apache.jena.query.TxnType;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.shared.Lock;
 import org.apache.jena.sparql.core.DatasetGraph;
@@ -86,7 +87,7 @@ public class RDFServiceDataset implements Dataset {
     }
 
 	@Override
-	public void addNamedModel(String uri, Model model)
+	public Dataset addNamedModel(String uri, Model model)
 			throws LabelExistsException {
 		Iterator<Node> graphNodes = g.listGraphNodes();
 		while (graphNodes.hasNext()) {
@@ -97,6 +98,7 @@ public class RDFServiceDataset implements Dataset {
 			}
 		}
 		g.addGraph(NodeFactory.createURI(uri), model.getGraph());
+		return this;
 	}
 
 	@Override
@@ -105,19 +107,22 @@ public class RDFServiceDataset implements Dataset {
 	}
 
 	@Override
-	public void removeNamedModel(String uri) {
+	public Dataset removeNamedModel(String uri) {
 		g.removeGraph(NodeFactory.createURI(uri));
+		return this;
 	}
 
 	@Override
-	public void replaceNamedModel(String uri, Model model) {
+	public Dataset replaceNamedModel(String uri, Model model) {
 		removeNamedModel(uri);
 		addNamedModel(uri, model);
+		return this;
 	}
 
 	@Override
-	public void setDefaultModel(Model model) {
+	public Dataset setDefaultModel(Model model) {
 		g.setDefaultGraph(model.getGraph());
+		return this;
 	}
 
 	@Override
@@ -173,4 +178,23 @@ public class RDFServiceDataset implements Dataset {
 		return g.isEmpty();
 	}
 
+	@Override
+	public TxnType transactionType() {
+		return g.transactionType();
+	}
+
+	@Override
+	public boolean promote(Promote promote) {
+		return g.promote(promote);
+	}
+
+	@Override
+	public ReadWrite transactionMode() {
+		return g.transactionMode();
+	}
+
+	@Override
+	public void begin(TxnType txnType) {
+		g.begin(txnType);
+	}
 }
