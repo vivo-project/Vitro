b5f75176f894cd4d5420bbeb7b997f147c7fb39f
a.galasso <a.galasso@NAGALASSO230567.pdl.ms.cineca.it>
2019-05-13 11:47:52 +0200

    VIVO-1686

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/utils/http/HttpClientFactory.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/utils/http/HttpClientFactory.java
@@ -3,17 +3,21 @@
 package edu.cornell.mannlib.vitro.webapp.utils.http;
 
 import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.PoolingClientConnectionManager;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 
 public final class HttpClientFactory {
-    private static final DefaultHttpClient httpClient;
+    
+	private static final HttpClient httpClient;
 
     static {
-        PoolingClientConnectionManager cm = new PoolingClientConnectionManager();
-        cm.setDefaultMaxPerRoute(50);
-        cm.setMaxTotal(300);
-        httpClient = new DefaultHttpClient(cm);
+    	
+    	PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
+		cm.setMaxTotal(300);
+    	cm.setDefaultMaxPerRoute(50);
+    	
+    	httpClient = HttpClients.custom().setConnectionManager(cm).build();
+   	
     }
 
     public static HttpClient getHttpClient() {
