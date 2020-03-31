edef9309ab71937cd66965cb0c0aa9921f58332e
Ben <mbgross@wustl.edu>
2020-03-31 10:25:26 -0600

    [VIVO-1755] - Better error handling when reasoner disabled (#137)
    * Better error handling when reasoner disabled
    
    * Change reasoner error log message to debug
    
    * Extend reasoner status error handling
    
    * Improve reasoner error log message and extend to JenaAdminActions
    
    * Bump up log level for admin action to 'warn' and edit admin panel error message

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/dao/jena/DataPropertyDaoJena.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dao/jena/DataPropertyDaoJena.java
@@ -360,8 +360,13 @@ public class DataPropertyDaoJena extends PropertyDaoJena implements
     }
 
     protected boolean reasoningAvailable() {
-    	TBoxReasonerStatus status = ApplicationUtils.instance().getTBoxReasonerModule().getStatus();
-    	return status.isConsistent() && !status.isInErrorState();
+        try {
+            TBoxReasonerStatus status = ApplicationUtils.instance().getTBoxReasonerModule().getStatus();
+            return status.isConsistent() && !status.isInErrorState();
+        } catch (IllegalStateException e) {
+            log.debug("Status of reasoner could not be determined. It is likely disabled.", e);
+            return false;
+        }
     }
 
     private String getRequiredDatatypeURI(Individual individual, DataProperty dataprop, List<String> vclassURIs) {
