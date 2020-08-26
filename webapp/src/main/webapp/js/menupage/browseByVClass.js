e603f263331c648d3186a347bce2189f0910c0ec
William Welling <wwelling@library.tamu.edu>
2020-08-26 11:51:25 -0500

    simplify and concat translation for browse by vclass

--- webapp/src/main/webapp/js/menupage/browseByVClass.js
+++ webapp/src/main/webapp/js/menupage/browseByVClass.js
@@ -234,9 +234,9 @@ var browseByVClass = {
         var alpha = this.selectedAlpha(alpha);
 
         if ( alpha != "all" ) {
-            nothingToSeeHere = '<p class="no-individuals">' + browseByVClass.thereAreNo + ' ' + vclass.name + ' ' + browseByVClass.indNamesStartWith + ' <em>'+ alpha.toUpperCase() +'</em>.</p> <p class="no-individuals">' + browseByVClass.tryAnotherLetter + '</p>';
+            nothingToSeeHere = '<p class="no-individuals">' + browseByVClass.thereAreNoEntryStartingWith + ' <em>'+ alpha.toUpperCase() +'</em>.</p> <p class="no-individuals">' + browseByVClass.tryAnotherLetter + '</p>';
         } else {
-            nothingToSeeHere = '<p class="no-individuals">' + browseByVClass.thereAreNo + ' ' + vclass.name + ' ' + browseByVClass.indsInSystem + '</p> <p class="no-individuals">' + browseByVClass.selectAnotherClass + '</p>';
+            nothingToSeeHere = '<p class="no-individuals">' + browseByVClass.thereAreNoEntryStartingWith + '</p> <p class="no-individuals">' + browseByVClass.selectAnotherClass + '</p>';
         }
 
         browseByVClass.individualsContainer.prepend(nothingToSeeHere);
