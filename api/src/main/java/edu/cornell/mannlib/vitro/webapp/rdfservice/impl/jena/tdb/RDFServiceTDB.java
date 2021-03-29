ff8ba8adccdd1f61961cc311239112bc15ad69c8
Brian Lowe <brian@ontocale.com>
2021-03-29 16:17:37 +0300

    Add usePreciseSubqueries property to example.runtime.properties.  Have RDFServiceTDB report a preference for precise optionals as does SDB.

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/rdfservice/impl/jena/tdb/RDFServiceTDB.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/rdfservice/impl/jena/tdb/RDFServiceTDB.java
@@ -93,6 +93,10 @@ public class RDFServiceTDB extends RDFServiceJena {
 		}
 	}
 
+        @Override
+        public boolean preferPreciseOptionals() {
+            return true;
+        }
 
 	@Override
 	public void close() {
