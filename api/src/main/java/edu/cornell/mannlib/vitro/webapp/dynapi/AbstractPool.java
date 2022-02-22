556a86118e50b788fd706482f56222b1baa869f0
William Welling <8352733+wwelling@users.noreply.github.com>
2022-02-22 14:00:06 -0600

    afford add/remove individual component from pools

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/AbstractPool.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/AbstractPool.java
@@ -1,6 +1,9 @@
 package edu.cornell.mannlib.vitro.webapp.dynapi;
 
+import static edu.cornell.mannlib.vitro.webapp.dao.VitroVocabulary.RDF_TYPE;
 import static edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames.FULL_UNION;
+import static edu.cornell.mannlib.vitro.webapp.utils.configuration.ConfigurationBeanLoader.toJavaUri;
+import static java.lang.String.format;
 
 import java.util.Map;
 import java.util.Set;
@@ -12,15 +15,22 @@ import javax.servlet.ServletContext;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.jena.ontology.OntModel;
+import org.apache.jena.rdf.model.NodeIterator;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
 
 import edu.cornell.mannlib.vitro.webapp.dynapi.components.Poolable;
 import edu.cornell.mannlib.vitro.webapp.modelaccess.ContextModelAccess;
 import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
 import edu.cornell.mannlib.vitro.webapp.utils.configuration.ConfigurationBeanLoader;
+import edu.cornell.mannlib.vitro.webapp.utils.configuration.ConfigurationBeanLoaderException;
 
 public abstract class AbstractPool<P extends Pool<C>, C extends Poolable> implements Pool<C> {
 
     private final Log log = LogFactory.getLog(this.getClass());
+
+    private static final Object mutex = new Object();
 
     private ConcurrentHashMap<String, C> components;
     private ServletContext ctx;
@@ -52,17 +62,56 @@ public abstract class AbstractPool<P extends Pool<C>, C extends Poolable> implem
 
     public void printNames() {
         for (Map.Entry<String, C> entry : components.entrySet()) {
-            log.debug("C in pool: '" + entry.getKey() + "'");
+            log.debug(format("%s in pool: '%s'", getType().getName(), entry.getKey()));
+        }
+    }
+
+    public void add(String uri, C component) {
+        String name = component.getName();
+        log.info(format("Adding component %s with URI %s", name, uri));
+        if (isInModel(uri)) {
+            synchronized (mutex) {
+                C oldComponent = components.put(name, component);
+                if (oldComponent != null) {
+                    obsoleteComponents.add(oldComponent);
+                    unloadObsoleteComponents();
+                }
+            }
+        } else {
+            throw new RuntimeException(format("%s %s with URI %s not found in model. Not adding to pool.", getType().getName(), name, uri));
+        }
+    }
+
+    public void remove(String uri, String name) {
+        log.info(format("Removing component %s with URI %s", name, uri));
+        if (!isInModel(uri)) {
+            synchronized (mutex) {
+                C oldComponent = components.remove(name);
+                if (oldComponent != null) {
+                    obsoleteComponents.add(oldComponent);
+                    unloadObsoleteComponents();
+                }
+            }
+        } else {
+            throw new RuntimeException(format("%s %s with URI %s still exists in model. Not removing from pool.", getType().getName(), name, uri));
+        }
+    }
+
+    public void reload(String uri) {
+        try {
+            add(uri, loader.loadInstance(uri, getType()));
+        } catch (ConfigurationBeanLoaderException e) {
+            throw new RuntimeException(format("Failed to reload %s with URI %s.", getType().getName(), uri));
         }
     }
 
     public synchronized void reload() {
         if (ctx == null) {
-            log.error("Context is null. Can't reload component pool.");
+            log.error(format("Context is null. Can't reload %s.", this.getClass().getName()));
             return;
         }
         if (loader == null) {
-            log.error("Loader is null. Can't reload component pool.");
+            log.error(format("Loader is null. Can't reload %s.", this.getClass().getName()));
             return;
         }
         ConcurrentHashMap<String, C> newActions = new ConcurrentHashMap<>();
@@ -95,15 +144,15 @@ public abstract class AbstractPool<P extends Pool<C>, C extends Poolable> implem
 
     private void loadComponents(ConcurrentHashMap<String, C> components) {
         Set<C> newActions = loader.loadEach(getType());
-        log.debug("Context Initialization. components loaded: " + components.size());
+        log.debug(format("Context Initialization. %s %s(s) currently loaded.", components.size(), getType().getName()));
         for (C component : newActions) {
             if (component.isValid()) {
                 components.put(component.getName(), component);
             } else {
-                log.error("C with rpcName " + component.getName() + " is invalid.");
+                log.error(format("%s with rpcName %s is invalid.", getType().getName(), component.getName()));
             }
         }
-        log.debug("Context Initialization finished. " + components.size() + " components loaded.");
+        log.debug(format("Context Initialization finished. %s %s(s) loaded.", components.size(), getType().getName()));
     }
 
     private void unloadObsoleteComponents() {
@@ -124,6 +173,23 @@ public abstract class AbstractPool<P extends Pool<C>, C extends Poolable> implem
             return false;
         }
         return true;
+    }
+
+    private boolean isInModel(String uri) {
+        Resource s = dynamicAPIModel.getResource(uri);
+        Property p = dynamicAPIModel.getProperty(RDF_TYPE); 
+
+        String javaUri = toJavaUri(getType());
+
+        NodeIterator nit = dynamicAPIModel.listObjectsOfProperty(s, p);
+        while (nit.hasNext()) {
+            RDFNode node = nit.next();
+            if (node.isResource() && node.toString().replace("#", ".").equals(javaUri)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
--- api/src/test/java/edu/cornell/mannlib/vitro/webapp/dynapi/ResourcePoolTest.java
+++ api/src/test/java/edu/cornell/mannlib/vitro/webapp/dynapi/ResourcePoolTest.java
@@ -14,20 +14,29 @@ import org.junit.Test;
 
 import edu.cornell.mannlib.vitro.webapp.dynapi.components.DefaultResource;
 import edu.cornell.mannlib.vitro.webapp.dynapi.components.Resource;
+import edu.cornell.mannlib.vitro.webapp.utils.configuration.ConfigurationBeanLoaderException;
 
 public class ResourcePoolTest extends ServletContextTest {
+
+    protected final static String TEST_RELOAD_RESOURCE_URI = "https://vivoweb.org/ontology/vitro-dynamic-api/resource/testReloadResource1";
 
     @After
     public void reset() {
         setup();
 
+        ResourcePool resourcePool = ResourcePool.getInstance();
+        resourcePool.init(servletContext);
+        resourcePool.reload();
+
         ActionPool actionPool = ActionPool.getInstance();
         actionPool.init(servletContext);
         actionPool.reload();
 
-        ResourcePool resourcePool = ResourcePool.getInstance();
-        resourcePool.init(servletContext);
-        resourcePool.reload();
+        assertEquals(0, resourcePool.count());
+        assertEquals(0, resourcePool.obsoleteCount());
+
+        assertEquals(0, actionPool.count());
+        assertEquals(0, actionPool.obsoleteCount());
     }
 
     @Test
@@ -46,7 +55,7 @@ public class ResourcePoolTest extends ServletContextTest {
     }
 
     @Test
-    public void testPrintActionNamesBeforeInit() {
+    public void testPrintNamesBeforeInit() {
         ResourcePool resourcePool = ResourcePool.getInstance();
         resourcePool.printNames();
         // nothing to assert
@@ -62,16 +71,143 @@ public class ResourcePoolTest extends ServletContextTest {
     @Test
     public void testInit() throws IOException {
         ResourcePool resourcePool = initWithDefaultModel();
+        assertEquals(1, resourcePool.count());
+        assertEquals(0, resourcePool.obsoleteCount());
 
         assertResourceByName(resourcePool.getByName(TEST_RESOURCE_NAME), TEST_RESOURCE_NAME, TEST_ACTION_NAME);
     }
 
     @Test
-    public void testPrintActionNames() throws IOException {
+    public void testPrintNames() throws IOException {
         ResourcePool resourcePool = initWithDefaultModel();
 
         resourcePool.printNames();
         // nothing to assert
+    }
+
+    @Test
+    public void testAdd() throws IOException, ConfigurationBeanLoaderException {
+        ResourcePool resourcePool = initWithDefaultModel();
+
+        loadReloadModel();
+
+        Resource resource = loader.loadInstance(TEST_RELOAD_RESOURCE_URI, Resource.class);
+
+        resourcePool.add(TEST_RELOAD_RESOURCE_URI, resource);
+
+        assertEquals(0, resourcePool.obsoleteCount());
+
+        assertResourceByName(resourcePool.getByName(TEST_RELOAD_RESOURCE_NAME), TEST_RELOAD_RESOURCE_NAME, TEST_RELOAD_ACTION_NAME);
+    }
+
+    @Test
+    public void testAddHasClient() throws IOException, ConfigurationBeanLoaderException {
+        ResourcePool resourcePool = initWithDefaultModel();
+
+        loadReloadModel();
+
+        resourcePool.reload();
+
+        Resource resource = loader.loadInstance(TEST_RELOAD_RESOURCE_URI, Resource.class);
+
+        assertEquals(0, resourcePool.obsoleteCount());
+
+        Resource resourceHasClient = resourcePool.getByName(TEST_RELOAD_RESOURCE_NAME);
+
+        resourcePool.add(TEST_RELOAD_RESOURCE_URI, resource);
+
+        assertEquals(1, resourcePool.obsoleteCount());
+
+        resourceHasClient.removeClient();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testAddWithoutModelLoaded() throws IOException, ConfigurationBeanLoaderException {
+        ResourcePool resourcePool = initWithDefaultModel();
+
+        loadReloadModel();
+
+        Resource resource = loader.loadInstance(TEST_RELOAD_RESOURCE_URI, Resource.class);
+
+        reset();
+
+        assertTrue(resourcePool.getByName(TEST_RELOAD_RESOURCE_NAME) instanceof DefaultResource);
+
+        resourcePool.add(TEST_RELOAD_RESOURCE_URI, resource);
+    }
+
+    @Test
+    public void testRemove() throws IOException, ConfigurationBeanLoaderException {
+        ResourcePool resourcePool = initWithDefaultModel();
+
+        loadReloadModel();
+
+        resourcePool.reload();
+
+        Resource resource = resourcePool.getByName(TEST_RELOAD_RESOURCE_NAME);
+
+        assertFalse(resource instanceof DefaultResource);
+
+        resource.removeClient();
+
+        reset();
+
+        resourcePool.remove(TEST_RELOAD_RESOURCE_URI, TEST_RELOAD_RESOURCE_NAME);
+
+        assertEquals(0, resourcePool.obsoleteCount());
+
+        assertTrue(resourcePool.getByName(TEST_RELOAD_RESOURCE_NAME) instanceof DefaultResource);
+    }
+
+    @Test
+    public void testRemoveHasClient() throws IOException, ConfigurationBeanLoaderException {
+        ResourcePool resourcePool = initWithDefaultModel();
+
+        loadReloadModel();
+
+        resourcePool.reload();
+
+        Resource resourceHasClient = resourcePool.getByName(TEST_RELOAD_RESOURCE_NAME);
+
+        assertFalse(resourceHasClient instanceof DefaultResource);
+
+        setup();
+
+        resourcePool.init(servletContext);
+
+        resourcePool.remove(TEST_RELOAD_RESOURCE_URI, TEST_RELOAD_RESOURCE_NAME);
+
+        assertEquals(1, resourcePool.obsoleteCount());
+
+        assertTrue(resourcePool.getByName(TEST_RELOAD_RESOURCE_NAME) instanceof DefaultResource);
+
+        resourceHasClient.removeClient();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testRemoveWithModelLoaded() throws IOException, ConfigurationBeanLoaderException {
+        ResourcePool resourcePool = initWithDefaultModel();
+
+        loadReloadModel();
+
+        resourcePool.reload();
+
+        resourcePool.remove(TEST_RELOAD_RESOURCE_URI, TEST_RELOAD_RESOURCE_NAME);
+    }
+
+    @Test
+    public void testReloadSingle() throws IOException {
+        ResourcePool resourcePool = initWithDefaultModel();
+
+        loadReloadModel();
+
+        Resource resource = resourcePool.getByName(TEST_RELOAD_RESOURCE_NAME);
+
+        assertTrue(resource instanceof DefaultResource);
+
+        resourcePool.reload(TEST_RELOAD_RESOURCE_URI);
+
+        assertResourceByName(resourcePool.getByName(TEST_RELOAD_RESOURCE_NAME), TEST_RELOAD_RESOURCE_NAME, TEST_RELOAD_ACTION_NAME);
     }
 
     @Test
@@ -83,6 +219,9 @@ public class ResourcePoolTest extends ServletContextTest {
         loadReloadModel();
 
         resourcePool.reload();
+
+        assertEquals(2, resourcePool.count());
+        assertEquals(0, resourcePool.obsoleteCount());
 
         assertResourceByName(resourcePool.getByName(TEST_RELOAD_RESOURCE_NAME), TEST_RELOAD_RESOURCE_NAME, TEST_RELOAD_ACTION_NAME);
     }
@@ -107,7 +246,7 @@ public class ResourcePoolTest extends ServletContextTest {
     }
 
     @Test
-    public void testRealodOfActionInUse() throws IOException {
+    public void testRealodOfResourceHasClient() throws IOException {
         ResourcePool resourcePool = initWithDefaultModel();
 
         loadReloadModel();
@@ -136,7 +275,7 @@ public class ResourcePoolTest extends ServletContextTest {
 
         assertFalse(resource.hasClients());
 
-        Thread t1 = getResourceInThread(resourcePool, TEST_RESOURCE_NAME, TEST_ACTION_NAME);
+        Thread t1 = getResourceInThread(resourcePool, TEST_RESOURCE_NAME);
 
         t1.join();
 
@@ -147,12 +286,13 @@ public class ResourcePoolTest extends ServletContextTest {
         assertEquals(initalCount, resourcePool.obsoleteCount());
     }
 
-    private Thread getResourceInThread(ResourcePool resourcePool, String name, String actionName) {
+    private Thread getResourceInThread(ResourcePool resourcePool, String resourceName) {
         Runnable client = new Runnable() {
             @Override
             public void run() {
-                Resource resource = resourcePool.getByName(name);
-                assertResourceByName(resource, name, actionName);
+                Resource resource = resourcePool.getByName(resourceName);
+                assertEquals(resourceName, resource.getName());
+                assertTrue(resource.hasClients());
             }
         };
         Thread thread = new Thread(client);
@@ -163,24 +303,25 @@ public class ResourcePoolTest extends ServletContextTest {
     private ResourcePool initWithDefaultModel() throws IOException {
         loadDefaultModel();
 
-        ActionPool actionPool = ActionPool.getInstance();
-        actionPool.init(servletContext);
-
         ResourcePool resourcePool = ResourcePool.getInstance();
         resourcePool.init(servletContext);
+
+        ActionPool actionPool = ActionPool.getInstance();
+        actionPool.init(servletContext);
 
         return resourcePool;
     }
 
-    private void assertResourceByName(Resource resource, String name, String actionName) {
+    private void assertResourceByName(Resource resource, String resourceName, String actionName) {
         assertNotNull(resource);
-        assertFalse(format("%s not loaded!", name), resource instanceof DefaultResource);
-        assertEquals(name, resource.getName());
+        assertFalse(format("%s not loaded!", resourceName), resource instanceof DefaultResource);
+        assertEquals(resourceName, resource.getName());
         assertTrue(resource.hasClients());
 
         String minVer = "0.1.0";
 
         assertEquals(actionName, resource.getRpcOnGet().getName());
+        // TODO: figure out why these are all POST
         // assertEquals("GET", resource.getRpcOnGet().getHttpMethod().getName());
         assertEquals(minVer, resource.getRpcOnGet().getMinVersion());
         assertEquals(actionName, resource.getRpcOnPost().getName());
@@ -195,6 +336,8 @@ public class ResourcePoolTest extends ServletContextTest {
         assertEquals(actionName, resource.getRpcOnPatch().getName());
         // assertEquals("PATCH", resource.getRpcOnPatch().getHttpMethod().getName());
         assertEquals(minVer, resource.getRpcOnPatch().getMinVersion());
+
+        resource.removeClient();
     }
 
 }
