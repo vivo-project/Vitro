da82e22acb419388e9d78579c65871cdf2a45b8c
Georgy Litvinov <git@litvinovg.pro>
2022-02-23 18:29:05 +0100

    feat: added interfaces to get required information for parameter scopes computation

---
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/ParameterInfo.java
@@ -0,0 +1,9 @@
+package edu.cornell.mannlib.vitro.webapp.dynapi.components;
+
+public interface ParameterInfo {
+  
+  public Parameters getRequiredParams();
+  
+  public Parameters getProvidedParams();
+  
+}
