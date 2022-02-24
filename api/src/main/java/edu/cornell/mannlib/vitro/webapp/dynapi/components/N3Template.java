c6c91511b5f7feaae498b945d60e3074d5f84b3e
Veljko Maksimovic <maxi.maksimovic7@gmail.com>
2022-02-24 23:43:51 +0100

    added refactored SPARQLQueryOperation and N3Template

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/N3Template.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/N3Template.java
@@ -3,7 +3,7 @@ package edu.cornell.mannlib.vitro.webapp.dynapi.components;
 import edu.cornell.mannlib.vitro.webapp.dynapi.OperationData;
 import edu.cornell.mannlib.vitro.webapp.utils.configuration.Property;
 
-public class N3Template implements Template{
+public class N3Template extends AbstractOperation implements Template{
 
 	private String n3Text;
 
@@ -23,28 +23,5 @@ public class N3Template implements Template{
 		return null;
 	}
 
-	@Override
-	public void addRequiredParameter(Parameter param) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addProvidedParameter(Parameter param) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Parameters getRequiredParams() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Parameters getProvidedParams() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
