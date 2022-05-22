e253514555170c9e8d51670addb77e0bae90a0fd
Veljko Maksimovic <45179708+VeljkoMaksimovic@users.noreply.github.com>
2022-05-22 18:22:19 +0200

    N3template operation (#286)
    * refactored SolrTest to be inside the 'components' package
    
    * Created first version of N3 template. Testing needs to be implemented
    
    * N3 templates create, with test example, and new ParameterType for anyURI
    
    * Changed tests and testing .n3 files to match new ontology
    
    * Fixed string constant, and added Parameter types into created literals
    
    * Changed n3Template top work with new Parameters and new ontology
    
    * refactored Parameters class
    
    * Moved parameter filtering from N3Template to Parameters class
    
    * Fixing merge bugs
    
    Co-authored-by: Georgy Litvinov <git@litvinovg.pro>

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/N3Template.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/N3Template.java
@@ -1,13 +1,26 @@
 package edu.cornell.mannlib.vitro.webapp.dynapi.components;
 
 import edu.cornell.mannlib.vitro.webapp.dynapi.OperationData;
+import edu.cornell.mannlib.vitro.webapp.edit.n3editing.VTwo.AdditionsAndRetractions;
+import edu.cornell.mannlib.vitro.webapp.edit.n3editing.VTwo.EditN3GeneratorVTwo;
+import edu.cornell.mannlib.vitro.webapp.edit.n3editing.VTwo.ProcessRdfForm;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
 import edu.cornell.mannlib.vitro.webapp.utils.configuration.Property;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.Model;
+
+import java.util.*;
 
 public class N3Template extends Operation implements Template {
 
-    private String n3Text;
+	private static final Log log = LogFactory.getLog(N3Template.class);
 
-    private Parameters requiredParams = new Parameters();
+
+	private Parameters requiredParams = new Parameters();
+	private String n3Text;
+	private ModelComponent templateModel;
 
     // region @Property Setters
 
@@ -15,6 +28,9 @@ public class N3Template extends Operation implements Template {
     public void addRequiredParameter(Parameter param) {
         requiredParams.add(param);
     }
+
+	@Property(uri = "https://vivoweb.org/ontology/vitro-dynamic-api#hasModel", minOccurs = 1, maxOccurs = 1)
+	public void setTemplateModel(ModelComponent templateModel){ this.templateModel = templateModel; }
 
     @Property(uri = "https://vivoweb.org/ontology/vitro-dynamic-api#n3Text", minOccurs = 1, maxOccurs = 1)
     public void setN3Text(String n3Text) {
@@ -30,21 +46,76 @@ public class N3Template extends Operation implements Template {
         return requiredParams;
     }
 
-    @Override
-    public Parameters getProvidedParams() {
-        return new Parameters();
-    }
+	@Override
+	public Parameters getProvidedParams() {
+		return new Parameters();
+	}
 
-    // endregion
+	public String getN3Text() { return this.n3Text; }
 
-    @Override
-    public OperationResult run(OperationData input) {
-        return null;
-    }
+	//endregion
 
-    @Override
-    public void dereference() {
+	@Override
+	public OperationResult run(OperationData input) {
+		if (!isInputValid(input)) {
+			return new OperationResult(500);
+		}
 
-    }
+		String substitutedN3Template;
+		try {
+			substitutedN3Template=insertParameters(input);
+		}catch (InputMismatchException e){
+			log.error(e);
+			return new OperationResult(500);
+		}
+
+		List<Model> additionModels;
+		try {
+			additionModels = ProcessRdfForm.parseN3ToRDF(Arrays.asList(substitutedN3Template), ProcessRdfForm.N3ParseType.REQUIRED);
+		} catch (Exception e) {
+			log.error("Error while trying to parse N3Template string and create a Jena rdf Model");
+			log.error(e);
+			return new OperationResult(500);
+		}
+
+		AdditionsAndRetractions changes = new AdditionsAndRetractions(additionModels, new ArrayList<Model>());
+		Model writeModel = ModelAccess.on(input.getContext()).getOntModel(templateModel.getName());
+		ProcessRdfForm.applyChangesToWriteModel(changes, null, writeModel,"");
+
+		return new OperationResult(200);
+	}
+
+	private String insertParameters(OperationData input) throws InputMismatchException{
+
+		EditN3GeneratorVTwo gen = new EditN3GeneratorVTwo();
+
+		//Had to convert String to List<String> to be compatible with EditN3GeneratorVTwo methods
+		List<String> n3WithParameters = Arrays.asList(n3Text);
+
+		//region Substitute IRI variables
+		Map<String, List<String>> parametersToUris = requiredParams.getUrisMap(input);
+
+		gen.subInMultiUris(parametersToUris, n3WithParameters);
+		//endregion
+
+		//region Substitute other (literal) variables
+		Map<String, List<Literal>> parametersToLiterals = requiredParams.getLiteralsMap(input);
+
+		gen.subInMultiLiterals(parametersToLiterals, n3WithParameters);
+		//endregion
+
+		//Check if any n3 variables are left without inserted value
+		String[] leftoverVariables = Arrays.stream(n3WithParameters.get(0).split(" "))
+				.filter(token -> token.startsWith("?")).toArray(String[]::new);
+		if(leftoverVariables.length>0){
+			throw new InputMismatchException("N3 template variables:'" + Arrays.toString(leftoverVariables) +
+					"' dont have corresponding input parameters with values that should be inserted in their place.");
+		}
+		return n3WithParameters.get(0);
+	}
+
+
+	@Override
+	public void dereference() {}
 
 }
