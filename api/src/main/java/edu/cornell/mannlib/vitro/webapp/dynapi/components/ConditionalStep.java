f7ed2c8202caf3658cabb1f61f689221c766d20a
Georgy Litvinov <git@litvinovg.pro>
2022-06-08 16:28:15 +0200

    feat: automatic computation of action required params (#294)
    * feat: automatic computation of action required params
    
    * log removed action clients
    
    * fix: new line at the end of the file
    
    * fix access modifier
    
    * fix: introduced NullStep
    
    * feat: support computation of multiple execution paths, formatting fixes
    
    * fix: take into account parameters that should be included in results of action
    
    * cleanups and fixes
    
    * removed useless check
    
    * fixes for tests and null objects
    
    * fix: create local copy of thread ids before requesting list of all threads to compare to prevent case when new client is added to the client's list after list of all threads was requiested
    
    * review fixes
    
    * fix: thread safety fix for ResourceAPI

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/ConditionalStep.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/ConditionalStep.java
@@ -1,18 +1,20 @@
 package edu.cornell.mannlib.vitro.webapp.dynapi.components;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import edu.cornell.mannlib.vitro.webapp.dynapi.OperationData;
+import edu.cornell.mannlib.vitro.webapp.dynapi.computation.StepInfo;
 
 public class ConditionalStep implements Step {
 
     private Condition condition;
+    
+    Step nextStepIfFailed = NullStep.getInstance();
+    Step nextStepIfSucceded = NullStep.getInstance();
 
-    @Override
-    public void dereference() {
-        // TODO Auto-generated method stub
+    private Parameters parameters = new Parameters();
 
-    }
 
     @Override
     public OperationResult run(OperationData input) {
@@ -20,19 +22,23 @@ public class ConditionalStep implements Step {
     }
 
     @Override
-    public Set<Link> getNextLinks() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public Parameters getRequiredParams() {
-        return condition.getRequiredParams();
-    }
-
-    @Override
     public Parameters getProvidedParams() {
         return new Parameters();
+    }
+
+    public void setCondition(Condition condition) {
+        this.condition = condition;
+    }
+
+    @Override
+    public void dereference() {}
+
+    @Override
+    public Set<StepInfo> getNextNodes() {
+        HashSet<StepInfo> nextNodes = new HashSet<StepInfo>();
+        nextNodes.add(nextStepIfFailed);
+        nextNodes.add(nextStepIfSucceded);
+        return nextNodes;
     }
 
     @Override
@@ -40,8 +46,22 @@ public class ConditionalStep implements Step {
         return false;
     }
 
-    public void setCondition(Condition condition) {
-        this.condition = condition;
+    @Override
+    public boolean isOptional() {
+        return false;
+    }
+
+    @Override
+    public Parameters getRequiredParams() {
+        return parameters ;
+    }
+
+    public void setNextStepFalse(Step step) {
+        nextStepIfFailed = step;
+    }
+
+    public void setNextStepTrue(Step step) {
+        nextStepIfSucceded = step;
     }
 
 }
---
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/computation/ExecutionTree.java
@@ -0,0 +1,89 @@
+package edu.cornell.mannlib.vitro.webapp.dynapi.computation;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import edu.cornell.mannlib.vitro.webapp.dynapi.components.NullStep;
+
+public class ExecutionTree {
+    private Map<StepInfo, List<StepInfo>> forwardMap;
+    private Map<StepInfo, List<StepInfo>> backwardMap;
+    
+    private ExecutionTree() {
+        //map from previous to list of next
+        forwardMap = new HashMap<StepInfo, List<StepInfo>>();
+        //map from next to list of previous
+        backwardMap = new HashMap<StepInfo, List<StepInfo>>();
+    }
+    
+    public ExecutionTree(StepInfo rootNode) {
+        this();
+        addNodesToMaps(rootNode);
+    }
+
+    private void addNodesToMaps(StepInfo node) {
+        Set<StepInfo> nextNodes = node.getNextNodes();
+        for (StepInfo next : nextNodes) {
+            if (!containsPrev(next)) {
+                addArc(node, next);
+                addNodesToMaps(next);
+            }
+        }
+    }
+    
+    public List<StepInfo> getLeafs(){
+        return getPrevOf(NullStep.getInstance());
+    }
+
+    public List<StepInfo> getPrevOf(StepInfo node){
+        if (!backwardMap.containsKey(node)) {
+            return Collections.emptyList();
+        }
+        return backwardMap.get(node);
+    }
+    
+    public List<StepInfo> getNextOf(StepInfo node){
+        if (!forwardMap.containsKey(node)) {
+            return Collections.emptyList();
+        }
+        return forwardMap.get(node);
+    }
+    
+    private void addArc(StepInfo prev, StepInfo next) {
+        addToForwardMap(prev, next);
+        addToBackwardMap(prev, next);
+    }
+    
+    private boolean containsNext(StepInfo node) {
+        return backwardMap.containsKey(node);
+    }
+    
+    private boolean containsPrev(StepInfo node) {
+        return forwardMap.containsKey(node);
+    }
+
+    private void addToBackwardMap(StepInfo prev, StepInfo next) {
+        if (backwardMap.containsKey(next)){
+            backwardMap.get(next).add(prev);
+        } else {
+            LinkedList<StepInfo> prevs = new LinkedList<StepInfo>();
+            prevs.add(prev);
+            backwardMap.put(next, prevs);
+        }
+    }
+    
+    private void addToForwardMap(StepInfo prev, StepInfo next) {
+        if (forwardMap.containsKey(prev)){
+            forwardMap.get(prev).add(next);
+        } else {
+            LinkedList<StepInfo> nextNodes = new LinkedList<StepInfo>();
+            nextNodes.add(next);
+            forwardMap.put(prev, nextNodes);
+        }
+    }
+
+} 
