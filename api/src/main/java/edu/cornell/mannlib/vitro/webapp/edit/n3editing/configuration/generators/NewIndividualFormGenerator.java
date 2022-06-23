0780f6fac7a4d884f888d099af2a37092329c978
Georgy Litvinov <git@litvinovg.pro>
2022-06-23 16:25:18 +0200

    fix: generators fixes

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/edit/n3editing/configuration/generators/NewIndividualFormGenerator.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/edit/n3editing/configuration/generators/NewIndividualFormGenerator.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.RDFS;
 import org.apache.jena.vocabulary.XSD;
 
@@ -219,7 +220,7 @@ public class NewIndividualFormGenerator implements EditConfigurationGenerator {
 			VitroRequest vreq, Map<String, FieldVTwo> fields) {
     	FieldVTwo field = new FieldVTwo();
     	field.setName("label");
-    	field.setRangeDatatypeUri( XSD.xstring.toString() );
+    	field.setRangeDatatypeUri( RDF.dtLangString.getURI() );
 
     	List<String> validators = new ArrayList<String>();
     	validators.add("nonempty");
