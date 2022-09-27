0df84517919aa8d2dd7adff2d505f083f2cf73d6
Georgy Litvinov <git@litvinovg.pro>
2022-09-27 21:03:33 +0200

    Report Generator component start

---
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/data/FileView.java
@@ -0,0 +1,10 @@
+package edu.cornell.mannlib.vitro.webapp.dynapi.data;
+
+import edu.cornell.mannlib.vitro.webapp.dynapi.components.Parameter;
+
+public class FileView {
+
+    public static boolean isFile(Parameter param) {
+         return false;
+    }
+}
