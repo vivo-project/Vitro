1e18858cb6603b315295ec12179ff68eb2161798
Georgy Litvinov <git@litvinovg.pro>
2022-10-04 20:07:38 +0200

    Cleanup, fixed formatting

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/data/BinaryView.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/data/BinaryView.java
@@ -9,31 +9,31 @@ import edu.cornell.mannlib.vitro.webapp.dynapi.data.types.ParameterType;
 
 public class BinaryView {
 
-    public static boolean isByteArray(Parameter param) {
-        ParameterType type = param.getType();
-        ImplementationType implType = type.getImplementationType();
-        String className = implType.getClassName().getCanonicalName();
-        if (ByteArray.class.getCanonicalName().equals(className)) {
-            return true;
-        }
-        return false;
-    }
+	public static boolean isByteArray(Parameter param) {
+		ParameterType type = param.getType();
+		ImplementationType implType = type.getImplementationType();
+		String className = implType.getClassName().getCanonicalName();
+		if (ByteArray.class.getCanonicalName().equals(className)) {
+			return true;
+		}
+		return false;
+	}
 
-    public static void setByteArray(DataStore dataStore, Parameter reportFile, ByteArrayOutputStream baos) {
-        final String name = reportFile.getName();
-        Data data;
-        if (dataStore.contains(name)) {
-            data = dataStore.getData(name);    
-        } else {
-            data = new Data(reportFile);
-            dataStore.addData(name, data);
-        }
-        data.setObject(new ByteArray(baos.toByteArray()));
-    }
+	public static void setByteArray(DataStore dataStore, Parameter reportFile, ByteArrayOutputStream baos) {
+		final String name = reportFile.getName();
+		Data data;
+		if (dataStore.contains(name)) {
+			data = dataStore.getData(name);
+		} else {
+			data = new Data(reportFile);
+			dataStore.addData(name, data);
+		}
+		data.setObject(new ByteArray(baos.toByteArray()));
+	}
 
-    public static byte[] getByteArray(DataStore dataStore, Parameter templateFile) {
-        Data data = dataStore.getData(templateFile.getName());   
-        ByteArray array = (ByteArray) data.getObject();
-        return array.get();
-    }
+	public static byte[] getByteArray(DataStore dataStore, Parameter templateFile) {
+		Data data = dataStore.getData(templateFile.getName());
+		ByteArray array = (ByteArray) data.getObject();
+		return array.get();
+	}
 }
--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/data/implementation/JsonFasterxmlNode.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/data/implementation/JsonFasterxmlNode.java
@@ -10,24 +10,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.cornell.mannlib.vitro.webapp.dynapi.data.conversion.ConversionException;
 
 public class JsonFasterxmlNode {
-    private static final Log log = LogFactory.getLog(JsonFasterxmlNode.class.getName());
-    private static ObjectMapper mapper = new ObjectMapper();
-    
-    public static JsonNode deserialize(String jsonString) throws ConversionException{
-        JsonNode node = null;
-        try {
-            node = mapper.readTree(jsonString);
-        } catch (JsonProcessingException e) {
-            log.error(e, e);
-        }
-        if (node == null) {
-            String message = "Error reading json:\n" + jsonString;
-            throw new ConversionException(message);
-        }
-        return node;
-    }
-    
-    public static String serialize(JsonNode object){
-        return object.toString();
-    }
+	private static final Log log = LogFactory.getLog(JsonFasterxmlNode.class.getName());
+	private static ObjectMapper mapper = new ObjectMapper();
+
+	public static JsonNode deserialize(String jsonString) throws ConversionException {
+		JsonNode node = null;
+		try {
+			node = mapper.readTree(jsonString);
+		} catch (JsonProcessingException e) {
+			log.error(e, e);
+		}
+		if (node == null) {
+			String message = "Error reading json:\n" + jsonString;
+			throw new ConversionException(message);
+		}
+		return node;
+	}
+
+	public static String serialize(JsonNode object) {
+		return object.toString();
+	}
 }
