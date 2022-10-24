e6ffe64edc4a334e9aed3b0889c714e01d46c8b8
Dragan Ivanovic <chenejac@uns.ac.rs>
2022-10-24 15:21:32 +0200

    Revert "caching only selected locale (not approximate matches locales)"
    This reverts commit 983d8442

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/i18n/I18n.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/i18n/I18n.java
@@ -263,7 +263,6 @@ public class I18n {
 
 		private final ServletContext ctx;
 		private final String themeDirectory;
-		private Locale localeForLoading;
 
 
 		public ThemeBasedControl(ServletContext ctx, String themeDirectory) {
@@ -326,7 +325,6 @@ public class I18n {
 				approximateMatches.remove(locale);
 			}
 			if (approximateMatches.isEmpty()) {
-				localeForLoading = (usualList!=null)?usualList.get(0):null;
 				return usualList;
 			}
 
@@ -340,20 +338,7 @@ public class I18n {
 				mergedList.addAll(rootLocaleHere, approximateMatches);
 			}
 
-			localeForLoading = (mergedList!=null)?mergedList.get(0):null;
 			return mergedList;
-		}
-
-		@Override
-		public long getTimeToLive(String baseName, Locale locale) {
-			if (baseName == null) {
-				throw new NullPointerException();
-			} else if (locale == null) {
-				throw new NullPointerException();
-			} else if ((localeForLoading) != null && (!localeForLoading.equals(locale))){
-				return ResourceBundle.Control.TTL_DONT_CACHE;
-			}
-			return ResourceBundle.Control.TTL_NO_EXPIRATION_CONTROL;
 		}
 
 		private SortedSet<Locale> findApproximateMatches(Locale locale) {
