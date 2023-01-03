74104ca9b8027b4f7a3e3750307afd2158657702
Georgy Litvinov <git@litvinovg.pro>
2023-01-03 12:14:08 +0100

    removed broken inferencing

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/DynapiModelProvider.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/DynapiModelProvider.java
@@ -1,29 +1,19 @@
 package edu.cornell.mannlib.vitro.webapp.dynapi;
 
-import java.io.StringWriter;
-import java.util.List;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.jena.ontology.OntModel;
-import org.apache.jena.rdf.model.InfModel;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.reasoner.Reasoner;
-import org.apache.jena.reasoner.rulesys.GenericRuleReasoner;
-import org.apache.jena.reasoner.rulesys.Rule;
 
 public class DynapiModelProvider {
 
 	private static DynapiModelProvider INSTANCE = new DynapiModelProvider();
 	private static final Log log = LogFactory.getLog(DynapiModelProvider.class);
-
 	private OntModel abox;
 	private OntModel tbox;
 	private OntModel permanentModel;
-	private List<Rule> rules;
-	private Reasoner reasoner;
-
+    
 	public static DynapiModelProvider getInstance() {
 		return INSTANCE;
 	}
@@ -31,45 +21,26 @@ public class DynapiModelProvider {
 	public void init(OntModel abox, OntModel tbox) {
 		this.abox = abox;
 		this.tbox = tbox;
-		initReasoner();
 	}
-
-    private void initReasoner() {
-        rules = Rule.parseRules("[rdfs9:  (?x rdfs:subClassOf ?y), (?a rdf:type ?x) -> (?a rdf:type ?y)]");
-		reasoner = new GenericRuleReasoner(rules);
-    }
 
 	public void setModel(OntModel model) {
 		this.permanentModel = model;
 	}
 
 	public Model getModel() {
-		if (permanentModel != null) {
-		    if (reasoner == null) {
-		        initReasoner();
-		    }
-		    InfModel inferencingModel = ModelFactory.createInfModel(reasoner, permanentModel);
-			return inferencingModel;
-		}
-		return constructModel();
+		return constructModelWithSparql();
 	}
-
-	private Model constructModel() {
-		Model union = ModelFactory.createUnion(abox, tbox);
-		Model memModel = ModelFactory.createDefaultModel();
-		memModel.add(union);
-		InfModel unionWithInferencing = ModelFactory.createInfModel(reasoner, memModel);
-		if (log.isDebugEnabled()) {
-			log.debug("ABox size " + abox.size());
-			log.debug("TBox size " + tbox.size());
-			log.debug("Union size " + union.size());
-			log.debug("memModel size " + memModel.size());
-			log.debug("unionWithInferencing size " + unionWithInferencing.size());
-			Model inf = unionWithInferencing.difference(memModel);
-			StringWriter writer = new StringWriter();
-			inf.write(writer, "N3");
-			log.debug(writer.toString());
-		}
-		return unionWithInferencing;
-	}
+	
+    private Model constructModelWithSparql() {
+        Model memModel;
+        if (permanentModel != null) {
+            memModel = permanentModel;
+        } else {
+            Model union = ModelFactory.createUnion(abox, tbox);
+            memModel = ModelFactory.createDefaultModel();
+            memModel.add(union);
+        }
+        return memModel;
+    }
+	
 }
