e13803785d02ad17ec8746720bd1e6eb27118e63
Georgy Litvinov <git@litvinovg.pro>
2023-01-03 12:17:38 +0100

    fix report generator integration test

--- api/src/test/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/ReportGeneratorIntegrationTest.java
+++ api/src/test/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/ReportGeneratorIntegrationTest.java
@@ -104,6 +104,7 @@ public class ReportGeneratorIntegrationTest extends ServletContextTest {
 
     public void loadOntology(OntModel ontModel) throws IOException {
         loadModel(ontModel, IMPLEMENTATION_FILE_PATH);
+        loadModel(ontModel, ONTOLOGY_FILE_PATH);
         loadModel(ontModel, getFileList(ABOX_PREFIX));
     }
 }
