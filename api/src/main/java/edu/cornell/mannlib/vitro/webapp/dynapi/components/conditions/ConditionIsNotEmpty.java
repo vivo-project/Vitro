ad46006489fba710b7fb555fac930746b3ed8db6
Georgy Litvinov <git@litvinovg.pro>
2023-01-05 17:32:53 +0100

    store dependency information in data store to check and select required subactions before action execution

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/conditions/ConditionIsNotEmpty.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/conditions/ConditionIsNotEmpty.java
@@ -1,9 +1,13 @@
 package edu.cornell.mannlib.vitro.webapp.dynapi.components.conditions;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.apache.commons.lang3.StringUtils;
 
 import edu.cornell.mannlib.vitro.webapp.dynapi.components.Parameter;
 import edu.cornell.mannlib.vitro.webapp.dynapi.components.Parameters;
+import edu.cornell.mannlib.vitro.webapp.dynapi.components.ProcedureDescriptor;
 import edu.cornell.mannlib.vitro.webapp.dynapi.data.DataStore;
 import edu.cornell.mannlib.vitro.webapp.dynapi.data.SimpleDataView;
 import edu.cornell.mannlib.vitro.webapp.utils.configuration.Property;
@@ -32,5 +36,10 @@ public class ConditionIsNotEmpty implements Condition {
     @Property(uri = "https://vivoweb.org/ontology/vitro-dynamic-api#requiresParameter", minOccurs = 1)
     public void addInputParameter(Parameter param) {
         inputParams.add(param);
+    }
+
+    @Override
+    public Map<String, ProcedureDescriptor> getDependencies() {
+        return Collections.emptyMap();
     }
 }
