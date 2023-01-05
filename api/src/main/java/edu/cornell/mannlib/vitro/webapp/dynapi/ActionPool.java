40eac663bb157b7a99431e17e9178f49a1dc24e6
Georgy Litvinov <git@litvinovg.pro>
2023-01-05 14:21:00 +0100

    Revert "renamed ActionPool to RPCPool (first commit)"
    This reverts commit 4489ffcabf640bb449dec2339ab98d81aa3ee30c.

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/ActionPool.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/ActionPool.java
@@ -3,16 +3,16 @@ package edu.cornell.mannlib.vitro.webapp.dynapi;
 import edu.cornell.mannlib.vitro.webapp.dynapi.components.Action;
 import edu.cornell.mannlib.vitro.webapp.dynapi.components.NullAction;
 
-public class RPCPool extends AbstractPool<String, Action, RPCPool> {
+public class ActionPool extends AbstractPool<String, Action, ActionPool> {
 
-    private static RPCPool INSTANCE = new RPCPool();
+    private static ActionPool INSTANCE = new ActionPool();
 
-    public static RPCPool getInstance() {
+    public static ActionPool getInstance() {
         return INSTANCE;
     }
 
     @Override
-    public RPCPool getPool() {
+    public ActionPool getPool() {
         return getInstance();
     }
 
