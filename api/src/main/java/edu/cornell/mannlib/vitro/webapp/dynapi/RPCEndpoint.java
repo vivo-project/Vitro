e5cc47eac9e6faa8974dfe9deef70dbbfccbacdf
Georgy Litvinov <git@litvinovg.pro>
2023-01-09 17:00:57 +0100

    Extracted Endpoint abstract class

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/RPCEndpoint.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/RPCEndpoint.java
@@ -2,8 +2,6 @@ package edu.cornell.mannlib.vitro.webapp.dynapi;
 
 import static edu.cornell.mannlib.vitro.webapp.dynapi.request.ApiRequestPath.RPC_SERVLET_PATH;
 
-import java.util.Map;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,23 +9,13 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import edu.cornell.mannlib.vitro.webapp.beans.UserAccount;
-import edu.cornell.mannlib.vitro.webapp.controller.VitroHttpServlet;
-import edu.cornell.mannlib.vitro.webapp.dynapi.components.Action;
 import edu.cornell.mannlib.vitro.webapp.dynapi.components.OperationResult;
-import edu.cornell.mannlib.vitro.webapp.dynapi.components.ProcedureDescriptor;
-import edu.cornell.mannlib.vitro.webapp.dynapi.data.DataStore;
-import edu.cornell.mannlib.vitro.webapp.dynapi.data.conversion.ConversionException;
-import edu.cornell.mannlib.vitro.webapp.dynapi.data.conversion.Converter;
-import edu.cornell.mannlib.vitro.webapp.dynapi.data.conversion.InitializationException;
 import edu.cornell.mannlib.vitro.webapp.dynapi.request.ApiRequestPath;
 
 @WebServlet(name = "RPCEndpoint", urlPatterns = { RPC_SERVLET_PATH + "/*" })
-public class RPCEndpoint extends VitroHttpServlet {
+public class RPCEndpoint extends Endpoint {
 
     private static final Log log = LogFactory.getLog(RPCEndpoint.class);
-
-    private ActionPool actionPool = ActionPool.getInstance();
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
@@ -37,62 +25,12 @@ public class RPCEndpoint extends VitroHttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) {
         ApiRequestPath requestPath = ApiRequestPath.from(request);
-        if (requestPath.isValid()) {
-            if (log.isDebugEnabled()) {
-                actionPool.printKeys();
-            }
-            Action action = actionPool.get(requestPath.getActionName());
-			UserAccount user = (UserAccount) request.getSession(false).getAttribute("user");
-	        if (!action.hasPermissions(user)) {
-	        	OperationResult.notAuthorized().prepareResponse(response);
-                action.removeClient();
-	        	return;
-	        } 
-            DataStore dataStore = new DataStore();
-            if (requestPath.isResourceRequest()) {
-                dataStore.setResourceID(requestPath.getResourceId());
-            }
-            try {
-                Map<String, ProcedureDescriptor> dependencies = action.getDependencies();
-                Action defaultInstance = actionPool.getDefault();
-                for (String uri : dependencies.keySet()) {
-                    Action dependency = actionPool.getByUri(uri);
-                    if (defaultInstance.equals(dependency)) {
-                        throw new InitializationException(
-                                Action.class.getSimpleName() + " dependency with uri:'" + uri + "' not found in pool.");
-                    }
-                    dataStore.putDependency(uri, dependency);
-                }
-            } catch (InitializationException e) {
-                log.error(e, e);
-                dataStore.removeDependencies();
-                action.removeClient();
-                response.setStatus(500);
-                return;
-            }
-            try {
-            	Converter.convert(request, action, dataStore);
-            } catch (Exception e) {
-            	log.error(e,e);
-                dataStore.removeDependencies();
-                action.removeClient();
-            	response.setStatus(500);
-            	return;
-            }
-            try {
-                OperationResult result = action.run(dataStore);
-                Converter.convert(response, action, result, dataStore);
-            } catch (ConversionException e) {
-            	log.error(e,e);
-            	response.setStatus(500);
-            	return;
-            } finally {
-                dataStore.removeDependencies();
-                action.removeClient();
-            }
-        } else {
+        if (!requestPath.isValid()) {
             OperationResult.notFound().prepareResponse(response);
+            return;
         }
+        String actionName = requestPath.getActionName();
+        processActionRequest(request, response, requestPath, actionName);
     }
 
     @Override
