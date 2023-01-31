d6fff64adf810130789bd9d0fef649d37c953572
Georgy Litvinov <git@litvinovg.pro>
2023-01-31 08:46:52 +0100

    Added condition isParameter

---
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/conditions/ConditionIsParameter.java
@@ -0,0 +1,43 @@
+package edu.cornell.mannlib.vitro.webapp.dynapi.components.conditions;
+
+import java.util.Collections;
+import java.util.Map;
+
+import edu.cornell.mannlib.vitro.webapp.dynapi.components.Parameter;
+import edu.cornell.mannlib.vitro.webapp.dynapi.components.Parameters;
+import edu.cornell.mannlib.vitro.webapp.dynapi.components.ProcedureDescriptor;
+import edu.cornell.mannlib.vitro.webapp.dynapi.data.DataStore;
+import edu.cornell.mannlib.vitro.webapp.dynapi.data.SimpleDataView;
+import edu.cornell.mannlib.vitro.webapp.utils.configuration.Property;
+
+public class ConditionIsParameter implements Condition {
+
+    private Parameters inputParams = new Parameters();
+    
+    @Override
+    public boolean isSatisfied(DataStore data) {
+        for (String name : SimpleDataView.getNames(inputParams)) {
+            //RawData values = data.getData(name);
+        	String value = SimpleDataView.getStringRepresentation(name, data);
+			if (!String.valueOf(true).equals(value)) {
+				return false;
+			}
+        }
+        return true;
+    }
+
+    @Override
+    public Parameters getInputParams() {
+        return inputParams;
+    }
+    
+    @Property(uri = "https://vivoweb.org/ontology/vitro-dynamic-api#requiresParameter", minOccurs = 1)
+    public void addInputParameter(Parameter param) {
+        inputParams.add(param);
+    }
+
+    @Override
+    public Map<String, ProcedureDescriptor> getDependencies() {
+        return Collections.emptyMap();
+    }
+}
