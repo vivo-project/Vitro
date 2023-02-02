d6756046de19c97f701e47e8d5bc0597ae437300
Georgy Litvinov <git@litvinovg.pro>
2023-02-02 16:03:25 +0100

    ShapeValidation draft and test

---
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/ShapesGraphPool.java
@@ -0,0 +1,51 @@
+package edu.cornell.mannlib.vitro.webapp.dynapi;
+
+import static java.lang.String.format;
+
+import java.util.concurrent.ConcurrentSkipListMap;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+public class ShapesGraphPool {
+    
+    private final Log log = LogFactory.getLog(this.getClass());
+    private static ShapesGraphPool INSTANCE = new ShapesGraphPool();
+    private static final Object mutex = new Object();
+    
+    public static ShapesGraphPool getInstance() {
+        return INSTANCE;
+    }
+    
+    private ConcurrentSkipListMap<String,ShapesGraphComponent> components = new ConcurrentSkipListMap<>();
+    
+    public ShapesGraphComponent get(String key) {
+        if (key == null) {
+            return null;
+        }
+        ShapesGraphComponent component = components.get(key);
+        if (component == null) {
+            return null;
+        }
+        return component;
+    }
+    
+    public void add(String name, ShapesGraphComponent component) {
+        log.info(format("Adding ShapesGraph with name %s", name));
+        synchronized (mutex) {
+            ShapesGraphComponent oldComponent = components.put(name, component);
+            if (oldComponent != null) {
+                log.info(format("Updated ShapesGraph with name %s", name));
+            }
+        }
+        
+    }
+
+    public void remove(String name) {
+        log.info(format("Removing ShapesGraph with name %s", name));
+        synchronized (mutex) {
+            components.remove(name);
+        }
+    }
+}
---
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/data/types/implementation/ModelParam.java
@@ -0,0 +1,70 @@
+package edu.cornell.mannlib.vitro.webapp.dynapi.data.types.implementation;
+
+import edu.cornell.mannlib.vitro.webapp.dynapi.components.Parameter;
+import edu.cornell.mannlib.vitro.webapp.dynapi.components.serialization.PrimitiveSerializationType;
+import edu.cornell.mannlib.vitro.webapp.dynapi.data.implementation.DynapiInMemoryOntModel;
+import edu.cornell.mannlib.vitro.webapp.dynapi.data.implementation.DynapiModelFactory;
+import edu.cornell.mannlib.vitro.webapp.dynapi.data.types.ImplementationConfig;
+import edu.cornell.mannlib.vitro.webapp.dynapi.data.types.ImplementationType;
+import edu.cornell.mannlib.vitro.webapp.dynapi.data.types.ParameterType;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.jena.rdf.model.Model;
+
+public class ModelParam extends Parameter {
+
+	private static final Log log = LogFactory.getLog(ModelParam.class);
+
+	public ModelParam(String name, boolean internal, boolean autocreate) {
+		this.setName(name);
+		try {
+			ParameterType type = new ParameterType();
+			type.setIsInternal(autocreate);
+			if (internal) {
+			    type.setName("internal model");
+			} else {
+			    type.setName("model");    
+			}
+			ImplementationType implType = new ImplementationType();
+			type.setImplementationType(implType);
+			PrimitiveSerializationType serializationType = new PrimitiveSerializationType();
+			serializationType.setName("string");
+            type.setSerializationType(serializationType);
+			implType.setSerializationConfig(getSerializationConfig());
+			implType.setDeserializationConfig(getDeserializationConfig(internal));	
+			implType.setClassName(Model.class.getCanonicalName());
+			if (internal) {
+			    this.setDefaultValue(name);
+			}
+			this.setType(type);
+		} catch (Exception e) {
+			log.error(e, e);
+			throw new RuntimeException(e.getLocalizedMessage());
+		}
+	}
+	
+	private ImplementationConfig getSerializationConfig() throws ClassNotFoundException {
+		ImplementationConfig serializationConfig = new ImplementationConfig();
+		serializationConfig.setClassName(DynapiInMemoryOntModel.class.getCanonicalName());
+		serializationConfig.setMethodName("serialize");
+		serializationConfig.setMethodArguments("input");
+		serializationConfig.setStaticMethod(true);
+		return serializationConfig;
+	}
+	
+	private ImplementationConfig getDeserializationConfig(boolean internal) throws ClassNotFoundException {
+		ImplementationConfig serializationConfig = new ImplementationConfig();
+		if (internal) {
+		      serializationConfig.setClassName(DynapiModelFactory.class.getCanonicalName());
+              serializationConfig.setMethodName("getModel");
+		} else {
+		      serializationConfig.setClassName(DynapiInMemoryOntModel.class.getCanonicalName());
+		      serializationConfig.setMethodName("deserialize");
+		}
+		serializationConfig.setMethodArguments("input");
+		serializationConfig.setStaticMethod(true);
+		
+		return serializationConfig;
+	}
+}
