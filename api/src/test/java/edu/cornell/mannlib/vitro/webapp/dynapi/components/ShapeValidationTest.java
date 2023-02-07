d3cf6242ec5d2cbf35d8a435c7292fd6784b03cb
Georgy Litvinov <git@litvinovg.pro>
2023-02-07 14:29:29 +0100

    Shape validation test improvements

--- api/src/test/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/ShapeValidationTest.java
+++ api/src/test/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/ShapeValidationTest.java
@@ -1,12 +1,17 @@
 package edu.cornell.mannlib.vitro.webapp.dynapi.components;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mockStatic;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.ontology.impl.OntModelImpl;
@@ -16,9 +21,10 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.mockito.MockedStatic;
 
-import edu.cornell.mannlib.vitro.webapp.dynapi.ProcedurePool;
 import edu.cornell.mannlib.vitro.webapp.dynapi.ServletContextTest;
 import edu.cornell.mannlib.vitro.webapp.dynapi.ShapesGraphPool;
 import edu.cornell.mannlib.vitro.webapp.dynapi.data.DataStore;
@@ -31,25 +37,56 @@ import edu.cornell.mannlib.vitro.webapp.dynapi.data.types.implementation.Boolean
 import edu.cornell.mannlib.vitro.webapp.dynapi.data.types.implementation.ModelParam;
 import edu.cornell.mannlib.vitro.webapp.dynapi.data.types.implementation.StringParam;
 
+@RunWith(Parameterized.class)
 public class ShapeValidationTest extends ServletContextTest {
+
+    private static final String RESULT_PARAM_NAME = "result";
+    private static final String REPORT_PARAM_NAME = "report";
+    private static final String PROCEDURE_PROVIDES_SHAPES_URI = "test:procedure-provides-shapes";
     
-    private static final String RESOURCES_PATH = "src/test/resources/edu/cornell/mannlib/vitro/webapp/dynapi/components/";
+    //private static final String TEST_RESOURCES_PATH = "src/test/resources/edu/cornell/mannlib/vitro/webapp/dynapi/components/";
+    
     private static final String DYNAPI_ABOX_URI = "http://vitro.mannlib.cornell.edu/default/dynamic-api-abox";
     private static final String SHAPES_MODEL_URI = "http://vitro.mannlib.cornell.edu/default/shapes";
     public static final String SHAPES_PREFIX = "../home/src/main/resources/rdf/shapes/everytime/";
-    private static final String PROCEDURE_FILE_NAME = RESOURCES_PATH + "shape-validation-empty-procedure.n3";
-
     private static final String SHAPES_MODEL_FILE = SHAPES_PREFIX + "dynamic_api_shapes.n3";
-    
+    private static final List<String> shapeFile = Arrays.asList(new String[] {SHAPES_MODEL_FILE});
+
+    @org.junit.runners.Parameterized.Parameter(0)
+    public  List<String> shapeFiles;
+
+    @org.junit.runners.Parameterized.Parameter(1)
+    public List<String> modelFiles;
+
+    @org.junit.runners.Parameterized.Parameter(2)
+    public boolean cache;
+
+    @org.junit.runners.Parameterized.Parameter(3)
+    public boolean validateShapes;
+
+    @org.junit.runners.Parameterized.Parameter(4)
+    public boolean details;
+
+    static List<String> dynapiFiles = Arrays.asList(
+            ArrayUtils.addAll(
+                    ArrayUtils.addAll(getFileList(ABOX_PREFIX), getFileList(TBOX_PREFIX)),
+                    new String[] { 
+                            IMPLEMENTATION_FILE_PATH, 
+                            ONTOLOGY_FILE_PATH }
+                    )
+            );
+
+    boolean manualDebugging = false;
+
     private static MockedStatic<DynapiModelFactory> dynapiModelFactory;
     OntModel shapesModel = ModelFactory.createOntologyModel();
-    
+
     @AfterClass
     public static void after() {
         restoreLogs();
         dynapiModelFactory.close();
     }
-    
+
     @BeforeClass
     public static void before() {
         offLogs();
@@ -61,97 +98,109 @@ public class ShapeValidationTest extends ServletContextTest {
         shapesModel = new OntModelImpl(OntModelSpec.OWL_MEM);
         dynapiModelFactory.when(() -> DynapiModelFactory.getModel(eq(DYNAPI_ABOX_URI))).thenReturn(ontModel);
         dynapiModelFactory.when(() -> DynapiModelFactory.getModel(eq(SHAPES_MODEL_URI))).thenReturn(shapesModel);
+        assertEquals(0, ShapesGraphPool.getInstance().count());
     }
-    private ProcedurePool initWithDefaultModel() throws IOException {
-        loadOntology(ontModel);
-        ProcedurePool procedurePool = ProcedurePool.getInstance();
-        procedurePool.init(servletContext);
-        return procedurePool;
-    }
-    
+
     @Test
     public void validationWithProvidedShapes() throws InitializationException, ConversionException, IOException {
-        
-        boolean manualDebugging = false;
-        ProcedurePool pool = initWithDefaultModel();
-        
+        loadOntology(ontModel);
         ShapeValidation sv = new ShapeValidation();
         Parameter dataModelParam = new ModelParam(DYNAPI_ABOX_URI, true, true);
         Parameter shapesModelParam = new ModelParam(SHAPES_MODEL_URI, true, true);
-        Parameter reportParam = new StringParam("report");
-        Parameter resultParam = new BooleanParam("result");
+        Parameter reportParam = new StringParam(REPORT_PARAM_NAME);
+        Parameter resultParam = new BooleanParam(RESULT_PARAM_NAME);
         sv.setDataModel(dataModelParam);
         sv.setShapesModel(shapesModelParam);
         sv.addOutputParam(reportParam);
         sv.addOutputParam(resultParam);
-        sv.setDetails(true);
-        sv.setCache(true);
-        sv.setValidateShapes(false);
+        sv.setDetails(details);
+        sv.setCache(cache);
+        sv.setValidateShapes(validateShapes);
         DataStore dataStore = new DataStore();
         Converter.convertInternalParams(sv.getInputParams(), dataStore);
         assertTrue(OperationResult.ok().equals(sv.run(dataStore)));
-        assertTrue(dataStore.contains("report"));
-        assertTrue(dataStore.contains("result"));
+        assertTrue(dataStore.contains(REPORT_PARAM_NAME));
+        assertTrue(dataStore.contains(RESULT_PARAM_NAME));
         if (manualDebugging) {
-            String report = (String) TestView.getObject(dataStore.getData("report"));
+            String report = (String) TestView.getObject(dataStore.getData(REPORT_PARAM_NAME));
             System.out.println(report);
+        }
+        if (cache) {
+            assertEquals(1, ShapesGraphPool.getInstance().count());
+        } else {
+            assertEquals(0, ShapesGraphPool.getInstance().count());
         }
         ShapesGraphPool.getInstance().clear();
     }
-    
+
     @Test
     public void validationWithProcedureInvocation() throws InitializationException, ConversionException, IOException {
-        
-        boolean manualDebugging = false;
-        ProcedurePool pool = initWithDefaultModel();
+        loadOntology(ontModel);
         ShapeValidation sv = new ShapeValidation();
         Parameter dataModelParam = new ModelParam(DYNAPI_ABOX_URI, true, true);
         Parameter shapesModelParam = new ModelParam(SHAPES_MODEL_URI, true, true);
-        
         ProcedureDescriptor pd = new ProcedureDescriptor();
         pd.addOutputParameter(shapesModelParam);
-        pd.setCallUri("test:procedure-provides-shapes");
+        pd.setCallUri(PROCEDURE_PROVIDES_SHAPES_URI);
         sv.setShapesProcedureDescriptor(pd);
-        
-        Parameter reportParam = new StringParam("report");
-        Parameter resultParam = new BooleanParam("result");
+        Parameter reportParam = new StringParam(REPORT_PARAM_NAME);
+        Parameter resultParam = new BooleanParam(RESULT_PARAM_NAME);
         sv.setDataModel(dataModelParam);
         sv.addOutputParam(reportParam);
         sv.addOutputParam(resultParam);
-        sv.setDetails(true);
-        sv.setCache(true);
-        sv.setValidateShapes(false);
+        sv.setDetails(details);
+        sv.setCache(cache);
+        sv.setValidateShapes(validateShapes);
         DataStore dataStore = new DataStore();
         Converter.convertInternalParams(sv.getInputParams(), dataStore);
-        try (Procedure shapesProcedure = pool.get("test:procedure-provides-shapes")){
-            dataStore.putDependency("test:procedure-provides-shapes", shapesProcedure);
-            assertTrue(OperationResult.ok().equals(sv.run(dataStore)));    
-        }finally {
-            dataStore.removeDependencies();
-        }
-        
-        assertTrue(dataStore.contains("report"));
-        assertTrue(dataStore.contains("result"));
+        Procedure shapesProcedure = new Procedure();
+        shapesProcedure.setUri(PROCEDURE_PROVIDES_SHAPES_URI);
+        shapesProcedure.addProvidedParameter(shapesModelParam);
+        assertTrue(shapesProcedure.isValid());
+        dataStore.putDependency(PROCEDURE_PROVIDES_SHAPES_URI, shapesProcedure);
+        assertTrue(OperationResult.ok().equals(sv.run(dataStore)));
+        assertTrue(dataStore.contains(REPORT_PARAM_NAME));
+        assertTrue(dataStore.contains(RESULT_PARAM_NAME));
         if (manualDebugging) {
-            String report = (String) TestView.getObject(dataStore.getData("report"));
+            String report = (String) TestView.getObject(dataStore.getData(REPORT_PARAM_NAME));
             System.out.println(report);
+        }
+        if (cache) {
+            assertEquals(1, ShapesGraphPool.getInstance().count());
+        } else {
+            assertEquals(0, ShapesGraphPool.getInstance().count());
         }
         ShapesGraphPool.getInstance().clear();
     }
-    
+
     protected void loadModel(Model model, String... files) throws IOException {
         for (String file : files) {
             String rdf = readFile(file);
             model.read(new StringReader(rdf), null, "n3");
         }
     }
-    
+
     public void loadOntology(OntModel ontModel) throws IOException {
-        loadModel(ontModel, IMPLEMENTATION_FILE_PATH);
-        loadModel(ontModel, ONTOLOGY_FILE_PATH);
+        for (String filePath : modelFiles) {
+            loadModel(ontModel, filePath);
+        }
         loadModel(ontModel, getFileList(ABOX_PREFIX));
         loadModel(ontModel, getFileList(TBOX_PREFIX));
-        loadModel(shapesModel, SHAPES_MODEL_FILE);
-        loadModel(ontModel, PROCEDURE_FILE_NAME);
+        for (String filePath : shapeFiles) {
+            loadModel(shapesModel, filePath);
+        }
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> requests() {
+        return Arrays.asList(new Object[][] { 
+            //Shape files, data files, cache, shape validation, details
+                { shapeFile, dynapiFiles, true, true, true },
+                { shapeFile, dynapiFiles, false, true, true }, 
+                { shapeFile, dynapiFiles, true, false, true },
+                { shapeFile, dynapiFiles, false, false, true }, 
+                { shapeFile, dynapiFiles, true, false, false },
+                { shapeFile, dynapiFiles, false, false, false }, 
+        });
     }
 }
