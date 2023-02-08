a60c38703d31bf1297a5a9e6815fba1ffae285b8
Georgy Litvinov <git@litvinovg.pro>
2023-02-08 09:56:25 +0100

    created error step

---
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/ErrorStep.java
@@ -0,0 +1,65 @@
+package edu.cornell.mannlib.vitro.webapp.dynapi.components;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+import edu.cornell.mannlib.vitro.webapp.dynapi.computation.StepInfo;
+import edu.cornell.mannlib.vitro.webapp.dynapi.data.DataStore;
+import edu.cornell.mannlib.vitro.webapp.dynapi.data.conversion.InitializationException;
+import edu.cornell.mannlib.vitro.webapp.utils.configuration.Property;
+
+public class ErrorStep implements Step{
+
+    OperationResult result = null;
+    @Override
+    public OperationResult run(DataStore dataStore) {
+        return result;
+    }
+
+    @Property(uri = "https://vivoweb.org/ontology/vitro-dynamic-api#errorCode", minOccurs = 1, maxOccurs = 1)
+    public void setMaxLength(int input) throws InitializationException {
+        if (!OperationResult.hasError(input)) {
+            throw new InitializationException(String.format("Code '%s' is not an error. Error range is between 400 and 599", input));
+        }
+        result = new OperationResult(input);
+    }
+    
+    @Override
+    public void dereference() {}
+
+    @Override
+    public Set<StepInfo> getNextNodes() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public boolean isRoot() {
+        return false;
+    }
+
+    @Override
+    public boolean isOptional() {
+        return false;
+    }
+
+    @Override
+    public Parameters getInputParams() {
+        return new Parameters();
+    }
+
+    @Override
+    public Parameters getOutputParams() {
+        return new Parameters();
+    }
+
+    @Override
+    public Map<String, ProcedureDescriptor> getDependencies() {
+        return Collections.emptyMap();
+    }
+    
+    public boolean isValid() {
+        return result != null;
+    }
+
+}
