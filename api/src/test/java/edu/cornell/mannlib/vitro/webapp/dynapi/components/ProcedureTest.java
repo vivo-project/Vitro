1e721e80123ce8cea344b51f0f6d63cfcad8df52
Georgy Litvinov <git@litvinovg.pro>
2023-02-10 17:39:29 +0100

    added optional param test

--- api/src/test/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/ProcedureTest.java
+++ api/src/test/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/ProcedureTest.java
@@ -1,5 +1,6 @@
 package edu.cornell.mannlib.vitro.webapp.dynapi.components;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -11,10 +12,18 @@ import edu.cornell.mannlib.vitro.webapp.beans.UserAccount;
 import edu.cornell.mannlib.vitro.webapp.dynapi.access.GroupAccessWhitelist;
 import edu.cornell.mannlib.vitro.webapp.dynapi.access.UserAccessWhitelist;
 import edu.cornell.mannlib.vitro.webapp.dynapi.access.UserGroup;
+import edu.cornell.mannlib.vitro.webapp.dynapi.data.Data;
+import edu.cornell.mannlib.vitro.webapp.dynapi.data.DataStore;
+import edu.cornell.mannlib.vitro.webapp.dynapi.data.TestView;
+import edu.cornell.mannlib.vitro.webapp.dynapi.data.conversion.ConversionException;
+import edu.cornell.mannlib.vitro.webapp.dynapi.data.conversion.Converter;
+import edu.cornell.mannlib.vitro.webapp.dynapi.data.types.implementation.BooleanParam;
 
 public class ProcedureTest {
 
-   private static final String USER_GROUP_URI = "user_group_uri";
+   private static final String OUTPUT = "output";
+private static final String INPUT = "input";
+private static final String USER_GROUP_URI = "user_group_uri";
 
    @Test
    public void testDefaultAccess() {
@@ -63,6 +72,68 @@ public class ProcedureTest {
            assertFalse(procedure.hasPermissions(user));
            user.setEmailAddress("user_email@example.com");
            assertTrue(procedure.hasPermissions(user));
+       }
+   }
+   
+   @Test
+   public void simpleParamTest() {
+       try (Procedure procedure = new Procedure()) {
+           OperationalStep step = new OperationalStep();
+           BooleanParam inputParam = new BooleanParam(INPUT);
+           BooleanParam outputParam = new BooleanParam(OUTPUT);
+           AssignOperation ao = new AssignOperation();
+           ao.addInputParameter(inputParam);
+           ao.addOutputParameter(outputParam);
+           procedure.setStep(step);
+           procedure.addProvidedParameter(outputParam);           
+           step.setOperation(ao);
+           procedure.setUri("test_rpc");
+           procedure.setPublicAccess(true);
+           DataStore store = new DataStore();
+           Data inputData = new Data(inputParam);
+           TestView.setObject(inputData, true);
+           store.addData(INPUT, inputData);
+           assertTrue(procedure.isValid());
+           assertEquals(OperationResult.ok(),procedure.run(store));
+           assertTrue(store.contains(OUTPUT));
+           Data outputData = store.getData(OUTPUT);
+           assertEquals("true",outputData.getSerializedValue());
+       }
+   }
+   
+   @Test
+   public void optionalParamTest() throws ConversionException {
+       try (Procedure procedure = new Procedure()) {
+           OperationalStep step = new OperationalStep();
+           BooleanParam inputParam = new BooleanParam(INPUT);
+           inputParam.setOptional(true);
+           BooleanParam outputParam = new BooleanParam(OUTPUT);
+           AssignOperation ao = new AssignOperation();
+           ao.addInputParameter(inputParam);
+           ao.addOutputParameter(outputParam);
+           procedure.setStep(step);
+           procedure.addProvidedParameter(outputParam);           
+           step.setOperation(ao);
+           procedure.setUri("test_rpc");
+           procedure.setPublicAccess(true);
+           DataStore store = new DataStore();
+           assertTrue(procedure.isValid());
+           Converter.convertInternalParams(procedure.getInternalParams(), store);
+           assertEquals(1, procedure.getOptionalParams().size());
+           assertEquals(OperationResult.ok(),procedure.run(store));
+           assertTrue(store.contains(OUTPUT));
+           Data outputData = store.getData(OUTPUT);
+           assertEquals("false",outputData.getSerializedValue());
+           
+           //Provide optional param
+           store = new DataStore();
+           Data inputData = new Data(inputParam);
+           TestView.setObject(inputData, true);
+           store.addData(INPUT, inputData);
+           assertEquals(OperationResult.ok(),procedure.run(store));
+           assertTrue(store.contains(OUTPUT));
+           outputData = store.getData(OUTPUT);
+           assertEquals("true",outputData.getSerializedValue());
        }
    }
 }
