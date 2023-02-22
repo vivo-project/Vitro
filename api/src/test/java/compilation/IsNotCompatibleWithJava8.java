def38906666fcfdc4e81ba89718b46710a1143d4
michel-heon <heon.michel@uqam.ca>
2023-02-22 19:04:31 +0000

    ""

--- api/src/test/java/compilation/IsNotCompatibleWithJava8.java
+++ api/src/test/java/compilation/IsNotCompatibleWithJava8.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class IsNotCompatibleWithJava8 {
-
+/*
+ * This code segment is compatible with Java 11 and not compatible with java 8. It will abort the compilation if the Java context is version 8
+ */
     @Test
     public void test() {
         String value = "This code is compiled in Java 11";
