0f8df3597a3678797950c093b775110c5355459c
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2023-03-17 16:38:34 +0100

    Added initial dump-restore script.

---
+++ home/src/main/resources/scripts/dumpRestore.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+action="$1"
+models="$2"
+email="$3"
+password="$4"
+
+host="http://localhost:8080" # URL where the VIVO instance is hosted
+purge="true"                 # Will the restoration process purge the models before restoring
+restoration_files="."        # Directory containing the files used for restoration
+dumped_files="."             # Directory containing the backed-up files
+app_name="vivo"              # app-name parameter
+
+session=$(mktemp -d)
+
+url="$host/$app_name/authenticate"
+
+loginName="email@email.com"
+loginPassword="password"
+loginForm="Log in"
+
+# Log in and get session cookies
+curl --cookie-jar "$session/cookies.txt" -d "loginName=$loginName" -d "loginPassword=$loginPassword" -d "loginForm=$loginForm" $url
+
+if [[ "$action" == "dump" ]]; then
+    echo "Starting dump..."
+    curl --cookie "$session/cookies.txt" "$host/$app_name/dumpRestore/dump/$models.nq?which=$models" -o "$dumped_files/$models.nq"
+    echo "Completed successfully."
+elif [[ "$action" == "restore" ]]; then
+    echo "Starting restoration process..."
+
+    url="$host/$app_name/dumpRestore/restore"
+    files=$(echo -n "$restoration_files/$models.nq")
+    params=$(echo -n "which=$models&purge=$purge")
+
+    curl --cookie "$session/cookies.txt" -X POST -F "sourceFile=@$files" "$url?$params" > /dev/null
+
+    if [[ $? -eq 0 ]]; then
+        echo "Restored successfully."
+    else
+        echo "Something went wrong."
+    fi
+fi
+
+rm -rf "$session"
