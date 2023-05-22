9290304c0842592afb9cadb631f96fa8188f706a
Georgy Litvinov <git@litvinovg.pro>
2023-05-22 08:11:30 +0200

    Added simple substitutions to sparql query component

--- api/src/test/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/SparqlSelectQueryTest.java
+++ api/src/test/java/edu/cornell/mannlib/vitro/webapp/dynapi/components/SparqlSelectQueryTest.java
@@ -26,9 +26,12 @@ public class SparqlSelectQueryTest {
 	private static final String S_VAR = "s";
 	private static final String O_VAR = "o";
 	private static final String ALL_VAR = "all";
+	private static final String LIMIT_VAR = "limit";
+    private static final String ORDER_VAR = "order";
 	private static final String QUERY_NO_VARS = "SELECT ?str WHERE { BIND(\"test\" as ?str) } ";
 	private static final String QUERY_OBJ_VAR = "SELECT ?s WHERE { ?s <test:property> ?str . } ";
 	private static final String QUERY_SUBJ_VAR = "SELECT ?o WHERE { ?str <test:property> ?o . } ";
+    private static final String QUERY_LIMIT_ORDER = "SELECT ?limit_o WHERE { ?str <test:property> ?limit_o . } ORDER BY ?order(?limit_o) LIMIT ?limit "; 
 	private static final String QUERY_LABEL = "SELECT ?o WHERE { ?str <http://www.w3.org/2000/01/rdf-schema#label> ?o . } ";
 
 	private OntModelImpl model;
@@ -183,5 +186,48 @@ public class SparqlSelectQueryTest {
 					JsonView.getJsonString(dataStore, outParam));
 		}
 	}
+	
+   @Test
+    public void orderWithLimit() throws Exception {
+        sparql.setQueryText(QUERY_LIMIT_ORDER);
+        Parameter modelParam = ParameterUtils.createModelParameter(MODEL);
+        sparql.setQueryModel(modelParam);
+        Data modelData = new Data(modelParam);
+        ParameterUtils.addStatement(model, "test:resource", "test:property", "alice");
+        ParameterUtils.addStatement(model, "test:resource", "test:property", "bob");
+        TestView.setObject(modelData, model);
+        dataStore.addData(MODEL, modelData);
+
+        Parameter limitParam = ParameterUtils.createStringLiteralParameter(LIMIT_VAR);
+        Data limitData = new Data(limitParam);
+        TestView.setObject(limitData, "1");
+        dataStore.addData(LIMIT_VAR, limitData);
+        sparql.addInputSubstitutionParameter(limitParam);
+        
+        Parameter orderParam = ParameterUtils.createStringLiteralParameter(ORDER_VAR);
+        Data orderData = new Data(orderParam);
+        TestView.setObject(orderData, "ASC");
+        dataStore.addData(ORDER_VAR, orderData);
+        sparql.addInputSubstitutionParameter(orderParam);
+
+        Parameter strParam = ParameterUtils.createUriParameter(STR_VAR);
+        sparql.addInputParameter(strParam);
+        // Not enough params
+        assertEquals(OperationResult.internalServerError(), sparql.run(dataStore));
+        Data strData = new Data(strParam);
+        TestView.setObject(strData, "test:resource");
+        dataStore.addData(STR_VAR, strData);
+        assertEquals(OperationResult.ok(), sparql.run(dataStore));
+        assertFalse(dataStore.contains(ALL_VAR));
+        Parameter outParam = ParameterUtils.createJsonParameter(ALL_VAR);
+        sparql.addOutputParameter(outParam);
+        assertEquals(OperationResult.ok(), sparql.run(dataStore));
+        assertTrue(dataStore.contains(ALL_VAR));
+        if (dataStore.contains(ALL_VAR)) {
+            assertEquals(
+                    "{\"head\":{\"vars\":[\"limit_o\"]},\"results\":{\"bindings\":[{\"limit_o\":{\"type\":\"literal\",\"value\":\"alice\"}}]}}",
+                    JsonView.getJsonString(dataStore, outParam));
+        }
+    }
 
 }
