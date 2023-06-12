0bb54a6d0709b03107089e57c19d6f97de9d80a4
Georgy Litvinov <git@litvinovg.pro>
2023-06-12 08:09:31 +0200

    arm migration

---
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/migration/auth/ArmMigrator.java
@@ -0,0 +1,255 @@
+package edu.cornell.mannlib.vitro.webapp.migration.auth;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.jena.query.ParameterizedSparqlString;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+
+import edu.cornell.mannlib.vitro.webapp.auth.attributes.AccessObjectType;
+import edu.cornell.mannlib.vitro.webapp.auth.attributes.OperationGroup;
+import edu.cornell.mannlib.vitro.webapp.auth.policy.EntityPolicyController;
+import edu.cornell.mannlib.vitro.webapp.auth.policy.PolicyLoader;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames;
+import edu.cornell.mannlib.vitro.webapp.rdfservice.RDFService;
+import edu.cornell.mannlib.vitro.webapp.rdfservice.RDFServiceException;
+import edu.cornell.mannlib.vitro.webapp.rdfservice.impl.RDFServiceUtils;
+
+public class ArmMigrator {
+    
+    private static final Log log = LogFactory.getLog(ArmMigrator.class);
+
+    protected static String DISPLAY = "Display";
+    protected static String UPDATE = "Update";
+    protected static String PUBLISH = "Publish";
+    
+    protected static final String ARM_ADMIN = "ADMIN";
+    protected static final String ARM_CURATOR = "CURATOR";
+    protected static final String ARM_EDITOR  = "EDITOR";
+    protected static final String ARM_SELF_EDITOR = "SELF_EDITOR";
+    protected static final String ARM_PUBLIC = "PUBLIC";
+
+    protected static final List<String> armRoles = Arrays.asList(ARM_ADMIN, ARM_CURATOR, ARM_EDITOR, ARM_SELF_EDITOR, ARM_PUBLIC);
+    protected static final List<String> armOperations = Arrays.asList(DISPLAY, UPDATE, PUBLISH);
+    protected static final List<AccessObjectType> entityTypes = Arrays.asList(
+            AccessObjectType.CLASS,
+            AccessObjectType.OBJECT_PROPERTY,
+            AccessObjectType.DATA_PROPERTY,
+            AccessObjectType.FAUX_OBJECT_PROPERTY,
+            AccessObjectType.FAUX_DATA_PROPERTY
+            );
+
+    private RDFService contentRdfService;
+    private RDFService configurationRdfService;
+
+    private String VALUE_QUERY = ""
+            + "SELECT ?uri \n"
+            + "WHERE {\n"
+            + "  GRAPH <" + ModelNames.USER_ACCOUNTS + "> {\n"
+            + "  ?permission <http://vitro.mannlib.cornell.edu/ns/vitro/authorization#forEntity> ?uri . \n"
+            + "  }\n"
+            + "}";
+
+    private static Map<String, String> roleMap;
+    private static Map<String, OperationGroup> operationMap;
+    
+    public ArmMigrator(RDFService contentRdfService, RDFService configurationRdfService) {
+        this.contentRdfService = contentRdfService;
+        this.configurationRdfService = configurationRdfService;  
+        operationMap = new HashMap<>();
+        operationMap.put(DISPLAY, OperationGroup.DISPLAY_GROUP);
+        operationMap.put(UPDATE, OperationGroup.UPDATE_GROUP);
+        operationMap.put(PUBLISH, OperationGroup.PUBLISH_GROUP);
+        roleMap = new HashMap<>();
+        roleMap.put(ARM_ADMIN, AuthMigrator.ROLE_ADMIN_URI);
+        roleMap.put(ARM_CURATOR, AuthMigrator.ROLE_CURATOR_URI);
+        roleMap.put(ARM_EDITOR, AuthMigrator.ROLE_EDITOR_URI);
+        roleMap.put(ARM_SELF_EDITOR, AuthMigrator.ROLE_SELF_EDITOR_URI);
+        roleMap.put(ARM_PUBLIC, AuthMigrator.ROLE_PUBLIC_URI);
+    }
+
+    public static String getArmPermissionSubject(String operation, String role) {
+        return "java:edu.cornell.mannlib.vitro.webapp.auth.permissions.Entity" + operation + "Permission#" + role ;
+    }
+
+    public void migrateConfiguration() {
+        cleanEntityDataSetValues();
+        Map<AccessObjectType,Set<String>> entityTypeMap = getEntityMap();
+        StringBuilder additions = new StringBuilder();
+        collectAdditions(entityTypeMap, additions);
+        PolicyLoader.getInstance().updateAccessControlModel(additions.toString(), true);
+    }
+
+    private void cleanEntityDataSetValues() {
+        StringBuilder removals = getStatementsToRemove();
+        PolicyLoader.getInstance().updateAccessControlModel(removals.toString(), false);
+    }
+
+    protected StringBuilder getStatementsToRemove() {
+        StringBuilder removals = new StringBuilder();
+        for (String role : armRoles) {
+            String newRole = roleMap.get(role);
+            for (String operation : armOperations) {
+                OperationGroup og = operationMap.get(operation);
+                for (AccessObjectType aot : entityTypes) {
+                    Set<String> entityUris = PolicyLoader.getInstance().getPolicyDataSetValues(og, aot, newRole);
+                    for( String entityUri : entityUris) {
+                        EntityPolicyController.getDataValueStatements(entityUri, aot, og, Collections.singleton(newRole), removals);    
+                    }
+                }
+            }
+        }
+        return removals;
+    }
+
+    protected void collectAdditions(Map<AccessObjectType, Set<String>> entityTypeMap, StringBuilder additions) {
+        for (String role : armRoles) {
+            String newRole = roleMap.get(role);
+            for (String operation : armOperations) {
+                String permissionUri = getArmPermissionSubject(operation, role);
+                Set<String> entities = getArmEntites(permissionUri);
+                OperationGroup og = operationMap.get(operation);
+                for (AccessObjectType aot : entityTypes) {
+                    if (entities.isEmpty()) {
+                        continue;
+                    }
+                    Set<String> retainEntitities = entityTypeMap.get(aot);
+                    if (retainEntitities.isEmpty()) {
+                        continue;
+                    }
+                    HashSet<String> typeEntities = new HashSet<>(entities);
+                    typeEntities.retainAll(retainEntitities);
+                    for (String entityUri : typeEntities) {
+                        EntityPolicyController.getDataValueStatements(entityUri, aot, og, Collections.singleton(newRole), additions);    
+                    }
+                }
+            }
+        }
+    }
+    
+    protected Map<AccessObjectType, Set<String>> getEntityMap() {
+        Map<AccessObjectType, Set<String>> map = new HashMap<>();
+        for (AccessObjectType type : entityTypes) {
+            addEntitiesForType(type, map);
+        } 
+        return map;
+    }
+
+    private void addEntitiesForType(AccessObjectType type, Map<AccessObjectType, Set<String>> map) {
+        Set<String> entities = new HashSet<>();
+        String queryText = getQueryText(type);
+        RDFService service = getRdfService(type);
+        try {
+            ResultSet rs = RDFServiceUtils.sparqlSelectQuery(queryText, service);
+            while (rs.hasNext()) {
+                QuerySolution qs = rs.next();
+                String entity = qs.getResource("uri").getURI();
+                if (AccessObjectType.FAUX_DATA_PROPERTY.equals(type)){
+                    String baseUri = qs.getResource("base").getURI();
+                    Set<String> set = map.get(AccessObjectType.DATA_PROPERTY);
+                    if (!set.contains(baseUri)) {
+                        continue;
+                    }
+                }
+                if (AccessObjectType.FAUX_OBJECT_PROPERTY.equals(type)){
+                    String baseUri = qs.getResource("base").getURI();
+                    Set<String> set = map.get(AccessObjectType.OBJECT_PROPERTY);
+                    if (!set.contains(baseUri)) {
+                        continue;
+                    }
+                }
+                entities.add(entity);
+            }
+        } catch (Exception e) {
+            log.error(e, e);
+        }
+        map.put(type, entities);
+    }
+    
+    private String getQueryText(AccessObjectType type) {
+        String fauxTypeSpecificPatterns = ""
+        + "GRAPH <" + ModelNames.DISPLAY + "> {"
+        + "  ?context <http://vitro.mannlib.cornell.edu/ns/vitro/ApplicationConfiguration#configContextFor> ?base .\n"
+        + "  ?context <http://vitro.mannlib.cornell.edu/ns/vitro/ApplicationConfiguration#hasConfiguration> ?uri .\n"
+        + "  ?uri rdf:type <http://vitro.mannlib.cornell.edu/ns/vitro/ApplicationConfiguration#ObjectPropertyDisplayConfig> .\n"
+        + "}";
+        switch (type) {
+        case FAUX_OBJECT_PROPERTY:
+            return getQuery(fauxTypeSpecificPatterns);
+        case FAUX_DATA_PROPERTY:
+            return getQuery(fauxTypeSpecificPatterns);
+        case CLASS:
+            return getQuery("{?uri rdf:type owl:Class .}\n");
+        case DATA_PROPERTY:
+            return getQuery("{?uri rdf:type owl:DatatypeProperty .}\n");
+        default:
+            return getQuery("{?uri rdf:type owl:ObjectProperty .}\n");
+        } 
+    }
+    
+    private static String getQuery(String typePatterns) {
+        return ""
+                + "PREFIX vitro: <http://vitro.mannlib.cornell.edu/ns/vitro/0.7#> \n"
+                + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+                + "PREFIX owl: <http://www.w3.org/2002/07/owl#> \n"
+                + "SELECT ?base ?uri \n"
+                + "WHERE {\n"
+                + typePatterns
+                + "} \n";
+    }
+
+    private RDFService getRdfService(AccessObjectType type) {
+        if (AccessObjectType.FAUX_OBJECT_PROPERTY.equals(type) ||
+            AccessObjectType.FAUX_DATA_PROPERTY.equals(type)) {
+            return configurationRdfService;
+        } 
+        return contentRdfService;
+    }
+
+    private Set<String> getArmEntites(String permissionUri) {
+        Set<String> entities = new HashSet<>();
+        ParameterizedSparqlString pss = new ParameterizedSparqlString(VALUE_QUERY );
+        pss.setIri("permission", permissionUri);
+        String queryText = pss.toString();
+        try {
+            ResultSet rs = RDFServiceUtils.sparqlSelectQuery(queryText, configurationRdfService);
+            while (rs.hasNext()) {
+                QuerySolution qs = rs.next();
+                String entity = qs.getResource("uri").getURI();
+                entities.add(entity);
+            }
+        } catch (Exception e) {
+            log.error(e, e);
+        }
+        return entities;
+    }
+
+    public boolean isArmConfiguation() {
+        return containsAdminDisplayPermission();
+    }
+    
+    private boolean containsAdminDisplayPermission() {
+        boolean result = false;
+        String query = ""
+                + "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+                + "ASK WHERE {\n"
+                + "  GRAPH <http://vitro.mannlib.cornell.edu/default/vitro-kb-userAccounts> {\n"
+                + "       <" + getArmPermissionSubject(DISPLAY, ARM_ADMIN) + "> ?p ?o .\n" 
+                + "  }\n" 
+                + "}";
+        try {
+            result = configurationRdfService.sparqlAskQuery(query);
+        } catch (RDFServiceException e) {
+            log.error(e, e);
+        }
+        return result;
+    }
+}
---
+++ api/src/test/java/edu/cornell/mannlib/vitro/webapp/migration/auth/AnnotationMigratorTest.java
@@ -0,0 +1,68 @@
+package edu.cornell.mannlib.vitro.webapp.migration.auth;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.junit.Test;
+
+import edu.cornell.mannlib.vitro.webapp.auth.attributes.OperationGroup;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames;
+import edu.cornell.mannlib.vitro.webapp.rdfservice.impl.jena.model.RDFServiceModel;
+
+    
+public class AnnotationMigratorTest extends AuthMigratorTest {
+    
+    @Test
+    public void testGetAnnotationConfigs() {
+        AnnotationMigrator annotationMigrator = new AnnotationMigrator(new RDFServiceModel(contentModel), new RDFServiceModel(configurationDataSet));
+        Map<String, Map<OperationGroup, Set<String>>> configs = annotationMigrator.getObjectPropertyAnnotations();
+        Set<String> ops = configs.keySet();
+        assertEquals(1, configs.size());
+        configs = annotationMigrator.getDataPropertyAnnotations();
+        Set<String> dps = configs.keySet();
+        assertEquals(1, configs.size());
+        configs = annotationMigrator.getClassAnnotations();
+        assertEquals(2, configs.size());
+        configs = annotationMigrator.getFauxObjectPropertyAnnotations(ops);
+        assertEquals(1, configs.size());
+        configs = annotationMigrator.getFauxDataPropertyAnnotations(dps);
+        assertEquals(1, configs.size());
+    }
+    
+    @Test
+    public void testConvertAnnotationConfiguration() {
+        AnnotationMigrator annotationMigrator = new AnnotationMigrator(new RDFServiceModel(contentModel), new RDFServiceModel(configurationDataSet));
+        Model tmpModel = ModelFactory.createDefaultModel();
+        tmpModel.add(configurationDataSet.getNamedModel(ModelNames.ACCESS_CONTROL));
+        long initialSize = accessControlModel.size();
+        annotationMigrator.migrateConfiguration();
+        assertTrue(configurationDataSet.getNamedModel(ModelNames.ACCESS_CONTROL).size() > initialSize);
+        Model diff = configurationDataSet.getNamedModel(ModelNames.ACCESS_CONTROL).difference(tmpModel);
+        try(ByteArrayOutputStream baos = new ByteArrayOutputStream()){
+            diff.write(baos, "TTL");
+            String newData = baos.toString();
+            //System.out.println(newData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void testConfigurationVersion() {
+        Model tmpModel = ModelFactory.createDefaultModel();
+        tmpModel.add(configurationDataSet.getNamedModel(ModelNames.ACCESS_CONTROL));
+        assertEquals(0L, migrator.getVersion());
+        migrator.setVersion(1);
+        assertEquals(1L, migrator.getVersion());
+        migrator.removeVersion(1);
+        migrator.setVersion(2);
+        assertEquals(2L, migrator.getVersion());
+    }
+}
