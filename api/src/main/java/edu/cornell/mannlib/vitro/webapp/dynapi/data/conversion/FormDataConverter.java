4694c073b7c897a965774edb9ab404021e65cb5b
Georgy Litvinov <git@litvinovg.pro>
2023-06-27 10:29:39 +0200

    chore: identation corrections

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/data/conversion/FormDataConverter.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/dynapi/data/conversion/FormDataConverter.java
@@ -22,16 +22,18 @@ import edu.cornell.mannlib.vitro.webapp.dynapi.data.Data;
 public class FormDataConverter {
 
     private static final Log log = LogFactory.getLog(FormDataConverter.class);
-	private static ObjectMapper mapper = new ObjectMapper();
+    private static ObjectMapper mapper = new ObjectMapper();
 
-	public static void convert(HttpServletRequest request, Procedure procedure, DataStore dataStore) throws ConversionException {
-		Parameters required = procedure.getInputParams();
-		Map<String, String[]> received = request.getParameterMap();
-		convertRequiredParams(dataStore, required, received);
-		convertOptionalParams(dataStore, procedure.getOptionalParams(), received);
-	}
+    public static void convert(HttpServletRequest request, Procedure procedure, DataStore dataStore)
+            throws ConversionException {
+        Parameters required = procedure.getInputParams();
+        Map<String, String[]> received = request.getParameterMap();
+        convertRequiredParams(dataStore, required, received);
+        convertOptionalParams(dataStore, procedure.getOptionalParams(), received);
+    }
 
-	private static void convertOptionalParams(DataStore dataStore, Parameters optional, Map<String, String[]> received) {
+    private static void convertOptionalParams(DataStore dataStore, Parameters optional,
+            Map<String, String[]> received) {
         for (String name : optional.getNames()) {
             String[] values = received.get(name);
             if (values == null || values.length == 0) {
@@ -46,58 +48,60 @@ public class FormDataConverter {
                 } else {
                     readParam(dataStore, name, values, param);
                 }
-            } catch(Exception e) {
-                log.debug(e,e);
+            } catch (Exception e) {
+                log.debug(e, e);
             }
         }
     }
-	
+
     private static void convertRequiredParams(DataStore dataStore, Parameters required, Map<String, String[]> received)
             throws ConversionException {
         for (String name : required.getNames()) {
-			String[] values = received.get(name);
-			if (values == null || values.length == 0) {
-			    if (name.equals(RESTEndpoint.RESOURCE_ID) && !StringUtils.isBlank(dataStore.getResourceId())) {
-			        values = new String[] {dataStore.getResourceId()};
-			    } else {
-			        String message = String.format("Parameter %s not found", name);
-	                throw new ConversionException(message);    
-			    }
-			}
-			Parameter param = required.get(name);
-			
-			if (param.isArray()) {
-				readArray(dataStore, name, values, param);
-			} else {
-				readParam(dataStore, name, values, param);
-			}
-		}
+            String[] values = received.get(name);
+            if (values == null || values.length == 0) {
+                if (name.equals(RESTEndpoint.RESOURCE_ID) && !StringUtils.isBlank(dataStore.getResourceId())) {
+                    values = new String[] { dataStore.getResourceId() };
+                } else {
+                    String message = String.format("Parameter %s not found", name);
+                    throw new ConversionException(message);
+                }
+            }
+            Parameter param = required.get(name);
+
+            if (param.isArray()) {
+                readArray(dataStore, name, values, param);
+            } else {
+                readParam(dataStore, name, values, param);
+            }
+        }
     }
 
-	private static void readParam(DataStore dataStore, String name, String[] values, Parameter param) throws ConversionException {
-		Data data = new Data(param);
-		data.setRawString(values[0]);
-		try {
-			data.earlyInitialization();
-		} catch (Exception e) {
-			log.error(e, e);
-			throw new ConversionException(e.getLocalizedMessage());
-		}
-		dataStore.addData(name, data);
-	}
+    private static void readParam(DataStore dataStore, String name, String[] values, Parameter param)
+            throws ConversionException {
+        Data data = new Data(param);
+        data.setRawString(values[0]);
+        try {
+            data.earlyInitialization();
+        } catch (Exception e) {
+            log.error(e, e);
+            throw new ConversionException(e.getLocalizedMessage());
+        }
+        dataStore.addData(name, data);
+    }
 
-	private static void readArray(DataStore dataStore, String name, String[] values, Parameter param) {
-		Data data = new Data(param);
-		ArrayNode node = mapper.createArrayNode();
-		for (int i = 0; i< values.length;i++) {
-			node.add(values[i]);
-		}
-		data.setRawString(node.toString());
-		dataStore.addData(name, data);
-	}
+    private static void readArray(DataStore dataStore, String name, String[] values, Parameter param) {
+        Data data = new Data(param);
+        ArrayNode node = mapper.createArrayNode();
+        for (int i = 0; i < values.length; i++) {
+            node.add(values[i]);
+        }
+        data.setRawString(node.toString());
+        dataStore.addData(name, data);
+    }
 
-	public static void convert(HttpServletResponse response, Procedure action, DataStore dataStore) throws ConversionException {
-		throw new ConversionException("Not implemented!");		
-	}
+    public static void convert(HttpServletResponse response, Procedure action, DataStore dataStore)
+            throws ConversionException {
+        throw new ConversionException("Not implemented!");
+    }
 
 }
