74528fb9f12a189095933a27329132a105c32534
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2023-10-13 15:48:51 +0200

    Fixed wait time bug.

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/controller/authenticate/PasswordChangeRequestSpamMitigation.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/controller/authenticate/PasswordChangeRequestSpamMitigation.java
@@ -12,27 +12,35 @@ public class PasswordChangeRequestSpamMitigation {
 
     private static final Map<String, Integer> requestFrequency = new HashMap<>();
 
-    private static void initializeHistoryRequestDataIfNotExists(String emailAddress) {
+    private static final long INTERVAL_INCREASE_MINUTES = 1;
+
+    private static boolean initializeHistoryRequestDataIfNotExists(String emailAddress) {
         if (requestHistory.containsKey(emailAddress)) {
-            return;
+            return false;
         }
 
         requestHistory.put(emailAddress, LocalDateTime.now());
         requestFrequency.put(emailAddress, 0);
+        return true;
     }
 
     public static PasswordChangeRequestSpamMitigationResponse isPasswordResetRequestable(UserAccount userAccount) {
-        initializeHistoryRequestDataIfNotExists(userAccount.getEmailAddress());
+        boolean justInitialised = initializeHistoryRequestDataIfNotExists(userAccount.getEmailAddress());
 
         Integer numberOfSuccessiveRequests = requestFrequency.get(userAccount.getEmailAddress());
         LocalDateTime momentOfFirstRequest = requestHistory.get(userAccount.getEmailAddress());
-        LocalDateTime nextRequestAvailableAt = momentOfFirstRequest.plusMinutes(numberOfSuccessiveRequests * 10);
+        LocalDateTime nextRequestAvailableAt =
+            momentOfFirstRequest.plusMinutes(numberOfSuccessiveRequests * INTERVAL_INCREASE_MINUTES);
 
         if (nextRequestAvailableAt.isAfter(LocalDateTime.now())) {
             String[] dateTimeTokens = nextRequestAvailableAt.toString().split("T");
             String dateString = dateTimeTokens[0];
             String timeString = dateTimeTokens[1].split("\\.")[0];
             return new PasswordChangeRequestSpamMitigationResponse(false, dateString, timeString);
+        }
+
+        if (numberOfSuccessiveRequests > 0) {
+            requestHistory.put(userAccount.getEmailAddress(), LocalDateTime.now());
         }
 
         return new PasswordChangeRequestSpamMitigationResponse(true);
