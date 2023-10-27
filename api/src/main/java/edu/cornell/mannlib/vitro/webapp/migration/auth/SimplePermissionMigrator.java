348ac43e9264eb6548e129a52db251a7eb2e25e0
Georgy Litvinov <georgy.litvinov@tib.eu>
2023-10-27 10:57:52 +0200

    Simple permission conversion and check fixes.

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/migration/auth/SimplePermissionMigrator.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/migration/auth/SimplePermissionMigrator.java
@@ -1,6 +1,16 @@
 package edu.cornell.mannlib.vitro.webapp.migration.auth;
 
+import static edu.cornell.mannlib.vitro.webapp.migration.auth.AuthMigrator.ALL_ROLES;
+import static edu.cornell.mannlib.vitro.webapp.migration.auth.AuthMigrator.ROLE_ADMIN_URI;
+import static edu.cornell.mannlib.vitro.webapp.migration.auth.AuthMigrator.ROLE_CURATOR_URI;
+import static edu.cornell.mannlib.vitro.webapp.migration.auth.AuthMigrator.ROLE_EDITOR_URI;
+import static edu.cornell.mannlib.vitro.webapp.migration.auth.AuthMigrator.ROLE_PUBLIC_URI;
+import static edu.cornell.mannlib.vitro.webapp.migration.auth.AuthMigrator.ROLE_SELF_EDITOR_URI;
+
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import edu.cornell.mannlib.vitro.webapp.auth.attributes.AccessObjectType;
@@ -25,31 +35,50 @@ public class SimplePermissionMigrator {
             + "prefix auth: <http://vitro.mannlib.cornell.edu/ns/vitro/authorization#>\n"
             + "SELECT ?uri \n"
             + "WHERE {\n"
-            + "    ?uri a auth:PermissionSet . \n"
+            + "  ?uri a auth:PermissionSet . \n"
             + "}";
 
     private String ROLE_PERMISSIONS_QUERY = ""
             + "prefix auth: <http://vitro.mannlib.cornell.edu/ns/vitro/authorization#>\n"
             + "SELECT ?permission \n"
             + "WHERE {\n"
-            + "    ?role a auth:PermissionSet . \n"
-            + "    ?role auth:hasPermission ?permission .\n"
+            + "  ?role a auth:PermissionSet . \n"
+            + "  ?role auth:hasPermission ?permission .\n"
+            + "  FILTER (strstarts(str(?permission), "
+            + "'java:edu.cornell.mannlib.vitro.webapp.auth.permissions.SimplePermission#'))"
             + "}";
 
+    private Map<String, Set<String>> roleMap;
 
     public SimplePermissionMigrator(OntModel userAccountsModel) {
         this.userAccountsModel = userAccountsModel;
+        roleMap = new HashMap<>();
+        roleMap.put(ROLE_ADMIN_URI, ALL_ROLES);
+        roleMap.put(ROLE_CURATOR_URI, new HashSet<String>(
+                Arrays.asList(ROLE_CURATOR_URI, ROLE_EDITOR_URI, ROLE_SELF_EDITOR_URI, ROLE_PUBLIC_URI)));
+        roleMap.put(ROLE_EDITOR_URI,
+                new HashSet<String>(Arrays.asList(ROLE_EDITOR_URI, ROLE_SELF_EDITOR_URI, ROLE_PUBLIC_URI)));
+        roleMap.put(ROLE_SELF_EDITOR_URI, new HashSet<String>(Arrays.asList(ROLE_SELF_EDITOR_URI, ROLE_PUBLIC_URI)));
+        roleMap.put(ROLE_PUBLIC_URI, new HashSet<String>(Arrays.asList(ROLE_PUBLIC_URI)));
     }
 
     public void migrateConfiguration() {
         Set<String> roles = getPermissionSets();
+        if (!roles.containsAll(AuthMigrator.ALL_ROLES)) {
+            log.error("Roles not found. Simple permission migration failed.");
+            return;
+        }
+        migrateRoles(roles);
+    }
+
+    private void migrateRoles(Set<String> roles) {
         PolicyLoader policyLoader = PolicyLoader.getInstance();
         for (String role : roles) {
             // get all simple permissions for role
             Set<String> policyPermissions =
                     policyLoader.getDataSetValues(AccessOperation.EXECUTE, AccessObjectType.NAMED_OBJECT, role);
             // Compare with simple permissions in access control graph
-            Set<String> userAccountsPermissions = getUserAccountsPermissions(role);
+            Set<String> userAccountsPermissions = getUserAccountPermissions(role);
             Set<String> toRevoke = new HashSet<>(policyPermissions);
             toRevoke.removeAll(userAccountsPermissions);
             Set<String> toGrant = new HashSet<>(userAccountsPermissions);
@@ -57,12 +86,12 @@ public class SimplePermissionMigrator {
             for (String entityUri : toGrant) {
                 policyLoader.addEntityToPolicyDataSet(entityUri, AccessObjectType.NAMED_OBJECT, AccessOperation.EXECUTE,
                         role);
-                log.info(String.format("Granted simple permission %s to role %s ", entityUri, role ));
+                log.info(String.format("Granted simple permission %s to role %s ", entityUri, role));
             }
             for (String entityUri : toRevoke) {
                 policyLoader.removeEntityFromPolicyDataSet(entityUri, AccessObjectType.NAMED_OBJECT,
                         AccessOperation.EXECUTE, role);
-                log.info(String.format("Revoked simple permission %s from role %s ", entityUri, role ));
+                log.info(String.format("Revoked simple permission %s from role %s ", entityUri, role));
             }
         }
     }
@@ -89,8 +118,19 @@ public class SimplePermissionMigrator {
         return permissionSets;
     }
 
-    Set<String> getUserAccountsPermissions(String role) {
+    Set<String> getUserAccountPermissions(String role) {
         Set<String> permissions = new HashSet<>();
+        if (AuthMigrator.ALL_ROLES.contains(role)) {
+            for (String standardRole : roleMap.get(role)) {
+                populateUserAccountPermissions(standardRole, permissions);
+            }
+        } else {
+            populateUserAccountPermissions(role, permissions);
+        }
+        return permissions;
+    }
+
+    private void populateUserAccountPermissions(String role, Set<String> permissions) {
         ParameterizedSparqlString pss = new ParameterizedSparqlString(ROLE_PERMISSIONS_QUERY);
         pss.setIri("role", role);
         String queryText = pss.toString();
@@ -110,6 +150,5 @@ public class SimplePermissionMigrator {
         } finally {
             userAccountsModel.leaveCriticalSection();
         }
-        return permissions;
     }
 }
