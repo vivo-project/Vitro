15ca5dac0b4763569d9e00313e9d5d6cf458ee66
Georgy Litvinov <georgy.litvinov@tib.eu>
2023-11-07 17:17:29 +0100

    removed dead code in HasProfileFactory

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/auth/identifier/factory/HasProfileFactory.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/auth/identifier/factory/HasProfileFactory.java
@@ -5,60 +5,47 @@ package edu.cornell.mannlib.vitro.webapp.auth.identifier.factory;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import javax.servlet.ServletContext;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import edu.cornell.mannlib.vitro.webapp.auth.identifier.ArrayIdentifierBundle;
-import edu.cornell.mannlib.vitro.webapp.auth.identifier.Identifier;
 import edu.cornell.mannlib.vitro.webapp.auth.identifier.IdentifierBundle;
 import edu.cornell.mannlib.vitro.webapp.auth.identifier.common.HasProfile;
-import edu.cornell.mannlib.vitro.webapp.auth.identifier.common.IsBlacklisted;
 import edu.cornell.mannlib.vitro.webapp.beans.Individual;
 import edu.cornell.mannlib.vitro.webapp.beans.SelfEditingConfiguration;
 import edu.cornell.mannlib.vitro.webapp.beans.UserAccount;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
- * Find all of the individuals that are associated with the current user, and
- * create either an IsBlacklisted or HasAssociatedIndividual for each one.
+ * Find all of the individuals that are associated with the current user and create HasAssociatedIndividual for each
+ * one.
  */
-public class HasProfileOrIsBlacklistedFactory extends
-		BaseUserBasedIdentifierBundleFactory {
-	private static final Log log = LogFactory
-			.getLog(HasProfileOrIsBlacklistedFactory.class);
+public class HasProfileFactory extends BaseUserBasedIdentifierBundleFactory {
+    private static final Log log = LogFactory.getLog(HasProfileFactory.class);
 
-	@Override
-	public IdentifierBundle getIdentifierBundleForUser(UserAccount user) {
-		ArrayIdentifierBundle ids = new ArrayIdentifierBundle();
+    @Override
+    public IdentifierBundle getIdentifierBundleForUser(UserAccount user) {
+        ArrayIdentifierBundle ids = new ArrayIdentifierBundle();
 
-		for (Individual ind : getAssociatedIndividuals(user)) {
-			// If they are blacklisted, this factory will return an identifier
-			Identifier id = IsBlacklisted.getInstance(ind);
-			if (id != null) {
-				ids.add(id);
-			} else {
-				ids.add(new HasProfile(ind.getURI()));
-			}
-		}
+        for (Individual ind : getAssociatedIndividuals(user)) {
+            ids.add(new HasProfile(ind.getURI()));
+        }
 
-		return ids;
-	}
+        return ids;
+    }
 
-	/**
-	 * Get all Individuals associated with the current user as SELF.
-	 */
-	private Collection<Individual> getAssociatedIndividuals(UserAccount user) {
-		Collection<Individual> individuals = new ArrayList<Individual>();
+    /**
+     * Get all Individuals associated with the current user as SELF.
+     */
+    private Collection<Individual> getAssociatedIndividuals(UserAccount user) {
+        Collection<Individual> individuals = new ArrayList<Individual>();
 
-		if (user == null) {
-			log.debug("No Associated Individuals: not logged in.");
-			return individuals;
-		}
+        if (user == null) {
+            log.debug("No Associated Individuals: not logged in.");
+            return individuals;
+        }
 
-		SelfEditingConfiguration sec = SelfEditingConfiguration.getInstance();
-		individuals.addAll(sec.getAssociatedIndividuals(indDao, user));
+        SelfEditingConfiguration sec = SelfEditingConfiguration.getInstance();
+        individuals.addAll(sec.getAssociatedIndividuals(indDao, user));
 
-		return individuals;
-	}
+        return individuals;
+    }
 }
