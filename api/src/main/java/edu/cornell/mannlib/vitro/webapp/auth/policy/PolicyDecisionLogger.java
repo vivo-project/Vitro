305bea55bc510d00b81656945a7b4f9fe2485902
Georgy Litvinov <georgy.litvinov@tib.eu>
2023-11-27 18:19:22 +0100

    improved logging in PolicyDecisionLogger

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/auth/policy/PolicyDecisionLogger.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/auth/policy/PolicyDecisionLogger.java
@@ -6,171 +6,160 @@ import static edu.cornell.mannlib.vitro.webapp.auth.policy.ifaces.DecisionResult
 
 import java.util.regex.Pattern;
 
+import edu.cornell.mannlib.vitro.webapp.auth.attributes.AccessOperation;
+import edu.cornell.mannlib.vitro.webapp.auth.identifier.IdentifierBundle;
+import edu.cornell.mannlib.vitro.webapp.auth.objects.AccessObject;
+import edu.cornell.mannlib.vitro.webapp.auth.policy.ifaces.Policy;
+import edu.cornell.mannlib.vitro.webapp.auth.policy.ifaces.PolicyDecision;
+import edu.cornell.mannlib.vitro.webapp.auth.requestedAction.AuthorizationRequest;
+import edu.cornell.mannlib.vitro.webapp.utils.developer.DeveloperSettings;
+import edu.cornell.mannlib.vitro.webapp.utils.developer.Key;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import edu.cornell.mannlib.vitro.webapp.auth.identifier.IdentifierBundle;
-import edu.cornell.mannlib.vitro.webapp.auth.objects.AccessObject;
-import edu.cornell.mannlib.vitro.webapp.auth.policy.ifaces.PolicyDecision;
-import edu.cornell.mannlib.vitro.webapp.auth.policy.ifaces.Policy;
-import edu.cornell.mannlib.vitro.webapp.utils.developer.DeveloperSettings;
-import edu.cornell.mannlib.vitro.webapp.utils.developer.Key;
-
 /**
- * If enabled in the developer settings (and log levels), log each
- * PolicyDecision (subject to restrictions).
+ * If enabled in the developer settings (and log levels), log each PolicyDecision (subject to restrictions).
  *
- * Some restrictions apply to the logger as a whole. Others apply to the
- * particular policy or the particular decision.
+ * Some restrictions apply to the logger as a whole. Others apply to the particular policy or the particular decision.
  */
 public class PolicyDecisionLogger {
-	private static final Log log = LogFactory
-			.getLog(PolicyDecisionLogger.class);
+    private static final Log log = LogFactory.getLog(PolicyDecisionLogger.class);
 
-	private static final Pattern NEVER_MATCHES = Pattern.compile("^__NEVER__$");
+    private static final Pattern NEVER_MATCHES = Pattern.compile("^__NEVER__$");
 
-	private static final BasicPolicyDecision NULL_DECISION = new BasicPolicyDecision(
-			INCONCLUSIVE, "The decision was null.");
+    private static final BasicPolicyDecision NULL_DECISION =
+            new BasicPolicyDecision(INCONCLUSIVE, "The decision was null.");
 
-	private final DeveloperSettings settings;
-	private final AccessObject whatToAuth;
-	private final IdentifierBundle whoToAuth;
+    private final DeveloperSettings settings;
+    private final AccessObject object;
+    private final AccessOperation operation;
+    private final IdentifierBundle whoToAuth;
 
-	private final boolean enabled;
+    private final boolean enabled;
 
-	private final Pattern policyRestriction;
-	private final boolean skipInconclusive;
-	private final boolean includeIdentifiers;
+    private final Pattern policyRestriction;
+    private final boolean skipInconclusive;
+    private final boolean includeIdentifiers;
 
-	public PolicyDecisionLogger(IdentifierBundle whoToAuth,
-			AccessObject whatToAuth) {
-		this.settings = DeveloperSettings.getInstance();
-		this.whoToAuth = whoToAuth;
-		this.whatToAuth = whatToAuth;
+    public PolicyDecisionLogger(AuthorizationRequest ar) {
+        this.settings = DeveloperSettings.getInstance();
+        this.whoToAuth = ar.getIds();
+        this.object = ar.getAccessObject();
+        this.operation = ar.getAccessOperation();
 
-		this.enabled = figureEnabled();
+        this.enabled = figureEnabled();
 
-		this.policyRestriction = figurePolicyRestriction();
-		this.skipInconclusive = figureSkipInconclusive();
-		this.includeIdentifiers = figureIncludeIdentifiers();
-	}
+        this.policyRestriction = figurePolicyRestriction();
+        this.skipInconclusive = figureSkipInconclusive();
+        this.includeIdentifiers = figureIncludeIdentifiers();
+    }
 
-	private boolean figureEnabled() {
-		return log.isInfoEnabled()
-				&& settings.getBoolean(Key.AUTHORIZATION_LOG_DECISIONS_ENABLE)
-				&& passesUserRestriction() && passesActionRestriction();
-	}
+    private boolean figureEnabled() {
+        return log.isInfoEnabled() && settings.getBoolean(Key.AUTHORIZATION_LOG_DECISIONS_ENABLE)
+                && passesUserRestriction() && passesActionRestriction();
+    }
 
-	/**
-	 * The identifier bundle passes if there is no restriction, or if the
-	 * restriction pattern is found within concatenated string of the identifier
-	 * bundle.
-	 *
-	 * If the restriction is invalid, the action fails.
-	 */
-	private boolean passesUserRestriction() {
-		Pattern userRestriction = compilePatternFromSetting(Key.AUTHORIZATION_LOG_DECISIONS_USER_RESTRICTION);
-		return userRestriction == null
-				|| userRestriction.matcher(String.valueOf(whoToAuth)).find();
-	}
+    /**
+     * The identifier bundle passes if there is no restriction, or if the restriction pattern is found within
+     * concatenated string of the identifier bundle.
+     *
+     * If the restriction is invalid, the action fails.
+     */
+    private boolean passesUserRestriction() {
+        Pattern userRestriction = compilePatternFromSetting(Key.AUTHORIZATION_LOG_DECISIONS_USER_RESTRICTION);
+        return userRestriction == null || userRestriction.matcher(String.valueOf(whoToAuth)).find();
+    }
 
-	/**
-	 * The requested action passes if there is no restriction, or if the
-	 * restriction pattern is found within the class name of the action.
-	 *
-	 * If the restriction is invalid, the action fails.
-	 */
-	private boolean passesActionRestriction() {
-		Pattern actionRestriction = compilePatternFromSetting(Key.AUTHORIZATION_LOG_DECISIONS_ACTION_RESTRICTION);
-		return actionRestriction == null
-				|| actionRestriction.matcher(String.valueOf(whatToAuth)).find();
-	}
+    /**
+     * The requested action passes if there is no restriction, or if the restriction pattern is found within the class
+     * name of the action.
+     *
+     * If the restriction is invalid, the action fails.
+     */
+    private boolean passesActionRestriction() {
+        Pattern actionRestriction = compilePatternFromSetting(Key.AUTHORIZATION_LOG_DECISIONS_ACTION_RESTRICTION);
+        return actionRestriction == null || actionRestriction.matcher(String.valueOf(object)).find();
+    }
 
-	/**
-	 * Only compile the policy restriction pattern once.
-	 */
-	private Pattern figurePolicyRestriction() {
-		return compilePatternFromSetting(Key.AUTHORIZATION_LOG_DECISIONS_POLICY_RESTRICTION);
-	}
+    /**
+     * Only compile the policy restriction pattern once.
+     */
+    private Pattern figurePolicyRestriction() {
+        return compilePatternFromSetting(Key.AUTHORIZATION_LOG_DECISIONS_POLICY_RESTRICTION);
+    }
 
-	/**
-	 * Do we log inconclusive decisions?
-	 */
-	private boolean figureSkipInconclusive() {
-		return settings
-				.getBoolean(Key.AUTHORIZATION_LOG_DECISIONS_SKIP_INCONCLUSIVE);
-	}
+    /**
+     * Do we log inconclusive decisions?
+     */
+    private boolean figureSkipInconclusive() {
+        return settings.getBoolean(Key.AUTHORIZATION_LOG_DECISIONS_SKIP_INCONCLUSIVE);
+    }
 
-	/**
-	 * Do we include Identifiers in the log record?
-	 */
-	private boolean figureIncludeIdentifiers() {
-		return settings
-				.getBoolean(Key.AUTHORIZATION_LOG_DECISIONS_ADD_IDENTIFERS);
-	}
+    /**
+     * Do we include Identifiers in the log record?
+     */
+    private boolean figureIncludeIdentifiers() {
+        return settings.getBoolean(Key.AUTHORIZATION_LOG_DECISIONS_ADD_IDENTIFERS);
+    }
 
-	/**
-	 * If no pattern was provided, return null. If an invalid pattern was
-	 * provided, return a pattern that never matches.
-	 */
-	private Pattern compilePatternFromSetting(Key key) {
-		String setting = settings.getString(key);
-		if (setting.isEmpty()) {
-			return null;
-		} else {
-			try {
-				return Pattern.compile(setting);
-			} catch (Exception e) {
-				return NEVER_MATCHES;
-			}
-		}
-	}
+    /**
+     * If no pattern was provided, return null. If an invalid pattern was provided, return a pattern that never matches.
+     */
+    private Pattern compilePatternFromSetting(Key key) {
+        String setting = settings.getString(key);
+        if (setting.isEmpty()) {
+            return null;
+        } else {
+            try {
+                return Pattern.compile(setting);
+            } catch (Exception e) {
+                return NEVER_MATCHES;
+            }
+        }
+    }
 
-	/**
-	 * If the logger and the policy and the decision all pass the restrictions,
-	 * write to the log. A null decision is treated as inconclusive.
-	 */
-	public void log(Policy policy, PolicyDecision pd) {
-		if (passesRestrictions(String.valueOf(policy), pd)) {
-			if (this.includeIdentifiers) {
-				log.info(String.format(
-						"Decision on %s by %s was %s; user is %s",
-						this.whatToAuth, policy, pd, this.whoToAuth));
-			} else {
-				log.info(String.format("Decision on %s by %s was %s",
-						this.whatToAuth, policy, pd));
-			}
-		}
-	}
+    /**
+     * If the logger and the policy and the decision all pass the restrictions, write to the log. A null decision is
+     * treated as inconclusive.
+     */
+    public void log(Policy policy, PolicyDecision pd) {
+        if (passesRestrictions(String.valueOf(policy), pd)) {
+            if (this.includeIdentifiers) {
+                log.info(String.format("Decision on %s %s by %s was %s; user is %s", this.operation, this.object,
+                        policy.getShortUri(), pd, this.whoToAuth));
+            } else {
+                log.info(String.format("Decision on %s %s by %s was %s", this.operation, this.object,
+                        policy.getShortUri(), pd));
+            }
+        }
+    }
 
-	private boolean passesRestrictions(String policyString, PolicyDecision pd) {
-		if (pd == null) {
-			pd = NULL_DECISION;
-		}
-		return enabled && passesPolicyRestriction(policyString)
-				&& passesConclusiveRestriction(pd);
-	}
+    private boolean passesRestrictions(String policyString, PolicyDecision pd) {
+        if (pd == null) {
+            pd = NULL_DECISION;
+        }
+        return enabled && passesPolicyRestriction(policyString) && passesConclusiveRestriction(pd);
+    }
 
-	private boolean passesPolicyRestriction(String policyString) {
-		return this.policyRestriction == null
-				|| this.policyRestriction.matcher(policyString).find();
-	}
+    private boolean passesPolicyRestriction(String policyString) {
+        return this.policyRestriction == null || this.policyRestriction.matcher(policyString).find();
+    }
 
-	private boolean passesConclusiveRestriction(PolicyDecision pd) {
-		return !(skipInconclusive && isInconclusive(pd));
-	}
+    private boolean passesConclusiveRestriction(PolicyDecision pd) {
+        return !(skipInconclusive && isInconclusive(pd));
+    }
 
-	private boolean isInconclusive(PolicyDecision pd) {
-		return pd == null || pd.getDecisionResult() == INCONCLUSIVE;
-	}
+    private boolean isInconclusive(PolicyDecision pd) {
+        return pd == null || pd.getDecisionResult() == INCONCLUSIVE;
+    }
 
-	public void logNoDecision(PolicyDecision pd) {
-		if (enabled) {
-			if (this.includeIdentifiers) {
-				log.info(pd.getMessage() + "; user is " + this.whoToAuth);
-			} else {
-				log.info(pd.getMessage());
-			}
-		}
-	}
+    public void logNoDecision(PolicyDecision pd) {
+        if (enabled) {
+            if (this.includeIdentifiers) {
+                log.info(pd.getMessage() + "; user is " + this.whoToAuth);
+            } else {
+                log.info(pd.getMessage());
+            }
+        }
+    }
 }
