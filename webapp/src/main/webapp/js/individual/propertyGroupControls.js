e0bf28dbea3376d9b29ad8cacb3292134b55d21c
Miloš Popović <misa01523@gmail.com>
2023-12-07 11:58:21 +0100

    Cherry-pick Added Bootstrap and updated CSS for Wilma theme to make it responsive (#429)
    * Updated wilma to bootstrap 5
    
    * Moved bootstrap to webapp folder, changed property tabs to use bootstrap
    
    * Fixed property group tabs
    
    * Bugfix: multiple dom object with same id, individual tabs
    
    * Removed duplicate bootstrap 5.3.2
    
    * Removed unused js code in propertGroupControls
    
    * Reimplemented tab state persistence after refresh feature
    
    * Removed unnecessary code

--- webapp/src/main/webapp/js/individual/propertyGroupControls.js
+++ webapp/src/main/webapp/js/individual/propertyGroupControls.js
@@ -4,60 +4,37 @@ $(document).ready(function(){
 
     $.extend(this, individualLocalName);
     adjustFontSize();
-    padSectionBottoms();
     checkLocationHash();
-
-    // ensures that shorter property group sections don't cause the page to "jump around"
-    // when the tabs are clicked
-    function padSectionBottoms() {
-        $.each($('section.property-group'), function() {
-            var sectionHeight = $(this).height();
-            if ( sectionHeight < 1000 ) {
-                    $(this).css('margin-bottom', 1000-sectionHeight + "px");
-            }
-        });
-    }
-
+    
     // controls the property group tabs
-    $.each($('li.clickable'), function() {
-        var groupName = $(this).attr("groupName");
-        var $propertyGroupLi = $(this);
+    let showAllBtn = $('#show-all-tabs')[0];
+    let tabList = $('ul.propertyTabsList')[0];
 
-        $(this).click(function() {
+    showAllBtn.addEventListener('show.bs.tab', function (event) {
+        event.preventDefault()
+        showAllTabs();
+        manageLocalStorage();
+    })
 
-            if ( $propertyGroupLi.attr("class") == "nonSelectedGroupTab clickable" ) {
-                $.each($('li.selectedGroupTab'), function() {
-                    $(this).removeClass("selectedGroupTab clickable");
-                    $(this).addClass("nonSelectedGroupTab clickable");
-                });
-                $propertyGroupLi.removeClass("nonSelectedGroupTab clickable");
-                $propertyGroupLi.addClass("selectedGroupTab clickable");
-            }
-            if ( $propertyGroupLi.attr("groupname") == "viewAll" ) {
-                processViewAllTab();
-            }
-            else {
-                padSectionBottoms();
-                var $visibleSection = $('section.property-group:visible');
-                $visibleSection.hide();
-                $('h2[pgroup=tabs]').addClass("hidden");
-                $('nav#scroller').addClass("hidden");
-                $('section#' + groupName).show();
-            }
-            manageLocalStorage();
-            return false;
+    showAllBtn.addEventListener('hide.bs.tab', function (event) {
+        $(".tab-content>section.tab-pane").removeClass('show active')
+    })
+
+    tabList.addEventListener('shown.bs.tab', function (event) {
+        manageLocalStorage();
+    })
+
+
+    function showAllTabs() {
+        $('.propertyTabsList.nav.nav-tabs > li').each(function() {
+            $(this).attr("aria-selected", "false");
+            $(this).removeClass("active");
         });
-    });
-
-    function processViewAllTab() {
-        $.each($('section.property-group'), function() {
-            $(this).css("margin-bottom", "1px");
-            $(this).children('h2').css("margin-top", "-15px").css("border-bottom","1px solid #DFEBE5").css("padding","12px 25px 10px 20px");
-            $(this).show();
-            $('h2[pgroup=tabs]').removeClass("hidden");
-            $('nav#scroller').removeClass("hidden");
-        });
+        
+        $('#show-all-tabs').addClass("active").attr("aria-selected", "true");
+        $(".tab-content>section.tab-pane").addClass('active show')    
     }
+
 
     // If users click a marker on the home page map, they are taken to the profile
     // page of the corresponding country. The url contains the word "Research" in
@@ -74,17 +51,12 @@ $(document).ready(function(){
                 tabNameCapped = tabName.charAt(0).toUpperCase() + tabName.slice(1);
                 // if the name of the first tab section = tabName we don't have to do anything;
                 // otherwise, select the correct tab and deselect the first one
-                var $firstTab = $('li.clickable').first();
+                var $firstTab = $('li.nav-link').first();
                 if ( $firstTab.text() != tabNameCapped ) {
                     // select the correct tab
-                    $('li[groupName="' + tabName + '"]').removeClass("nonSelectedGroupTab clickable");
-                    $('li[groupName="' + tabName + '"]').addClass("selectedGroupTab clickable");
-                    // deselect the first tab
-                    $firstTab.removeClass("selectedGroupTab clickable");
-                    $firstTab.addClass("nonSelectedGroupTab clickable");
-                    $('section.property-group:visible').hide();
-                    // show the selected tab section
-                    $('section#' + tabName).show();
+                    let tabSelect = $('li[groupName="' + tabName + '"]');
+                    let tab = new bootstrap.Tab(tabSelect);
+                    tab.show()
                 }
                 // if there is a more link, "click" more to show all the researchers
                 // we need the timeout delay so that the more link can get rendered
@@ -142,14 +114,13 @@ $(document).ready(function(){
             }
         }
         var selectedTab = [];
-        selectedTab.push($('li.selectedGroupTab').attr('groupName'));
+        selectedTab.push($('li.nav-link.active').attr('groupName'));
         amplify.store(localName, selectedTab);
         var checkLength = amplify.store(localName);
         if ( checkLength.length == 0 ) {
             amplify.store(localName, null);
         }
     }
-
     function retrieveLocalStorage() {
 
         var localName = this.individualLocalName;
@@ -163,27 +134,23 @@ $(document).ready(function(){
             // So ensure that the tab in local storage has been rendered on the page.
             if ( $("ul.propertyTabsList li[groupName='" + groupName + "']").length ) {
                 // if the selected tab is the default first one, don't do anything
-                if ( $('li.clickable').first().attr("groupName") != groupName ) {
+                if ( $('li.nav-link').first().attr("groupName") != groupName ) {
                     // deselect the default first tab
-                    var $firstTab = $('li.clickable').first();
-                    $firstTab.removeClass("selectedGroupTab clickable");
-                    $firstTab.addClass("nonSelectedGroupTab clickable");
-                    // select the stored tab
-                    $("li[groupName='" + groupName + "']").removeClass("nonSelectedGroupTab clickable");
-                    $("li[groupName='" + groupName + "']").addClass("selectedGroupTab clickable");
-                    // hide the first tab section
-                    $('section.property-group:visible').hide();
 
                     if ( groupName == "viewAll" ) {
-                        processViewAllTab();
-                    }
+                        console.log("View all")
+                        showAllTabs();
+                    } else {
 
-                    // show the selected tab section
-                    $('section#' + groupName).show();
+                        let tabSelect = $('li[groupName="' + groupName + '"]');
+                        let tab = new bootstrap.Tab(tabSelect);
+                        tab.show();
+                    }
                 }
             }
         }
     }
+
     // if there are so many tabs that they wrap to a second line, adjust the font size to
     //prevent wrapping
     function adjustFontSize() {
