be94088f5d489148ceed3e995092901a894a9b1f
Georgy Litvinov <georgy.litvinov@tib.eu>
2024-01-09 11:56:39 +0100

    removed not used key component for previous commit

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/auth/attributes/NamedKeyComponent.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/auth/attributes/NamedKeyComponent.java
@@ -3,6 +3,5 @@ package edu.cornell.mannlib.vitro.webapp.auth.attributes;
 public enum NamedKeyComponent {
     SUPPRESSION_BY_URI,
     SUPPRESSION_BY_TYPE,
-    PROPERTY_SUPPRESSION,
     NOT_RELATED,
 }
