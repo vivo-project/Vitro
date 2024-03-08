74d04d99a49c56dcd8a9217dea9091b8790fdabe
Georgy Litvinov <georgy.litvinov@tib.eu>
2024-03-08 09:49:52 +0100

    Split: extracted ConfigurationTripleSource
        use dynapi model instead of full union
    
        moved dynamic-api-implementation.n3 to dynapi model

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/modules/tripleSource/ConfigurationTripleSource.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/modules/tripleSource/ConfigurationTripleSource.java
@@ -2,17 +2,8 @@
 
 package edu.cornell.mannlib.vitro.webapp.modules.tripleSource;
 
-import static edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames.DISPLAY;
-import static edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames.DISPLAY_DISPLAY;
-import static edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames.DISPLAY_TBOX;
-import static edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames.USER_ACCOUNTS;
-import static edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames.ABOX_ASSERTIONS_FIRSTTIME_BACKUP;
-import static edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames.TBOX_ASSERTIONS_FIRSTTIME_BACKUP;
-import static edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames.APPLICATION_METADATA_FIRSTTIME_BACKUP;
-import static edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames.USER_ACCOUNTS_FIRSTTIME_BACKUP;
-import static edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames.DISPLAY_FIRSTTIME_BACKUP;
-import static edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames.DISPLAY_TBOX_FIRSTTIME_BACKUP;
-import static edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames.DISPLAY_DISPLAY_FIRSTTIME_BACKUP;
+import static edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames.*;
+
 
 
 import org.apache.jena.rdf.model.ModelMaker;
@@ -25,11 +16,23 @@ public abstract class ConfigurationTripleSource implements TripleSource {
 	 * A list of all Configuration models, in case the implementation wants to
 	 * add memory-mapping.
 	 */
-	protected static final String[] CONFIGURATION_MODELS = { DISPLAY,
-			DISPLAY_TBOX, DISPLAY_DISPLAY, USER_ACCOUNTS, ABOX_ASSERTIONS_FIRSTTIME_BACKUP,
-			TBOX_ASSERTIONS_FIRSTTIME_BACKUP, APPLICATION_METADATA_FIRSTTIME_BACKUP,
-			USER_ACCOUNTS_FIRSTTIME_BACKUP, DISPLAY_FIRSTTIME_BACKUP, 
-			DISPLAY_TBOX_FIRSTTIME_BACKUP, DISPLAY_DISPLAY_FIRSTTIME_BACKUP };
+	protected static final String[] CONFIGURATION_MODELS = { 
+			DISPLAY,
+			DISPLAY_TBOX, 
+			DISPLAY_DISPLAY, 
+			USER_ACCOUNTS, 
+			ABOX_ASSERTIONS_FIRSTTIME_BACKUP,
+			TBOX_ASSERTIONS_FIRSTTIME_BACKUP, 
+			APPLICATION_METADATA_FIRSTTIME_BACKUP,
+			USER_ACCOUNTS_FIRSTTIME_BACKUP, 
+			DISPLAY_FIRSTTIME_BACKUP, 
+			DISPLAY_TBOX_FIRSTTIME_BACKUP, 
+			DISPLAY_DISPLAY_FIRSTTIME_BACKUP, 
+			DYNAMIC_API_ABOX,
+			DYNAMIC_API_ABOX_FIRSTTIME_BACKUP,
+			DYNAMIC_API_TBOX,
+			DYNAMIC_API_TBOX_FIRSTTIME_BACKUP,
+			};
 
 	/**
 	 * These decorators are added to a Configuration ModelMaker, regardless of
