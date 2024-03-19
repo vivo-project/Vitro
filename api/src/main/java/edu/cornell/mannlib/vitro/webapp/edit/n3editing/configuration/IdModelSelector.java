a9b8c13849055fdb1996eb0984d5670f20f34580
Georgy Litvinov <georgy.litvinov@tib.eu>
2024-03-19 11:56:39 +0100

    Provide correct RdfService ang graph uri on apply changes to write model

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/edit/n3editing/configuration/IdModelSelector.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/edit/n3editing/configuration/IdModelSelector.java
@@ -11,6 +11,8 @@ import org.apache.jena.rdf.model.ModelMaker;
 
 import edu.cornell.mannlib.vitro.webapp.dao.jena.VitroModelSource;
 import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess.WhichService;
 
 public class IdModelSelector implements ModelSelector {
 
@@ -25,13 +27,29 @@ public class IdModelSelector implements ModelSelector {
 
 	@Override
 	public Model getModel(HttpServletRequest request, ServletContext context) {
-		ModelMaker modelMaker = ModelAccess.on(context).getModelMaker(CONTENT);
+		ModelMaker modelMaker = ModelAccess.getInstance().getModelMaker(getRdfService());
 		VitroModelSource mSource = new VitroModelSource(modelMaker, context);
 		return mSource.getModel(name);
 	}
 
+	private WhichService getRdfService() {
+		if (isConfigurationModel()) {
+			return WhichService.CONFIGURATION;
+        	}
+		return CONTENT;
+	}
+
+	private boolean isConfigurationModel() {
+		return name.equals(VitroModelSource.ModelName.DISPLAY.toString()) ||
+			name.equals(VitroModelSource.ModelName.DISPLAY_TBOX.toString()) ||
+			name.equals(VitroModelSource.ModelName.DISPLAY_DISPLAY.toString());
+	}
+
 	@Override
 	public String getDefaultGraphUri() {
+		if (ModelNames.namesMap.containsKey(name)) {
+			return ModelNames.namesMap.get(name);
+		}
 		return name;
 	}
 
