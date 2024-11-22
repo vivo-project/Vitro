8980e5e0873da014ad1c9a2313f1b7b050a82aa4
Georgy Litvinov <georgy.litvinov@tib.eu>
2024-11-22 09:23:18 +0100

    Revert "rm unimplemented or unnecessary classes"
    This reverts commit 7c2f665991b94c8d9f7e4f38342502815a4be8f7.

---
+++ api/src/main/java/edu/cornell/library/scholars/webapp/controller/api/distribute/html/RenderedShortViewDistributor.java
@@ -0,0 +1,13 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
+
+package edu.cornell.library.scholars.webapp.controller.api.distribute.html;
+
+/**
+ * TODO
+ * 
+ * Provide short view CONTEXT, Individual URI, most-specific class, and get the
+ * rendered HTML for the short view.
+ */
+public class RenderedShortViewDistributor {
+
+}
---
+++ api/src/main/java/edu/cornell/library/scholars/webapp/controller/api/distribute/rdf/graphbuilder/ThreadsafeGraphBuilder.java
@@ -0,0 +1,13 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
+
+package edu.cornell.library.scholars.webapp.controller.api.distribute.rdf.graphbuilder;
+
+/**
+ * TODO
+ * 
+ * Marker interface asserting that the GraphBuilder may be called by several
+ * threads at once without conflict
+ */
+public interface ThreadsafeGraphBuilder {
+
+}
