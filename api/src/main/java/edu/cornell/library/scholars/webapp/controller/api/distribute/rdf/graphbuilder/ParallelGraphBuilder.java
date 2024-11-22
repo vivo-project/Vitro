ac1ed45774e96ef82eba027530adfdfda2a92434
Georgy Litvinov <georgy.litvinov@tib.eu>
2024-11-22 09:23:18 +0100

    Revert "rm unimplemented or unnecessary classes"
    This reverts commit 7c2f665991b94c8d9f7e4f38342502815a4be8f7.

---
+++ api/src/main/java/edu/cornell/library/scholars/webapp/controller/api/distribute/rdf/graphbuilder/ParallelGraphBuilder.java
@@ -0,0 +1,14 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
+
+package edu.cornell.library.scholars.webapp.controller.api.distribute.rdf.graphbuilder;
+
+/**
+ * TODO
+ * 
+ * Iterate like the drill-down builder (share code?) Each sub-request is handled
+ * by a thread-pool, whose size can be configured. Requires the use of
+ * ThreadsafeGraphBuilders.
+ */
+public class ParallelGraphBuilder {
+
+}
