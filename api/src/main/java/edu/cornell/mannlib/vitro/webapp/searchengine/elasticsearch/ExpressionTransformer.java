f95843dfc831aaeef47a6b1353b021ab30ca9ddc
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2025-03-28 11:49:27 +0100

    Fixed match all query bug.

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/searchengine/elasticsearch/ExpressionTransformer.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/searchengine/elasticsearch/ExpressionTransformer.java
@@ -25,6 +25,74 @@ public class ExpressionTransformer {
         priorities = Collections.unmodifiableMap(priorityTempMap);
     }
 
+    public static String removeWhitespacesFromRangeExpression(String expression) {
+        String regex = "\\[\\s*([^\\s]+)\\s+TO\\s+([^\\s]+)\\s*\\]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(expression);
+
+        StringBuffer result = new StringBuffer();
+        while (matcher.find()) {
+            String replacement = "[" + matcher.group(1) + "TO" + matcher.group(2) + "]";
+            matcher.appendReplacement(result, replacement);
+        }
+        matcher.appendTail(result);
+
+        return result.toString();
+    }
+
+    public static String fillInMissingOperators(String query) {
+        String[] tokens = query.split(" ");
+        StringBuilder modifiedQuery = new StringBuilder();
+
+        for (int i = 0; i < tokens.length; i++) {
+            String currentToken = tokens[i];
+
+            if (!priorities.containsKey(currentToken) && !currentToken.contains(":") && !currentToken.equals(")")) {
+                currentToken = "ALLTEXT:" + currentToken + " OR nameLowercaseSingleValued:" + currentToken;
+            }
+
+            if (i > 0 && currentToken.contains(":") && !priorities.containsKey(tokens[i - 1])) {
+                modifiedQuery.append(" OR ");
+            }
+
+            if (i > 0 && currentToken.startsWith("(") && !priorities.containsKey(tokens[i - 1])) {
+                modifiedQuery.append(" AND ");
+            }
+
+            modifiedQuery.append(currentToken);
+
+            if (i < tokens.length - 1) {
+                modifiedQuery.append(" ");
+            }
+        }
+
+        return modifiedQuery.toString();
+    }
+
+    private static SearchType decideQueryType(String field, String value) {
+        SearchType searchType = SearchType.MATCH;
+
+        if (value.startsWith("\"") && value.endsWith("\"")) {
+            searchType = SearchType.PHRASE;
+        } else if (value.contains("TO") && !value.equals("TO")) {
+            if (value.equals("[*TO*]")) {
+                searchType = SearchType.EXISTS;
+            } else {
+                searchType = SearchType.RANGE;
+            }
+        } else if (field.contains("*") || value.contains("*")) {
+            if (field.trim().equals("*")) {
+                searchType = SearchType.MATCH_ALL;
+            } else {
+                searchType = SearchType.WILDCARD;
+            }
+        } else if (value.endsWith("~")) {
+            searchType = SearchType.FUZZY;
+        }
+
+        return searchType;
+    }
+
     public Query parseAdvancedQuery(List<String> expression) {
         return buildQueryFromPostFixExpression(transformToPostFixNotation(expression));
     }
@@ -94,7 +162,7 @@ public class ExpressionTransformer {
                     if (fieldValueTuple[0].startsWith("-")) {
                         queryStack.push(BoolQuery.of(q -> {
                             q.mustNot(CustomQueryBuilder.buildQuery(
-                                decideQueryType(fieldValueTuple[1]),
+                                decideQueryType(fieldValueTuple[0], fieldValueTuple[1]),
                                 fieldValueTuple[0].replaceFirst("-", ""),
                                 fieldValueTuple[1]));
                             return q;
@@ -102,7 +170,7 @@ public class ExpressionTransformer {
                         break;
                     }
 
-                    SearchType searchType = decideQueryType(fieldValueTuple[1]);
+                    SearchType searchType = decideQueryType(fieldValueTuple[0], fieldValueTuple[1]);
 
                     queryStack.push(CustomQueryBuilder.buildQuery(
                         searchType,
@@ -112,69 +180,5 @@ public class ExpressionTransformer {
         }
 
         return queryStack.pop();
-    }
-
-    public static String removeWhitespacesFromRangeExpression(String expression) {
-        String regex = "\\[\\s*([^\\s]+)\\s+TO\\s+([^\\s]+)\\s*\\]";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(expression);
-
-        StringBuffer result = new StringBuffer();
-        while (matcher.find()) {
-            String replacement = "[" + matcher.group(1) + "TO" + matcher.group(2) + "]";
-            matcher.appendReplacement(result, replacement);
-        }
-        matcher.appendTail(result);
-
-        return result.toString();
-    }
-
-    public static String fillInMissingOperators(String query) {
-        String[] tokens = query.split(" ");
-        StringBuilder modifiedQuery = new StringBuilder();
-
-        for (int i = 0; i < tokens.length; i++) {
-            String currentToken = tokens[i];
-
-            if (!priorities.containsKey(currentToken) && !currentToken.contains(":") && !currentToken.equals(")")) {
-                currentToken = "ALLTEXT:" + currentToken + " OR nameLowercaseSingleValued:" + currentToken;
-            }
-
-            if (i > 0 && currentToken.contains(":") && !priorities.containsKey(tokens[i - 1])) {
-                modifiedQuery.append(" OR ");
-            }
-
-            if (i > 0 && currentToken.startsWith("(") && !priorities.containsKey(tokens[i - 1])) {
-                modifiedQuery.append(" AND ");
-            }
-
-            modifiedQuery.append(currentToken);
-
-            if (i < tokens.length - 1) {
-                modifiedQuery.append(" ");
-            }
-        }
-
-        return modifiedQuery.toString();
-    }
-
-    private static SearchType decideQueryType(String value) {
-        SearchType searchType = SearchType.MATCH;
-
-        if (value.startsWith("\"") && value.endsWith("\"")) {
-            searchType = SearchType.PHRASE;
-        } else if (value.contains("TO") && !value.equals("TO")) {
-            if (value.equals("[*TO*]")) {
-                searchType = SearchType.EXISTS;
-            } else {
-                searchType = SearchType.RANGE;
-            }
-        } else if (value.contains("*")) {
-            searchType = SearchType.WILDCARD;
-        } else if (value.endsWith("~")) {
-            searchType = SearchType.FUZZY;
-        }
-
-        return searchType;
     }
 }
