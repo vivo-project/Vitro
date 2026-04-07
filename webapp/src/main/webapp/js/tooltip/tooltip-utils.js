ad7df3c9a20bf762d9ebb4938bbe4c568d842261
Milos Popovic <misa01523@gmail.com>
2026-04-07 15:08:35 +0200

    Fixed tooltip hover mouseleave

--- webapp/src/main/webapp/js/tooltip/tooltip-utils.js
+++ webapp/src/main/webapp/js/tooltip/tooltip-utils.js
@@ -116,11 +116,11 @@ function setupHoverTrigger(element, data) {
     };
 
     element.addEventListener('mouseenter', showTooltip);
-    element.addEventListener('mouseleave', () => handleSoftHide);
+    element.addEventListener('mouseleave', () => handleSoftHide());
 
     element.addEventListener('focus', showTooltip);
     element.addEventListener('click', showTooltip);
-    element.addEventListener('focusout', () => handleSoftHide);
+    element.addEventListener('focusout', () => handleSoftHide());
     element.addEventListener('keydown', (e) => {
         if (e.key === 'Escape' || e.key === 'Esc') {
             handleSoftHide(0);
@@ -129,7 +129,7 @@ function setupHoverTrigger(element, data) {
 
     element.cleanupListeners = () => {
         element.removeEventListener('mouseenter', showTooltip);
-        element.removeEventListener('mouseleave', () => handleSoftHide);
+        element.removeEventListener('mouseleave', () => handleSoftHide());
         tooltip = removeTooltip(tooltip, element);
     };
 }
