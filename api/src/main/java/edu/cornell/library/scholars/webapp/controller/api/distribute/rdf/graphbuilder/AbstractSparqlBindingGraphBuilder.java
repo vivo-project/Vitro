a2d2e69e52c4b2ee6fc677fe9ef289ade007d0c5
Georgy Litvinov <git@litvinovg.pro>
2026-04-24 18:31:44 -0300

    Added DataDistributor Uri binding authorization

--- api/src/main/java/edu/cornell/library/scholars/webapp/controller/api/distribute/rdf/graphbuilder/AbstractSparqlBindingGraphBuilder.java
+++ api/src/main/java/edu/cornell/library/scholars/webapp/controller/api/distribute/rdf/graphbuilder/AbstractSparqlBindingGraphBuilder.java
@@ -3,13 +3,23 @@
 package edu.cornell.library.scholars.webapp.controller.api.distribute.rdf.graphbuilder;
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
-import edu.cornell.library.scholars.webapp.controller.api.distribute.DataDistributor.MissingParametersException;
+import edu.cornell.library.scholars.webapp.controller.api.distribute.DataDistributor.DataDistributorException;
+import edu.cornell.library.scholars.webapp.controller.api.distribute.DataDistributor.NotAuthorizedException;
 import edu.cornell.library.scholars.webapp.controller.api.distribute.DataDistributorContext;
 import edu.cornell.library.scholars.webapp.controller.api.distribute.rdf.util.VariableBinder;
+import edu.cornell.mannlib.vitro.webapp.auth.attributes.AccessOperation;
+import edu.cornell.mannlib.vitro.webapp.auth.objects.AccessObject;
+import edu.cornell.mannlib.vitro.webapp.auth.objects.IndividualAccessObject;
+import edu.cornell.mannlib.vitro.webapp.auth.requestedAction.AuthorizationRequest;
+import edu.cornell.mannlib.vitro.webapp.auth.requestedAction.SimpleAuthorizationRequest;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
+import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames;
 import edu.cornell.mannlib.vitro.webapp.utils.configuration.Property;
 import edu.cornell.mannlib.vitro.webapp.utils.sparqlrunner.QueryHolder;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Keep track of the binding details for DataDistributors that bind request
@@ -30,8 +40,28 @@ public abstract class AbstractSparqlBindingGraphBuilder extends AbstractGraphBui
     }
 
     protected QueryHolder bindParametersToQuery(DataDistributorContext ddContext, QueryHolder rawQuery)
-            throws MissingParametersException {
-        return new VariableBinder(ddContext.getRequestParameters()).bindValuesToQuery(uriBindingNames,
-                literalBindingNames, rawQuery);
+            throws DataDistributorException {
+        Map<String, String[]> parameters = ddContext.getRequestParameters();
+        checkAuthorization(ddContext, parameters);
+        return new VariableBinder(parameters).bindValuesToQuery(uriBindingNames, literalBindingNames, rawQuery);
+    }
+
+    private void checkAuthorization(DataDistributorContext ddContext, Map<String, String[]> parameters)
+            throws NotAuthorizedException {
+        for (String name : uriBindingNames) {
+            if (parameters.containsKey(name)) {
+                String[] uris = parameters.get(name);
+                for (String uri : uris) {
+                    if (StringUtils.isNotBlank(uri)) {
+                        AccessObject ao = new IndividualAccessObject(uri);
+                        ao.setModel(ModelAccess.getInstance().getOntModel(ModelNames.FULL_UNION));
+                        AuthorizationRequest request = new SimpleAuthorizationRequest(ao, AccessOperation.DISPLAY);
+                        if (!ddContext.isAuthorized(request)) {
+                            throw new NotAuthorizedException();
+                        }
+                    }
+                }
+            }
+        }
     }
 }
